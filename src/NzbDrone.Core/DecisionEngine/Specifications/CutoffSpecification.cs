7fb3128618b35ba44832f5911c808a40a2837a31
Qstick <qstick@gmail.com>
2017-08-13 01:04:06 -0400

    Fix CutoffSpecification
    Fix CutoffSpecification

--- src/NzbDrone.Core/DecisionEngine/Specifications/CutoffSpecification.cs
+++ src/NzbDrone.Core/DecisionEngine/Specifications/CutoffSpecification.cs
@@ -3,46 +3,48 @@ using System.Linq;
 using NLog;
 using NzbDrone.Core.IndexerSearch.Definitions;
 using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.MediaFiles;
 
 namespace NzbDrone.Core.DecisionEngine.Specifications
 {
     public class CutoffSpecification : IDecisionEngineSpecification
     {
         private readonly QualityUpgradableSpecification _qualityUpgradableSpecification;
+        private readonly IMediaFileService _mediaFileService;
         private readonly Logger _logger;
 
-        public CutoffSpecification(QualityUpgradableSpecification qualityUpgradableSpecification, Logger logger)
+        public CutoffSpecification(QualityUpgradableSpecification qualityUpgradableSpecification, Logger logger, IMediaFileService mediaFileService)
         {
             _qualityUpgradableSpecification = qualityUpgradableSpecification;
             _logger = logger;
+            _mediaFileService = mediaFileService;
         }
 
         public RejectionType Type => RejectionType.Permanent;
 
         public virtual Decision IsSatisfiedBy(RemoteAlbum subject, SearchCriteriaBase searchCriteria)
         {
-            // TODO: Implement Cutoff checks for albums, require us to check tracks in album
 
-            throw new NotImplementedException();
+            foreach (var album in subject.Albums)
+            {
+                var trackFiles = _mediaFileService.GetFilesByAlbum(album.ArtistId, album.Id);
 
-            //foreach (var file in subject.Episodes.Where(c => c.EpisodeFileId != 0).Select(c => c.EpisodeFile.Value))
-            //    {
-            //        if (file == null)
-            //        {
-            //            _logger.Debug("File is no longer available, skipping this file.");
-            //            continue;
-            //        }
+                if (trackFiles.Any())
+                {
+                    var lowestQuality = trackFiles.Select(c => c.Quality).OrderBy(c => c.Quality.Id).First();
 
-            //        _logger.Debug("Comparing file quality with report. Existing file is {0}", file.Quality);
+                    _logger.Debug("Comparing file quality with report. Existing file is {0}", lowestQuality);
 
-            //        if (!_qualityUpgradableSpecification.CutoffNotMet(subject.Series.Profile, file.Quality, subject.ParsedEpisodeInfo.Quality))
-            //        {
-            //            _logger.Debug("Cutoff already met, rejecting.");
-            //            return Decision.Reject("Existing file meets cutoff: {0}", subject.Series.Profile.Value.Cutoff);
-            //        }
-            //    }
+                    if (!_qualityUpgradableSpecification.CutoffNotMet(subject.Artist.Profile, lowestQuality, subject.ParsedAlbumInfo.Quality))
+                    {
+                        _logger.Debug("Cutoff already met, rejecting.");
+                        return Decision.Reject("Existing file meets cutoff: {0}", subject.Artist.Profile.Value.Cutoff);
+                    }
 
-            //    return Decision.Accept();
+                }
+            }
+
+            return Decision.Accept();
         }
     }
 }
