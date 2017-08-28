30d6e18bd31d7bea6cb530df5b4b412ceaa9e41a
Qstick <qstick@gmail.com>
2017-08-28 18:09:10 -0400

    Remove Commented Out Line

--- src/NzbDrone.Core/MediaFiles/TrackImport/IImportDecisionEngineSpecification.cs
+++ src/NzbDrone.Core/MediaFiles/TrackImport/IImportDecisionEngineSpecification.cs
@@ -5,7 +5,6 @@ namespace NzbDrone.Core.MediaFiles.TrackImport
 {
     public interface IImportDecisionEngineSpecification
     {
-        //Decision IsSatisfiedBy(LocalEpisode localEpisode);
         Decision IsSatisfiedBy(LocalTrack localTrack);
     }
 }
