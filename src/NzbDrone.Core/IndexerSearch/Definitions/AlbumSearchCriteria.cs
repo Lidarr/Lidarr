bcec54d06be9cecac83fc176f34aa657dc998de8
Markus Reumueller <reumuellermarkus@gmail.com>
2018-09-16 09:59:24 +0200

    use string interpolation for AlbumQuery

--- src/NzbDrone.Core/IndexerSearch/Definitions/AlbumSearchCriteria.cs
+++ src/NzbDrone.Core/IndexerSearch/Definitions/AlbumSearchCriteria.cs
@@ -1,16 +1,14 @@
 using NzbDrone.Common.Extensions;
-using System;
 
 namespace NzbDrone.Core.IndexerSearch.Definitions
 {
     public class AlbumSearchCriteria : SearchCriteriaBase
     {
-
         public string AlbumTitle { get; set; }
         public int AlbumYear { get; set; }
         public string Disambiguation { get; set; }
 
-        public string AlbumQuery => GetQueryTitle(AlbumTitle+(Disambiguation.IsNullOrWhiteSpace() ? string.Empty : $"+{Disambiguation}"));
+        public string AlbumQuery => GetQueryTitle($"{AlbumTitle}{(Disambiguation.IsNullOrWhiteSpace() ? string.Empty : $"+{Disambiguation}")}");
 
         public override string ToString()
         {
