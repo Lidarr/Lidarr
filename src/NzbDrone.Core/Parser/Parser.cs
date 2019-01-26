91913138e791fa1256b9abc0dc1402e10ac33e01
Tom Andrews <tom@andrewsmail.co.uk>
2019-01-26 08:16:52 +0000

    Fix NRE when no tags

--- src/NzbDrone.Core/Parser/Parser.cs
+++ src/NzbDrone.Core/Parser/Parser.cs
@@ -687,7 +687,7 @@ namespace NzbDrone.Core.Parser
                 TrackNumbers = temp,
                 ArtistTitleInfo = artistTitleInfo,
                 Title = trackTitle,
-                CleanTitle = trackTitle.CleanTrackTitle(),
+                CleanTitle = trackTitle?.CleanTrackTitle(),
                 Country = IsoCountries.Find(file.Tag.MusicBrainzReleaseCountry)
             };
 
