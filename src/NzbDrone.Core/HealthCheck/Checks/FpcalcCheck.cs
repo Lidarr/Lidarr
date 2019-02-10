6e80f222696a7d9f73304f29084ef91582b6146e
Tom Andrews <tom@andrewsmail.co.uk>
2019-02-10 22:06:16 +0000

    fpcalc health check passes if fingerprinting disabled

--- src/NzbDrone.Core/HealthCheck/Checks/FpcalcCheck.cs
+++ src/NzbDrone.Core/HealthCheck/Checks/FpcalcCheck.cs
@@ -1,19 +1,31 @@
 using System;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Configuration.Events;
 using NzbDrone.Core.Parser;
 
 namespace NzbDrone.Core.HealthCheck.Checks
 {
+    [CheckOn(typeof(ConfigSavedEvent))]
     public class FpcalcCheck : HealthCheckBase
     {
         private readonly IFingerprintingService _fingerprintingService;
+        private readonly IConfigService _configService;
         
-        public FpcalcCheck(IFingerprintingService fingerprintingService)
+        public FpcalcCheck(IFingerprintingService fingerprintingService,
+                           IConfigService configService)
         {
             _fingerprintingService = fingerprintingService;
+            _configService = configService;
         }
         
         public override HealthCheck Check()
         {
+            // always pass if fingerprinting is disabled
+            if (_configService.AllowFingerprinting == AllowFingerprinting.Never)
+            {
+                return new HealthCheck(GetType());
+            }
+            
             if (!_fingerprintingService.IsSetup())
             {
                 return new HealthCheck(GetType(), HealthCheckResult.Warning, $"fpcalc could not be found.  Audio fingerprinting disabled.", "#fpcalc-missing");
