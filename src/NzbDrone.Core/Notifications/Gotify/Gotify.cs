f756d6ba705c9ab0161988ec81be89275e9e05bb
Stephan Renggli <stephanrenggli@gmail.com>
2019-04-07 12:55:44 +0200

    Added support for Gotify notifications

---
+++ src/NzbDrone.Core/Notifications/Gotify/Gotify.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using FluentValidation.Results;
+using NzbDrone.Common.Extensions;
+
+namespace NzbDrone.Core.Notifications.Gotify
+{
+    public class Gotify : NotificationBase<GotifySettings>
+    {
+        private readonly IGotifyProxy _proxy;
+        
+        public Gotify(IGotifyProxy proxy)
+        {
+            _proxy = proxy;
+        }
+
+        public override string Name => "Gotify";
+        public override string Link => "https://gotify.net/";
+
+        public override void OnGrab(GrabMessage grabMessage)
+        {
+            _proxy.SendNotification(ALBUM_GRABBED_TITLE, grabMessage.Message, Settings);
+        }
+
+        public override void OnReleaseImport(AlbumDownloadMessage message)
+        {
+            _proxy.SendNotification(ALBUM_DOWNLOADED_TITLE, message.Message, Settings);
+        }
+
+        public override void OnHealthIssue(HealthCheck.HealthCheck healthCheck)
+        {
+            _proxy.SendNotification(HEALTH_ISSUE_TITLE, healthCheck.Message, Settings);
+        }
+
+        public override void OnDownloadFailure(DownloadFailedMessage message)
+        {
+            _proxy.SendNotification(DOWNLOAD_FAILURE_TITLE, message.Message, Settings);
+        }
+
+        public override void OnImportFailure(AlbumDownloadMessage message)
+        {
+            _proxy.SendNotification(IMPORT_FAILURE_TITLE, message.Message, Settings);
+        }
+
+        public override ValidationResult Test()
+        {
+            var failures = new List<ValidationFailure>();
+
+            failures.AddIfNotNull(_proxy.Test(Settings));
+
+            return new ValidationResult(failures);
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Notifications/Gotify/GotifyPriority.cs
@@ -0,0 +1,11 @@
+namespace NzbDrone.Core.Notifications.Gotify
+{
+    public enum GotifyPriority
+    {
+        Silent = -2,
+        Quiet = -1,
+        Normal = 0,
+        High = 1,
+        Emergency = 2
+    }
+}
