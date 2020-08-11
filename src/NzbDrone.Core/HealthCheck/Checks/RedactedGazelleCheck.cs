6c2cd7fe167f528fd1b53105adb1a3ecfdd2cdf9
ta264 <ta264@users.noreply.github.com>
2020-08-11 21:09:18 +0100

    New: Support for Redacted API keys
    Fixes #1127

---
+++ src/NzbDrone.Core/HealthCheck/Checks/RedactedGazelleCheck.cs
@@ -0,0 +1,37 @@
+using NzbDrone.Core.Indexers;
+using NzbDrone.Core.Indexers.Gazelle;
+using NzbDrone.Core.ThingiProvider.Events;
+
+namespace NzbDrone.Core.HealthCheck.Checks
+{
+    [CheckOn(typeof(ProviderAddedEvent<IIndexer>))]
+    [CheckOn(typeof(ProviderDeletedEvent<IIndexer>))]
+    [CheckOn(typeof(ProviderUpdatedEvent<IIndexer>))]
+    public class RedactedGazelleCheck : HealthCheckBase
+    {
+        private readonly IIndexerFactory _indexerFactory;
+
+        public RedactedGazelleCheck(IIndexerFactory indexerFactory)
+        {
+            _indexerFactory = indexerFactory;
+        }
+
+        public override HealthCheck Check()
+        {
+            var indexers = _indexerFactory.GetAvailableProviders();
+
+            foreach (var indexer in indexers)
+            {
+                var definition = (IndexerDefinition)indexer.Definition;
+
+                if (definition.Settings is GazelleSettings s &&
+                    s.BaseUrl == "https://redacted.ch")
+                {
+                    return new HealthCheck(GetType(), HealthCheckResult.Warning, "You have set up Redacted as a Gazelle indexer, please reconfigure using the Redacted indexer setting");
+                }
+            }
+
+            return new HealthCheck(GetType());
+        }
+    }
+}
