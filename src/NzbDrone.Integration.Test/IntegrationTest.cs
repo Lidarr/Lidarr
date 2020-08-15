0a4a7816029de46e2ec8ae0d4969faeaf41e2d11
ta264 <ta264@users.noreply.github.com>
2020-08-15 14:18:51 +0100

    Run integration tests in parallel
    Revert "Try to fix alpine integration tests"
    
    This reverts commit a47854103c049587c9b942eb87176c8b92851dfd

--- src/NzbDrone.Integration.Test/IntegrationTest.cs
+++ src/NzbDrone.Integration.Test/IntegrationTest.cs
@@ -1,24 +1,33 @@
+using System.Threading;
 using Lidarr.Http.ClientSchema;
 using NLog;
+using NUnit.Framework;
 using NzbDrone.Core.Indexers.Newznab;
 using NzbDrone.Test.Common;
 
 namespace NzbDrone.Integration.Test
 {
+    [Parallelizable(ParallelScope.Fixtures)]
     public abstract class IntegrationTest : IntegrationTestBase
     {
+        protected static int StaticPort = 8686;
+
         protected NzbDroneRunner _runner;
 
         public override string ArtistRootFolder => GetTempDirectory("ArtistRootFolder");
 
-        protected override string RootUrl => "http://localhost:8686/";
+        protected int Port { get; private set; }
+
+        protected override string RootUrl => $"http://localhost:{Port}/";
 
         protected override string ApiKey => _runner.ApiKey;
 
         protected override void StartTestTarget()
         {
-            _runner = new NzbDroneRunner(LogManager.GetCurrentClassLogger());
-            _runner.KillAll();
+            Port = Interlocked.Increment(ref StaticPort);
+
+            _runner = new NzbDroneRunner(LogManager.GetCurrentClassLogger(), Port);
+            _runner.Kill();
 
             _runner.Start();
         }
@@ -45,7 +54,7 @@ namespace NzbDrone.Integration.Test
 
         protected override void StopTestTarget()
         {
-            _runner.KillAll();
+            _runner.Kill();
         }
     }
 }
