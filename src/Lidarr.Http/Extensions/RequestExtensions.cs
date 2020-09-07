ece02e734766fd50af46521b90716dfda45070eb
Qstick <qstick@gmail.com>
2020-09-06 22:51:25 -0400

    Simplify ManualImportModule null check

--- src/Lidarr.Http/Extensions/RequestExtensions.cs
+++ src/Lidarr.Http/Extensions/RequestExtensions.cs
@@ -78,5 +78,17 @@ namespace Lidarr.Http.Extensions
 
             return defaultValue;
         }
+
+        public static int? GetNullableIntegerQueryParameter(this Request request, string parameter, int? defaultValue = null)
+        {
+            var parameterValue = request.Query[parameter];
+
+            if (parameterValue.HasValue)
+            {
+                return int.Parse(parameterValue.Value);
+            }
+
+            return defaultValue;
+        }
     }
 }
