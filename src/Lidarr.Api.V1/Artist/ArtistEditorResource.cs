cca1b05991955c1b881f8f64e2e1ff5da337f0c7
Qstick <qstick@gmail.com>
2020-09-07 00:33:17 -0400

    Fixed: Delete files from Artist Mass Editor not actually deleting files
    Co-Authored-By: Mark McDowall <markus101@users.noreply.github.com>

--- src/Lidarr.Api.V1/Artist/ArtistEditorResource.cs
+++ src/Lidarr.Api.V1/Artist/ArtistEditorResource.cs
@@ -13,6 +13,7 @@ namespace Lidarr.Api.V1.Artist
         public List<int> Tags { get; set; }
         public ApplyTags ApplyTags { get; set; }
         public bool MoveFiles { get; set; }
+        public bool DeleteFiles { get; set; }
     }
 
     public enum ApplyTags
