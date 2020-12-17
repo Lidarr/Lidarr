Sonarr/Sonarr

New: Health events for Webhooks

src/NzbDrone.Core/Notifications/Webhook/WebhookGrabPayload.cs
@@ -4,6 +4,7 @@ namespace NzbDrone.Core.Notifications.Webhook
 {
     public class WebhookGrabPayload : WebhookPayload
     {
+        public WebhookSeries Series { get; set; }
         public List<WebhookEpisode> Episodes { get; set; }
         public WebhookRelease Release { get; set; }
         public string DownloadClient { get; set; }

src/NzbDrone.Core/Notifications/Webhook/WebhookImportPayload.cs
@@ -4,6 +4,7 @@ namespace NzbDrone.Core.Notifications.Webhook
 {
     public class WebhookImportPayload : WebhookPayload
     {
+        public WebhookSeries Series { get; set; }
         public List<WebhookEpisode> Episodes { get; set; }
         public WebhookEpisodeFile EpisodeFile { get; set; }
         public bool IsUpgrade { get; set; }

Lidarr/Lidarr

src/NzbDrone.Core/Notifications/Webhook/WebhookGrabPayload.cs
@@ -4,6 +4,7 @@ namespace NzbDrone.Core.Notifications.Webhook
 {
     public class WebhookGrabPayload : WebhookPayload
     {
+        public WebhookArtist Artist { get; set; }
         public List<WebhookAlbum> Albums { get; set; }
         public WebhookRelease Release { get; set; }
         public string DownloadClient { get; set; }

src/NzbDrone.Core/Notifications/Webhook/WebhookImportPayload.cs
@@ -4,6 +4,7 @@ namespace NzbDrone.Core.Notifications.Webhook
 {
     public class WebhookImportPayload : WebhookPayload
     {
+        public WebhookArtist Artist { get; set; }
         public List<WebhookTrack> Tracks { get; set; }
         public List<WebhookTrackFile> TrackFiles { get; set; }
         public bool IsUpgrade { get; set; }