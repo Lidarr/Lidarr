TheDoubleH/Sonarr

Added PrivacyLevel option to FieldDefinition for later usage

src/NzbDrone.Core/Notifications/Pushover/PushoverSettings.cs
@@ -27,10 +27,10 @@ namespace NzbDrone.Core.Notifications.Pushover
         }
 
         //TODO: Get Pushover to change our app name (or create a new app) when we have a new logo
-        [FieldDefinition(0, Label = "API Key", HelpLink = "https://pushover.net/apps/clone/sonarr")]
+        [FieldDefinition(0, Label = "API Key", Privacy = PrivacyLevel.ApiKey, HelpLink = "https://pushover.net/apps/clone/sonarr")]
         public string ApiKey { get; set; }
 
-        [FieldDefinition(1, Label = "User Key", HelpLink = "https://pushover.net/")]
+        [FieldDefinition(1, Label = "User Key", Privacy = PrivacyLevel.UserName, HelpLink = "https://pushover.net/")]
         public string UserKey { get; set; }
 
         [FieldDefinition(2, Label = "Devices", HelpText = "List of device names (leave blank to send to all devices)", Type = FieldType.Tag)]

Lidarr/Lidarr

src/NzbDrone.Core/Notifications/Pushover/PushoverSettings.cs
@@ -27,10 +27,10 @@ namespace NzbDrone.Core.Notifications.Pushover
         }
 
         //TODO: Get Pushover to change our app name (or create a new app) when we have a new logo
-        [FieldDefinition(0, Label = "API Key", HelpLink = "https://pushover.net/apps/clone/lidarr")]
+        [FieldDefinition(0, Label = "API Key", Privacy = PrivacyLevel.ApiKey, HelpLink = "https://pushover.net/apps/clone/lidarr")]
         public string ApiKey { get; set; }
 
-        [FieldDefinition(1, Label = "User Key", HelpLink = "https://pushover.net/")]
+        [FieldDefinition(1, Label = "User Key", Privacy = PrivacyLevel.UserName, HelpLink = "https://pushover.net/")]
         public string UserKey { get; set; }
 
         [FieldDefinition(2, Label = "Devices", HelpText = "List of device names (leave blank to send to all devices)", Type = FieldType.Tag)]