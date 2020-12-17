Sonarr/Sonarr

Fixed: Regression causing updater to fail (manual update required if on 3.0.3.971, see forums)

src/NzbDrone.Update/UpdateEngine/InstallUpdateService.cs
@@ -84,6 +84,12 @@ namespace NzbDrone.Update.UpdateEngine
 
             Verify(installationFolder, processId);
 
+            if (installationFolder.EndsWith(@"\bin\Sonarr") || installationFolder.EndsWith(@"/bin/Sonarr"))
+            {
+                installationFolder = installationFolder.GetParentPath();
+                _logger.Info("Fixed Installation Folder: {0}", installationFolder);
+            }
+
             var appType = _detectApplicationType.GetAppType();
 
             _processProvider.FindProcessByName(ProcessProvider.SONARR_CONSOLE_PROCESS_NAME);

Lidarr/Lidarr

src/NzbDrone.Update/UpdateEngine/InstallUpdateService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using NLog;
 using NzbDrone.Common.Disk;
@@ -89,6 +89,12 @@ namespace NzbDrone.Update.UpdateEngine
             _logger.Info("Updating Lidarr from version {0} to version {1}", _detectExistingVersion.GetExistingVersion(installationFolder), BuildInfo.Version);
 
             Verify(installationFolder, processId);
+
+            if (installationFolder.EndsWith(@"\bin\Lidarr") || installationFolder.EndsWith(@"/bin/Lidarr"))
+            {
+                installationFolder = installationFolder.GetParentPath();
+                _logger.Info("Fixed Installation Folder: {0}", installationFolder);
+            }
 
             var appType = _detectApplicationType.GetAppType();
 