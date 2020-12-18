Radarr/Radarr

Convert Notifications from RestSharp to HttpClient

src/NzbDrone.Core/Notifications/Boxcar/BoxcarProxy.cs
@@ -1,10 +1,9 @@
-﻿using System;
+using System;
 using System.Net;
 using FluentValidation.Results;
 using NLog;
 using NzbDrone.Common.EnvironmentInfo;
-using NzbDrone.Core.Rest;
-using RestSharp;
+using NzbDrone.Common.Http;
 
 namespace NzbDrone.Core.Notifications.Boxcar
 {
@@ -17,22 +16,20 @@ namespace NzbDrone.Core.Notifications.Boxcar
     public class BoxcarProxy : IBoxcarProxy
     {
         private const string URL = "https://new.boxcar.io/api/notifications";
-        private readonly IRestClientFactory _restClientFactory;
+        private readonly IHttpClient _httpClient;
         private readonly Logger _logger;
 
-        public BoxcarProxy(IRestClientFactory restClientFactory, Logger logger)
+        public BoxcarProxy(IHttpClient httpClient, Logger logger)
         {
-            _restClientFactory = restClientFactory;
+            _httpClient = httpClient;
             _logger = logger;
         }
 
         public void SendNotification(string title, string message, BoxcarSettings settings)
         {
-            var request = new RestRequest(Method.POST);
-
             try
             {
-                SendNotification(title, message, request, settings);
+                ProcessNotification(title, message, settings);
             }
             catch (BoxcarException ex)
             {
@@ -51,7 +48,7 @@ namespace NzbDrone.Core.Notifications.Boxcar
                 SendNotification(title, body, settings);
                 return null;
             }
-            catch (RestException ex)
+            catch (HttpException ex)
             {
                 if (ex.Response.StatusCode == HttpStatusCode.Unauthorized)
                 {
@@ -69,21 +66,22 @@ namespace NzbDrone.Core.Notifications.Boxcar
             }
         }
 
-        private void SendNotification(string title, string message, RestRequest request, BoxcarSettings settings)
+        private void ProcessNotification(string title, string message, BoxcarSettings settings)
         {
             try
             {
-                var client = _restClientFactory.BuildClient(URL);
+                var requestBuilder = new HttpRequestBuilder(URL).Post();
 
-                request.AddParameter("user_credentials", settings.Token);
-                request.AddParameter("notification[title]", title);
-                request.AddParameter("notification[long_message]", message);
-                request.AddParameter("notification[source_name]", BuildInfo.AppName);
-                request.AddParameter("notification[icon_url]", "https://raw.githubusercontent.com/Radarr/Radarr/develop/Logo/64.png");
+                var request = requestBuilder.AddFormParameter("user_credentials", settings.Token)
+                    .AddFormParameter("notification[title]", title)
+                    .AddFormParameter("notification[long_message]", message)
+                    .AddFormParameter("notification[source_name]", BuildInfo.AppName)
+                    .AddFormParameter("notification[icon_url]", "https://raw.githubusercontent.com/Radarr/Radarr/develop/Logo/64.png")
+                    .Build();
 
-                client.ExecuteAndValidate(request);
+                _httpClient.Post(request);
             }
-            catch (RestException ex)
+            catch (HttpException ex)
             {
                 if (ex.Response.StatusCode == HttpStatusCode.Unauthorized)
                 {

src/NzbDrone.Core/Notifications/Join/JoinProxy.cs
@@ -2,9 +2,8 @@ using System;
 using FluentValidation.Results;
 using NLog;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Common.Http;
 using NzbDrone.Common.Serializer;
-using NzbDrone.Core.Rest;
-using RestSharp;
 
 namespace NzbDrone.Core.Notifications.Join
 {
@@ -17,22 +16,22 @@ namespace NzbDrone.Core.Notifications.Join
     public class JoinProxy : IJoinProxy
     {
         private const string URL = "https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?";
-        private readonly IRestClientFactory _restClientFactory;
+        private readonly IHttpClient _httpClient;
         private readonly Logger _logger;
 
-        public JoinProxy(IRestClientFactory restClientFactory, Logger logger)
+        public JoinProxy(IHttpClient httpClient, Logger logger)
         {
-            _restClientFactory = restClientFactory;
+            _httpClient = httpClient;
             _logger = logger;
         }
 
         public void SendNotification(string title, string message, JoinSettings settings)
         {
-            var request = new RestRequest(Method.GET);
+            var method = HttpMethod.GET;
 
             try
             {
-                SendNotification(title, message, request, settings);
+                SendNotification(title, message, method, settings);
             }
             catch (JoinException ex)
             {
@@ -61,7 +60,7 @@ namespace NzbDrone.Core.Notifications.Join
                 _logger.Error(ex, "Unable to send test Join message.");
                 return new ValidationFailure("ApiKey", ex.Message);
             }
-            catch (RestException ex)
+            catch (HttpException ex)
             {
                 _logger.Error(ex, "Unable to send test Join message. Server connection failed.");
                 return new ValidationFailure("ApiKey", "Unable to connect to Join API. Please try again later.");
@@ -73,31 +72,34 @@ namespace NzbDrone.Core.Notifications.Join
             }
         }
 
-        private void SendNotification(string title, string message, RestRequest request, JoinSettings settings)
+        private void SendNotification(string title, string message, HttpMethod method, JoinSettings settings)
         {
-            var client = _restClientFactory.BuildClient(URL);
+            var requestBuilder = new HttpRequestBuilder(URL);
 
             if (settings.DeviceNames.IsNotNullOrWhiteSpace())
             {
-                request.AddParameter("deviceNames", settings.DeviceNames);
+                requestBuilder.AddQueryParam("deviceNames", settings.DeviceNames);
             }
             else if (settings.DeviceIds.IsNotNullOrWhiteSpace())
             {
-                request.AddParameter("deviceIds", settings.DeviceIds);
+                requestBuilder.AddQueryParam("deviceIds", settings.DeviceIds);
             }
             else
             {
-                request.AddParameter("deviceId", "group.all");
+                requestBuilder.AddQueryParam("deviceId", "group.all");
             }
 
-            request.AddParameter("apikey", settings.ApiKey);
-            request.AddParameter("title", title);
-            request.AddParameter("text", message);
-            request.AddParameter("icon", "https://cdn.rawgit.com/Radarr/Radarr/develop/Logo/256.png"); // Use the Radarr logo.
-            request.AddParameter("smallicon", "https://cdn.rawgit.com/Radarr/Radarr/develop/Logo/96-Outline-White.png"); // 96x96px with outline at 88x88px on a transparent background.
-            request.AddParameter("priority", settings.Priority);
+            var request = requestBuilder.AddQueryParam("apikey", settings.ApiKey)
+                          .AddQueryParam("title", title)
+                          .AddQueryParam("text", message)
+                          .AddQueryParam("icon", "https://cdn.rawgit.com/Radarr/Radarr/develop/Logo/256.png") // Use the Radarr logo.
+                          .AddQueryParam("smallicon", "https://cdn.rawgit.com/Radarr/Radarr/develop/Logo/96-Outline-White.png") // 96x96px with outline at 88x88px on a transparent background.
+                          .AddQueryParam("priority", settings.Priority)
+                          .Build();
 
-            var response = client.ExecuteAndValidate(request);
+            request.Method = method;
+
+            var response = _httpClient.Execute(request);
             var res = Json.Deserialize<JoinResponseModel>(response.Content);
 
             if (res.success)

src/NzbDrone.Core/Notifications/Xbmc/XbmcJsonApiProxy.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using NLog;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Common.Http;
 using NzbDrone.Common.Serializer;
 using NzbDrone.Core.Notifications.Xbmc.Model;
-using NzbDrone.Core.Rest;
-using RestSharp;
-using RestSharp.Authenticators;
 
 namespace NzbDrone.Core.Notifications.Xbmc
 {
@@ -21,87 +19,66 @@ namespace NzbDrone.Core.Notifications.Xbmc
 
     public class XbmcJsonApiProxy : IXbmcJsonApiProxy
     {
-        private readonly IRestClientFactory _restClientFactory;
+        private readonly IHttpClient _httpClient;
         private readonly Logger _logger;
 
-        public XbmcJsonApiProxy(IRestClientFactory restClientFactory, Logger logger)
+        public XbmcJsonApiProxy(IHttpClient httpClient, Logger logger)
         {
-            _restClientFactory = restClientFactory;
+            _httpClient = httpClient;
             _logger = logger;
         }
 
         public string GetJsonVersion(XbmcSettings settings)
         {
-            var request = new RestRequest();
-            return ProcessRequest(request, settings, "JSONRPC.Version");
+            return ProcessRequest(settings, "JSONRPC.Version");
         }
 
         public void Notify(XbmcSettings settings, string title, string message)
         {
-            var request = new RestRequest();
-
-            var parameters = new Dictionary<string, object>();
-            parameters.Add("title", title);
-            parameters.Add("message", message);
-            parameters.Add("image", "https://raw.github.com/Radarr/Radarr/develop/Logo/64.png");
-            parameters.Add("displaytime", settings.DisplayTime * 1000);
-
-            ProcessRequest(request, settings, "GUI.ShowNotification", parameters);
+            ProcessRequest(settings, "GUI.ShowNotification", title, message, "https://raw.github.com/Radarr/Radarr/develop/Logo/64.png", settings.DisplayTime * 1000);
         }
 
         public string UpdateLibrary(XbmcSettings settings, string path)
         {
-            var request = new RestRequest();
-            var parameters = new Dictionary<string, object>();
-            parameters.Add("directory", path);
-
-            if (path.IsNullOrWhiteSpace())
-            {
-                parameters = null;
-            }
-
-            var response = ProcessRequest(request, settings, "VideoLibrary.Scan", parameters);
+            var response = ProcessRequest(settings, "VideoLibrary.Scan", path);
 
             return Json.Deserialize<XbmcJsonResult<string>>(response).Result;
         }
 
         public void CleanLibrary(XbmcSettings settings)
         {
-            var request = new RestRequest();
-
-            ProcessRequest(request, settings, "VideoLibrary.Clean");
+            ProcessRequest(settings, "VideoLibrary.Clean");
         }
 
         public List<ActivePlayer> GetActivePlayers(XbmcSettings settings)
         {
-            var request = new RestRequest();
-
-            var response = ProcessRequest(request, settings, "Player.GetActivePlayers");
+            var response = ProcessRequest(settings, "Player.GetActivePlayers");
 
             return Json.Deserialize<ActivePlayersResult>(response).Result;
         }
 
         public List<XbmcMovie> GetMovies(XbmcSettings settings)
         {
-            var request = new RestRequest();
-            var parameters = new Dictionary<string, object>();
-            parameters.Add("properties", new[] { "file", "imdbnumber" });
-
-            var response = ProcessRequest(request, settings, "VideoLibrary.GetMovies", parameters);
+            var response = ProcessRequest(settings, "VideoLibrary.GetMovies", new[] { "file", "imdbnumber" });
 
             return Json.Deserialize<MovieResponse>(response).Result.Movies;
         }
 
-        private string ProcessRequest(IRestRequest request, XbmcSettings settings, string method, Dictionary<string, object> parameters = null)
+        private string ProcessRequest(XbmcSettings settings, string method, params object[] parameters)
         {
-            var client = BuildClient(settings);
+            var url = string.Format(@"http://{0}/jsonrpc", settings.Address);
+            var requestBuilder = new JsonRpcRequestBuilder(url, method, parameters);
 
-            request.Method = Method.POST;
-            request.RequestFormat = DataFormat.Json;
-            request.JsonSerializer = new JsonNetSerializer();
-            request.AddBody(new { jsonrpc = "2.0", method = method, id = 10, @params = parameters });
+            requestBuilder.LogResponseContent = true;
 
-            var response = client.ExecuteAndValidate(request);
+            var request = requestBuilder.Build();
+
+            if (!settings.Username.IsNullOrWhiteSpace())
+            {
+                request.AddBasicAuthentication(settings.Username, settings.Password);
+            }
+
+            var response = _httpClient.Execute(request);
             _logger.Trace("Response: {0}", response.Content);
 
             CheckForError(response);
@@ -109,20 +86,7 @@ namespace NzbDrone.Core.Notifications.Xbmc
             return response.Content;
         }
 
-        private IRestClient BuildClient(XbmcSettings settings)
-        {
-            var url = string.Format(@"http://{0}/jsonrpc", settings.Address);
-            var client = _restClientFactory.BuildClient(url);
-
-            if (!settings.Username.IsNullOrWhiteSpace())
-            {
-                client.Authenticator = new HttpBasicAuthenticator(settings.Username, settings.Password);
-            }
-
-            return client;
-        }
-
-        private void CheckForError(IRestResponse response)
+        private void CheckForError(HttpResponse response)
         {
             if (string.IsNullOrWhiteSpace(response.Content))
             {

Lidarr/Lidarr

src/NzbDrone.Core/Notifications/Boxcar/BoxcarProxy.cs
@@ -3,8 +3,7 @@ using System.Net;
 using FluentValidation.Results;
 using NLog;
 using NzbDrone.Common.EnvironmentInfo;
-using NzbDrone.Core.Rest;
-using RestSharp;
+using NzbDrone.Common.Http;
 
 namespace NzbDrone.Core.Notifications.Boxcar
 {
@@ -17,22 +16,20 @@ namespace NzbDrone.Core.Notifications.Boxcar
     public class BoxcarProxy : IBoxcarProxy
     {
         private const string URL = "https://new.boxcar.io/api/notifications";
-        private readonly IRestClientFactory _restClientFactory;
+        private readonly IHttpClient _httpClient;
         private readonly Logger _logger;
 
-        public BoxcarProxy(IRestClientFactory restClientFactory, Logger logger)
+        public BoxcarProxy(IHttpClient httpClient, Logger logger)
         {
-            _restClientFactory = restClientFactory;
+            _httpClient = httpClient;
             _logger = logger;
         }
 
         public void SendNotification(string title, string message, BoxcarSettings settings)
         {
-            var request = new RestRequest(Method.POST);
-
             try
             {
-                SendNotification(title, message, request, settings);
+                ProcessNotification(title, message, settings);
             }
             catch (BoxcarException ex)
             {
@@ -51,7 +48,7 @@ namespace NzbDrone.Core.Notifications.Boxcar
                 SendNotification(title, body, settings);
                 return null;
             }
-            catch (RestException ex)
+            catch (HttpException ex)
             {
                 if (ex.Response.StatusCode == HttpStatusCode.Unauthorized)
                 {
@@ -69,21 +66,22 @@ namespace NzbDrone.Core.Notifications.Boxcar
             }
         }
 
-        private void SendNotification(string title, string message, RestRequest request, BoxcarSettings settings)
+        private void ProcessNotification(string title, string message, BoxcarSettings settings)
         {
             try
             {
-                var client = _restClientFactory.BuildClient(URL);
+                var requestBuilder = new HttpRequestBuilder(URL).Post();
 
-                request.AddParameter("user_credentials", settings.Token);
-                request.AddParameter("notification[title]", title);
-                request.AddParameter("notification[long_message]", message);
-                request.AddParameter("notification[source_name]", BuildInfo.AppName);
-                request.AddParameter("notification[icon_url]", "https://github.com/lidarr/Lidarr/raw/develop/Logo/64.png");
+                var request = requestBuilder.AddFormParameter("user_credentials", settings.Token)
+                    .AddFormParameter("notification[title]", title)
+                    .AddFormParameter("notification[long_message]", message)
+                    .AddFormParameter("notification[source_name]", BuildInfo.AppName)
+                    .AddFormParameter("notification[icon_url]", "https://raw.githubusercontent.com/Lidarr/Lidarr/develop/Logo/64.png")
+                    .Build();
 
-                client.ExecuteAndValidate(request);
+                _httpClient.Post(request);
             }
-            catch (RestException ex)
+            catch (HttpException ex)
             {
                 if (ex.Response.StatusCode == HttpStatusCode.Unauthorized)
                 {

src/NzbDrone.Core/Notifications/Join/JoinProxy.cs
@@ -2,9 +2,8 @@ using System;
 using FluentValidation.Results;
 using NLog;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Common.Http;
 using NzbDrone.Common.Serializer;
-using NzbDrone.Core.Rest;
-using RestSharp;
 
 namespace NzbDrone.Core.Notifications.Join
 {
@@ -17,22 +16,22 @@ namespace NzbDrone.Core.Notifications.Join
     public class JoinProxy : IJoinProxy
     {
         private const string URL = "https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?";
-        private readonly IRestClientFactory _restClientFactory;
+        private readonly IHttpClient _httpClient;
         private readonly Logger _logger;
 
-        public JoinProxy(IRestClientFactory restClientFactory, Logger logger)
+        public JoinProxy(IHttpClient httpClient, Logger logger)
         {
-            _restClientFactory = restClientFactory;
+            _httpClient = httpClient;
             _logger = logger;
         }
 
         public void SendNotification(string title, string message, JoinSettings settings)
         {
-            var request = new RestRequest(Method.GET);
+            var method = HttpMethod.GET;
 
             try
             {
-                SendNotification(title, message, request, settings);
+                SendNotification(title, message, method, settings);
             }
             catch (JoinException ex)
             {
@@ -61,7 +60,7 @@ namespace NzbDrone.Core.Notifications.Join
                 _logger.Error(ex, "Unable to send test Join message.");
                 return new ValidationFailure("ApiKey", ex.Message);
             }
-            catch (RestException ex)
+            catch (HttpException ex)
             {
                 _logger.Error(ex, "Unable to send test Join message. Server connection failed.");
                 return new ValidationFailure("ApiKey", "Unable to connect to Join API. Please try again later.");
@@ -73,31 +72,34 @@ namespace NzbDrone.Core.Notifications.Join
             }
         }
 
-        private void SendNotification(string title, string message, RestRequest request, JoinSettings settings)
+        private void SendNotification(string title, string message, HttpMethod method, JoinSettings settings)
         {
-            var client = _restClientFactory.BuildClient(URL);
+            var requestBuilder = new HttpRequestBuilder(URL);
 
             if (settings.DeviceNames.IsNotNullOrWhiteSpace())
             {
-                request.AddParameter("deviceNames", settings.DeviceNames);
+                requestBuilder.AddQueryParam("deviceNames", settings.DeviceNames);
             }
             else if (settings.DeviceIds.IsNotNullOrWhiteSpace())
             {
-                request.AddParameter("deviceIds", settings.DeviceIds);
+                requestBuilder.AddQueryParam("deviceIds", settings.DeviceIds);
             }
             else
             {
-                request.AddParameter("deviceId", "group.all");
+                requestBuilder.AddQueryParam("deviceId", "group.all");
             }
 
-            request.AddParameter("apikey", settings.ApiKey);
-            request.AddParameter("title", title);
-            request.AddParameter("text", message);
-            request.AddParameter("icon", "https://cdn.rawgit.com/Lidarr/Lidarr/develop/Logo/256.png"); // Use the Lidarr logo.
-            request.AddParameter("smallicon", "https://cdn.rawgit.com/Lidarr/Lidarr/develop/Logo/96-Outline-White.png"); // 96x96px with outline at 88x88px on a transparent background.
-            request.AddParameter("priority", settings.Priority);
+            var request = requestBuilder.AddQueryParam("apikey", settings.ApiKey)
+                          .AddQueryParam("title", title)
+                          .AddQueryParam("text", message)
+                          .AddQueryParam("icon", "https://cdn.rawgit.com/Lidarr/Lidarr/develop/Logo/256.png") // Use the Lidarr logo.
+                          .AddQueryParam("smallicon", "https://cdn.rawgit.com/Lidarr/Lidarr/develop/Logo/96-Outline-White.png") // 96x96px with outline at 88x88px on a transparent background.
+                          .AddQueryParam("priority", settings.Priority)
+                          .Build();
 
-            var response = client.ExecuteAndValidate(request);
+            request.Method = method;
+
+            var response = _httpClient.Execute(request);
             var res = Json.Deserialize<JoinResponseModel>(response.Content);
 
             if (res.success)

src/NzbDrone.Core/Notifications/Xbmc/XbmcJsonApiProxy.cs
@@ -1,11 +1,9 @@
 using System.Collections.Generic;
 using NLog;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Common.Http;
 using NzbDrone.Common.Serializer;
 using NzbDrone.Core.Notifications.Xbmc.Model;
-using NzbDrone.Core.Rest;
-using RestSharp;
-using RestSharp.Authenticators;
 
 namespace NzbDrone.Core.Notifications.Xbmc
 {
@@ -21,87 +19,66 @@ namespace NzbDrone.Core.Notifications.Xbmc
 
     public class XbmcJsonApiProxy : IXbmcJsonApiProxy
     {
-        private readonly IRestClientFactory _restClientFactory;
+        private readonly IHttpClient _httpClient;
         private readonly Logger _logger;
 
-        public XbmcJsonApiProxy(IRestClientFactory restClientFactory, Logger logger)
+        public XbmcJsonApiProxy(IHttpClient httpClient, Logger logger)
         {
-            _restClientFactory = restClientFactory;
+            _httpClient = httpClient;
             _logger = logger;
         }
 
         public string GetJsonVersion(XbmcSettings settings)
         {
-            var request = new RestRequest();
-            return ProcessRequest(request, settings, "JSONRPC.Version");
+            return ProcessRequest(settings, "JSONRPC.Version");
         }
 
         public void Notify(XbmcSettings settings, string title, string message)
         {
-            var request = new RestRequest();
-
-            var parameters = new Dictionary<string, object>();
-            parameters.Add("title", title);
-            parameters.Add("message", message);
-            parameters.Add("image", "https://raw.github.com/Lidarr/Lidarr/develop/Logo/64.png");
-            parameters.Add("displaytime", settings.DisplayTime * 1000);
-
-            ProcessRequest(request, settings, "GUI.ShowNotification", parameters);
+            ProcessRequest(settings, "GUI.ShowNotification", title, message, "https://raw.github.com/Lidarr/Lidarr/develop/Logo/64.png", settings.DisplayTime * 1000);
         }
 
         public string UpdateLibrary(XbmcSettings settings, string path)
         {
-            var request = new RestRequest();
-            var parameters = new Dictionary<string, object>();
-            parameters.Add("directory", path);
-
-            if (path.IsNullOrWhiteSpace())
-            {
-                parameters = null;
-            }
-
-            var response = ProcessRequest(request, settings, "AudioLibrary.Scan", parameters);
+            var response = ProcessRequest(settings, "AudioLibrary.Scan", path);
 
             return Json.Deserialize<XbmcJsonResult<string>>(response).Result;
         }
 
         public void CleanLibrary(XbmcSettings settings)
         {
-            var request = new RestRequest();
-
-            ProcessRequest(request, settings, "AudioLibrary.Clean");
+            ProcessRequest(settings, "AudioLibrary.Clean");
         }
 
         public List<ActivePlayer> GetActivePlayers(XbmcSettings settings)
         {
-            var request = new RestRequest();
-
-            var response = ProcessRequest(request, settings, "Player.GetActivePlayers");
+            var response = ProcessRequest(settings, "Player.GetActivePlayers");
 
             return Json.Deserialize<ActivePlayersResult>(response).Result;
         }
 
         public List<KodiArtist> GetArtist(XbmcSettings settings)
         {
-            var request = new RestRequest();
-            var parameters = new Dictionary<string, object>();
-            parameters.Add("properties", new[] { "musicbrainzartistid" }); //TODO: Figure out why AudioLibrary doesnt list file location like videoLibray
-
-            var response = ProcessRequest(request, settings, "AudioLibrary.GetArtists", parameters);
+            var response = ProcessRequest(settings, "AudioLibrary.GetArtists", new[] { "properties", "musicbrainzartistid" });
 
             return Json.Deserialize<ArtistResponse>(response).Result.Artists;
         }
 
-        private string ProcessRequest(IRestRequest request, XbmcSettings settings, string method, Dictionary<string, object> parameters = null)
+        private string ProcessRequest(XbmcSettings settings, string method, params object[] parameters)
         {
-            var client = BuildClient(settings);
+            var url = string.Format(@"http://{0}/jsonrpc", settings.Address);
+            var requestBuilder = new JsonRpcRequestBuilder(url, method, parameters);
 
-            request.Method = Method.POST;
-            request.RequestFormat = DataFormat.Json;
-            request.JsonSerializer = new JsonNetSerializer();
-            request.AddBody(new { jsonrpc = "2.0", method = method, id = 10, @params = parameters });
+            requestBuilder.LogResponseContent = true;
 
-            var response = client.ExecuteAndValidate(request);
+            var request = requestBuilder.Build();
+
+            if (!settings.Username.IsNullOrWhiteSpace())
+            {
+                request.AddBasicAuthentication(settings.Username, settings.Password);
+            }
+
+            var response = _httpClient.Execute(request);
             _logger.Trace("Response: {0}", response.Content);
 
             CheckForError(response);
@@ -109,20 +86,7 @@ namespace NzbDrone.Core.Notifications.Xbmc
             return response.Content;
         }
 
-        private IRestClient BuildClient(XbmcSettings settings)
-        {
-            var url = string.Format(@"http://{0}/jsonrpc", settings.Address);
-            var client = _restClientFactory.BuildClient(url);
-
-            if (!settings.Username.IsNullOrWhiteSpace())
-            {
-                client.Authenticator = new HttpBasicAuthenticator(settings.Username, settings.Password);
-            }
-
-            return client;
-        }
-
-        private void CheckForError(IRestResponse response)
+        private void CheckForError(HttpResponse response)
         {
             if (string.IsNullOrWhiteSpace(response.Content))
             {