Sonarr/Sonarr

Pass no parameter instead of null parameter on Kodi Update

src/NzbDrone.Core/Notifications/Xbmc/XbmcJsonApiProxy.cs
@@ -40,7 +40,16 @@ namespace NzbDrone.Core.Notifications.Xbmc
 
         public string UpdateLibrary(XbmcSettings settings, string path)
         {
-            var response = ProcessRequest(settings, "VideoLibrary.Scan", path);
+            string response;
+
+            if (path == null)
+            {
+                response = ProcessRequest(settings, "VideoLibrary.Scan");
+            }
+            else
+            {
+                response = ProcessRequest(settings, "VideoLibrary.Scan", path);
+            }
 
             return Json.Deserialize<XbmcJsonResult<string>>(response).Result;
         }

Lidarr/Lidarr

src/NzbDrone.Core/Notifications/Xbmc/XbmcJsonApiProxy.cs
@@ -40,7 +40,16 @@ namespace NzbDrone.Core.Notifications.Xbmc
 
         public string UpdateLibrary(XbmcSettings settings, string path)
         {
-            var response = ProcessRequest(settings, "AudioLibrary.Scan", path);
+            string response;
+
+            if (path == null)
+            {
+                response = ProcessRequest(settings, "AudioLibrary.Scan");
+            }
+            else
+            {
+                response = ProcessRequest(settings, "AudioLibrary.Scan", path);
+            }
 
             return Json.Deserialize<XbmcJsonResult<string>>(response).Result;
         }