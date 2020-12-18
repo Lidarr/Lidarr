Sonarr/Sonarr

Kodi GetMovies fails due to Parameter Type

src/NzbDrone.Core/Notifications/Xbmc/XbmcJsonApiProxy.cs
@@ -59,7 +59,7 @@ namespace NzbDrone.Core.Notifications.Xbmc
 
         public List<TvShow> GetSeries(XbmcSettings settings)
         {
-            var response = ProcessRequest(settings, "VideoLibrary.GetMovies", new[] { "file", "imdbnumber" });
+            var response = ProcessRequest(settings, "VideoLibrary.GetTvShows", new List<string> { "file", "imdbnumber" });
 
             return Json.Deserialize<TvShowResponse>(response).Result.TvShows;
         }

Lidarr/Lidarr

src/NzbDrone.Core/Notifications/Xbmc/XbmcJsonApiProxy.cs
@@ -59,7 +59,7 @@ namespace NzbDrone.Core.Notifications.Xbmc
 
         public List<KodiArtist> GetArtist(XbmcSettings settings)
         {
-            var response = ProcessRequest(settings, "AudioLibrary.GetArtists", new[] { "properties", "musicbrainzartistid" });
+            var response = ProcessRequest(settings, "AudioLibrary.GetArtists", new List<string> { "properties", "musicbrainzartistid" });
 
             return Json.Deserialize<ArtistResponse>(response).Result.Artists;
         }