Sonarr/Sonarr

Fixed: Set SameSite=Strict for SonarrAuth cookie
Closes #4365

src/Sonarr.Http/Authentication/EnableAuthInNancy.cs
@@ -4,7 +4,6 @@ using Nancy;
 using Nancy.Authentication.Basic;
 using Nancy.Authentication.Forms;
 using Nancy.Bootstrapper;
-using Nancy.Cookies;
 using Nancy.Cryptography;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Authentication;
@@ -120,7 +119,7 @@ namespace Sonarr.Http.Authentication
 
                 if (FormsAuthentication.DecryptAndValidateAuthenticationCookie(formsAuthCookieValue, FormsAuthConfig).IsNotNullOrWhiteSpace())
                 {
-                    var formsAuthCookie = new NancyCookie(formsAuthCookieName, formsAuthCookieValue, true, false, DateTime.UtcNow.AddDays(7))
+                    var formsAuthCookie = new SonarrNancyCookie(formsAuthCookieName, formsAuthCookieValue, true, false, DateTime.UtcNow.AddDays(7))
                     {
                         Path = GetCookiePath()
                     };

Lidarr/Lidarr

Closes #2060
(cherry picked from commit 675c72f02e7565a937b40c23ec27df6d86f95dc3)

src/Lidarr.Http/Authentication/EnableAuthInNancy.cs
@@ -6,7 +6,6 @@ using Nancy;
 using Nancy.Authentication.Basic;
 using Nancy.Authentication.Forms;
 using Nancy.Bootstrapper;
-using Nancy.Cookies;
 using Nancy.Cryptography;
 using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Extensions;
@@ -118,7 +117,7 @@ namespace Lidarr.Http.Authentication
 
                 if (FormsAuthentication.DecryptAndValidateAuthenticationCookie(formsAuthCookieValue, _formsAuthConfig).IsNotNullOrWhiteSpace())
                 {
-                    var formsAuthCookie = new NancyCookie(formsAuthCookieName, formsAuthCookieValue, true, false, DateTime.UtcNow.AddDays(7))
+                    var formsAuthCookie = new LidarrNancyCookie(formsAuthCookieName, formsAuthCookieValue, true, false, DateTime.UtcNow.AddDays(7))
                     {
                         Path = GetCookiePath()
                     };