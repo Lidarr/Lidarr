14e54969a7dd4f0b30fe17fd8a36510abda2c20e
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2021-05-06 00:30:56 +0100

    New: MusicBrainz Series use as import list

---
+++ src/NzbDrone.Core/ImportLists/MusicBrainzSeries/MusicBrainzSeriesRequestGenerator.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using NzbDrone.Core.MetadataSource;
+
+namespace NzbDrone.Core.ImportLists.MusicBrainzSeries
+{
+    public class MusicBrainzSeriesRequestGenerator : IImportListRequestGenerator
+    {
+        public MusicBrainzSeriesSettings Settings { get; set; }
+
+        private readonly IMetadataRequestBuilder _requestBulder;
+
+        public MusicBrainzSeriesRequestGenerator(IMetadataRequestBuilder requestBuilder)
+        {
+            _requestBulder = requestBuilder;
+        }
+
+        public virtual ImportListPageableRequestChain GetListItems()
+        {
+            var pageableRequests = new ImportListPageableRequestChain();
+
+            pageableRequests.Add(GetPagedRequests());
+
+            return pageableRequests;
+        }
+
+        private IEnumerable<ImportListRequest> GetPagedRequests()
+        {
+            var request = _requestBulder.GetRequestBuilder()
+                                        .Create()
+                                        .SetSegment("route", "series/" + Settings.SeriesId)
+                                        .Build();
+
+            yield return new ImportListRequest(request);
+        }
+    }
+}
---
+++ src/NzbDrone.Core/ImportLists/MusicBrainzSeries/MusicBrainzSeriesSettings.cs
@@ -0,0 +1,33 @@
+using FluentValidation;
+using NzbDrone.Core.Annotations;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.ImportLists.MusicBrainzSeries
+{
+    public class MusicBrainzSeriesSettingsValidator : AbstractValidator<MusicBrainzSeriesSettings>
+    {
+        public MusicBrainzSeriesSettingsValidator()
+        {
+        }
+    }
+
+    public class MusicBrainzSeriesSettings : IImportListSettings
+    {
+        private static readonly MusicBrainzSeriesSettingsValidator Validator = new MusicBrainzSeriesSettingsValidator();
+
+        public MusicBrainzSeriesSettings()
+        {
+            BaseUrl = "";
+        }
+
+        public string BaseUrl { get; set; }
+
+        [FieldDefinition(0, Label = "Series Id", HelpText = "The GUID at the end of the MusicBrainz URL (e.g. 4b5f2897-9b05-4799-b895-6620e27143e7)")]
+        public string SeriesId { get; set; }
+
+        public NzbDroneValidationResult Validate()
+        {
+            return new NzbDroneValidationResult(Validator.Validate(this));
+        }
+    }
+}
