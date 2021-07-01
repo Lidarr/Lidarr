68abb5fe05898fa84312326dd6f1be2856c7fdcf
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-06-30 22:07:32 -0500

    fixup radarr ref

--- src/NzbDrone.Core/Notifications/Email/Email.cs
+++ src/NzbDrone.Core/Notifications/Email/Email.cs
@@ -67,7 +67,7 @@ namespace NzbDrone.Core.Notifications.Email
 
             try
             {
-                SendEmail(settings, "Radarr - Test Notification", body);
+                SendEmail(settings, "Lidarr - Test Notification", body);
             }
             catch (Exception ex)
             {
