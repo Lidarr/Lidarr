Radarr/Radarr

Fixed: Notifiarr Health Issue Level
ref Sonarr 5938c38bc3a76d1f1e105fb54d5d7f59aa207278

src/NzbDrone.Core/Notifications/Notifiarr/Notifiarr.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using FluentValidation.Results;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.HealthCheck;
 
 namespace NzbDrone.Core.Notifications.Notifiarr
 {
@@ -139,7 +140,7 @@ namespace NzbDrone.Core.Notifications.Notifiarr
             var variables = new StringDictionary();
 
             variables.Add("Radarr_EventType", "HealthIssue");
-            variables.Add("Radarr_Health_Issue_Level", nameof(healthCheck.Type));
+            variables.Add("Radarr_Health_Issue_Level", Enum.GetName(typeof(HealthCheckResult), healthCheck.Type));
             variables.Add("Radarr_Health_Issue_Message", healthCheck.Message);
             variables.Add("Radarr_Health_Issue_Type", healthCheck.Source.Name);
             variables.Add("Radarr_Health_Issue_Wiki", healthCheck.WikiUrl.ToString() ?? string.Empty);

Lidarr/Lidarr

(cherry picked from commit 6f0a2de5053ccb859b4ffbe1e84b4f3e0f7ef9c9)

src/NzbDrone.Core/Notifications/Notifiarr/Notifiarr.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using FluentValidation.Results;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.HealthCheck;
 
 namespace NzbDrone.Core.Notifications.Notifiarr
 {
@@ -87,7 +88,7 @@ namespace NzbDrone.Core.Notifications.Notifiarr
             var variables = new StringDictionary();
 
             variables.Add("Lidarr_EventType", "HealthIssue");
-            variables.Add("Lidarr_Health_Issue_Level", nameof(healthCheck.Type));
+            variables.Add("Lidarr_Health_Issue_Level", Enum.GetName(typeof(HealthCheckResult), healthCheck.Type));
             variables.Add("Lidarr_Health_Issue_Message", healthCheck.Message);
             variables.Add("Lidarr_Health_Issue_Type", healthCheck.Source.Name);
             variables.Add("Lidarr_Health_Issue_Wiki", healthCheck.WikiUrl.ToString() ?? string.Empty);