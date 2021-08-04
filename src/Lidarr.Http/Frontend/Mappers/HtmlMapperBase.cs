bdc63b6785e9b6ec49071c79906b78521f931118
ta264 <ta264@users.noreply.github.com>
2021-08-04 21:42:41 +0100

    Tidy conversion to aspnetcore
    (cherry picked from commit 490f6e2e6aa3f220cc98f257a3ca3b2bea48fb80)
    (cherry picked from commit 8f3f90d4078d9d072d8ad4ccc3be35963b7435d6)

--- src/Lidarr.Http/Frontend/Mappers/HtmlMapperBase.cs
+++ src/Lidarr.Http/Frontend/Mappers/HtmlMapperBase.cs
@@ -39,15 +39,6 @@ namespace Lidarr.Http.Frontend.Mappers
             return stream;
         }
 
-        /*
-        public override IActionResult GetResponse(string resourceUrl)
-        {
-            var response = base.GetResponse(resourceUrl);
-            response.Headers["X-UA-Compatible"] = "IE=edge";
-
-            return response;
-        }*/
-
         protected string GetHtmlText()
         {
             if (RuntimeInfo.IsProduction && _generatedContent != null)
