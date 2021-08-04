d3bc6ff90d54a9cb625f9c59ab85812192e0df88
ta264 <ta264@users.noreply.github.com>
2021-08-04 21:42:41 +0100

    Fixed: UI not updating on upgrade
    (cherry picked from commit d566c1efd42f9a94c524db311e8fa99bc6e0323f)
    (cherry picked from commit 4b0586bd3d1cca4682dee53cc5af5ef1fa66978e)

--- src/Lidarr.Http/Frontend/Mappers/IMapHttpRequestsToDisk.cs
+++ src/Lidarr.Http/Frontend/Mappers/IMapHttpRequestsToDisk.cs
@@ -6,6 +6,6 @@ namespace Lidarr.Http.Frontend.Mappers
     {
         string Map(string resourceUrl);
         bool CanHandle(string resourceUrl);
-        IActionResult GetResponse(string resourceUrl);
+        FileStreamResult GetResponse(string resourceUrl);
     }
 }
