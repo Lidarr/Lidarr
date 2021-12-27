5ce376d314949511a35dfe11921cf337859d7815
Marty Zalega <marty@zalega.me>
2021-12-27 18:31:59 +1000

    Don't lowercase UrlBase in ConfigFileProvider
    UrlBase should honour the case it is given.
    
    (cherry picked from commit e1de523c89f7649e64f520b090bbdb2f56cc4b85)

--- src/NzbDrone.Core/Configuration/ConfigFileProvider.cs
+++ src/NzbDrone.Core/Configuration/ConfigFileProvider.cs
@@ -221,7 +221,7 @@ namespace NzbDrone.Core.Configuration
                     return urlBase;
                 }
 
-                return "/" + urlBase.Trim('/').ToLower();
+                return "/" + urlBase;
             }
         }
 
