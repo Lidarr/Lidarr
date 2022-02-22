Sonarr/Sonarr

New: Link indexer to specific download client
Closes #1215
Co-authored-by: Qstick <qstick@gmail.com>

src/NzbDrone.Core/Download/DownloadService.cs
@@ -52,7 +52,7 @@ namespace NzbDrone.Core.Download
             Ensure.That(remoteEpisode.Episodes, () => remoteEpisode.Episodes).HasItems();
 
             var downloadTitle = remoteEpisode.Release.Title;
-            var downloadClient = _downloadClientProvider.GetDownloadClient(remoteEpisode.Release.DownloadProtocol);
+            var downloadClient = _downloadClientProvider.GetDownloadClient(remoteEpisode.Release.DownloadProtocol, remoteEpisode.Release.IndexerId);
 
             if (downloadClient == null)
             {

Lidarr/Lidarr

(cherry picked from commit 13aaa20f1bf1448fa804738804205cb16f0d91f9)

src/NzbDrone.Core/Download/DownloadService.cs
@@ -51,7 +51,7 @@ namespace NzbDrone.Core.Download
             Ensure.That(remoteAlbum.Albums, () => remoteAlbum.Albums).HasItems();
 
             var downloadTitle = remoteAlbum.Release.Title;
-            var downloadClient = _downloadClientProvider.GetDownloadClient(remoteAlbum.Release.DownloadProtocol);
+            var downloadClient = _downloadClientProvider.GetDownloadClient(remoteAlbum.Release.DownloadProtocol, remoteAlbum.Release.IndexerId);
 
             if (downloadClient == null)
             {