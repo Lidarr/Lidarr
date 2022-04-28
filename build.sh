cec8ced375aeae7b7f7bfb29c95ab272c4d86457
ta264 <ta264@users.noreply.github.com>
2022-04-28 21:23:02 +0100

    Allow build script to produce packages on non-windows OSes
    [common]

--- build.sh
+++ build.sh
@@ -206,7 +206,10 @@ PackageWindows()
     local folder=$artifactsFolder/$runtime/$framework/Lidarr
     
     PackageFiles "$folder" "$framework" "$runtime"
-    cp -r $outputFolder/$framework-windows/$runtime/publish/* $folder
+
+    if [ "$os" = "windows" ]; then
+        cp -r $outputFolder/$framework-windows/$runtime/publish/* $folder
+    fi
 
     echo "Removing Lidarr.Mono"
     rm -f $folder/Lidarr.Mono.*
