Sonarr/Sonarr

Fixed: Custom Script Health Issue Level

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -189,7 +189,7 @@ namespace NzbDrone.Core.Notifications.CustomScript
             var environmentVariables = new StringDictionary();
 
             environmentVariables.Add("Sonarr_EventType", "HealthIssue");
-            environmentVariables.Add("Sonarr_Health_Issue_Level", nameof(healthCheck.Type));
+            environmentVariables.Add("Sonarr_Health_Issue_Level", Enum.GetName(typeof(HealthCheckResult), healthCheck.Type));
             environmentVariables.Add("Sonarr_Health_Issue_Message", healthCheck.Message);
             environmentVariables.Add("Sonarr_Health_Issue_Type", healthCheck.Source.Name);
             environmentVariables.Add("Sonarr_Health_Issue_Wiki", healthCheck.WikiUrl.ToString() ?? string.Empty);

Lidarr/Lidarr

(cherry picked from commit 5938c38bc3a76d1f1e105fb54d5d7f59aa207278)

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -149,7 +149,7 @@ namespace NzbDrone.Core.Notifications.CustomScript
             var environmentVariables = new StringDictionary();
 
             environmentVariables.Add("Lidarr_EventType", "HealthIssue");
-            environmentVariables.Add("Lidarr_Health_Issue_Level", nameof(healthCheck.Type));
+            environmentVariables.Add("Lidarr_Health_Issue_Level", Enum.GetName(typeof(HealthCheckResult), healthCheck.Type));
             environmentVariables.Add("Lidarr_Health_Issue_Message", healthCheck.Message);
             environmentVariables.Add("Lidarr_Health_Issue_Type", healthCheck.Source.Name);
             environmentVariables.Add("Lidarr_Health_Issue_Wiki", healthCheck.WikiUrl.ToString() ?? string.Empty);