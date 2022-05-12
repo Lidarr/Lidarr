1553a8f37bfe77c7d09a4f8444a3cf21a8d5dbc0
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2022-05-12 17:10:10 +0100

    New: Set Instance Name

--- src/NzbDrone.Core/Validation/RuleBuilderExtensions.cs
+++ src/NzbDrone.Core/Validation/RuleBuilderExtensions.cs
@@ -61,5 +61,11 @@ namespace NzbDrone.Core.Validation
         {
             return ruleBuilder.WithState(v => NzbDroneValidationState.Warning);
         }
+
+        public static IRuleBuilderOptions<T, string> ContainsLidarr<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            ruleBuilder.SetValidator(new NotEmptyValidator(null));
+            return ruleBuilder.SetValidator(new RegularExpressionValidator("lidarr", RegexOptions.IgnoreCase)).WithMessage("Must contain lidarr");
+        }
     }
 }
