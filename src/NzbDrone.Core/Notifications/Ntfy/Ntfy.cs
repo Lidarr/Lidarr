Sonarr/Sonarr

Added: Ntfy provider for notifications

src/NzbDrone.Core/Notifications/Ntfy/Ntfy.cs
@@ -1,0 +1,60 @@
+using System.Collections.Generic;
+
+using FluentValidation.Results;
+using NzbDrone.Common.Extensions;
+
+namespace NzbDrone.Core.Notifications.Ntfy
+{
+    public class Ntfy : NotificationBase<NtfySettings>
+    {
+        private readonly INtfyProxy _proxy;
+
+        public Ntfy(INtfyProxy proxy)
+        {
+            _proxy = proxy;
+        }
+
+        public override string Name => "ntfy.sh";
+
+        public override string Link => "https://ntfy.sh/";
+
+        public override void OnGrab(GrabMessage grabMessage)
+        {
+            _proxy.SendNotification(EPISODE_GRABBED_TITLE_BRANDED, grabMessage.Message, Settings);
+        }
+
+        public override void OnDownload(DownloadMessage message)
+        {
+            _proxy.SendNotification(EPISODE_DOWNLOADED_TITLE_BRANDED, message.Message, Settings);
+        }
+
+        public override void OnEpisodeFileDelete(EpisodeDeleteMessage deleteMessage)
+        {
+            _proxy.SendNotification(EPISODE_DELETED_TITLE, deleteMessage.Message, Settings);
+        }
+
+        public override void OnSeriesDelete(SeriesDeleteMessage deleteMessage)
+        {
+            _proxy.SendNotification(SERIES_DELETED_TITLE, deleteMessage.Message, Settings);
+        }
+
+        public override void OnHealthIssue(HealthCheck.HealthCheck healthCheck)
+        {
+            _proxy.SendNotification(HEALTH_ISSUE_TITLE_BRANDED, healthCheck.Message, Settings);
+        }
+
+        public override void OnApplicationUpdate(ApplicationUpdateMessage updateMessage)
+        {
+            _proxy.SendNotification(APPLICATION_UPDATE_TITLE_BRANDED, updateMessage.Message, Settings);
+        }
+
+        public override ValidationResult Test()
+        {
+            var failures = new List<ValidationFailure>();
+
+            failures.AddIfNotNull(_proxy.Test(Settings));
+
+            return new ValidationResult(failures);
+        }
+    }
+}

src/NzbDrone.Core/Notifications/Ntfy/NtfyProxy.cs
@@ -1,0 +1,138 @@
+using System;
+using System.Linq;
+using System.Net;
+
+using FluentValidation.Results;
+using NLog;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Common.Http;
+
+namespace NzbDrone.Core.Notifications.Ntfy
+{
+    public interface INtfyProxy
+    {
+        void SendNotification(string title, string message, NtfySettings settings);
+
+        ValidationFailure Test(NtfySettings settings);
+    }
+
+    public class NtfyProxy : INtfyProxy
+    {
+        private const string DEFAULT_PUSH_URL = "https://ntfy.sh";
+
+        private readonly IHttpClient _httpClient;
+
+        private readonly Logger _logger;
+
+        public NtfyProxy(IHttpClient httpClient, Logger logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+
+        public void SendNotification(string title, string message, NtfySettings settings)
+        {
+            var error = false;
+
+            var serverUrl = settings.ServerUrl.IsNullOrWhiteSpace() ? NtfyProxy.DEFAULT_PUSH_URL : settings.ServerUrl;
+
+            foreach (var topic in settings.Topics)
+            {
+                var request = BuildTopicRequest(serverUrl, topic);
+
+                try
+                {
+                    SendNotification(title, message, request, settings);
+                }
+                catch (NtfyException ex)
+                {
+                    _logger.Error(ex, "Unable to send test message to {0}", topic);
+                    error = true;
+                }
+            }
+
+            if (error)
+            {
+                throw new NtfyException("Unable to send Ntfy notifications to all topics");
+            }
+        }
+
+        private HttpRequestBuilder BuildTopicRequest(string serverUrl, string topic)
+        {
+            var trimServerUrl = serverUrl.TrimEnd('/');
+
+            var requestBuilder = new HttpRequestBuilder($"{trimServerUrl}/{topic}").Post();
+
+            return requestBuilder;
+        }
+
+        public ValidationFailure Test(NtfySettings settings)
+        {
+            try
+            {
+                const string title = "Radarr - Test Notification";
+
+                const string body = "This is a test message from Radarr";
+
+                SendNotification(title, body, settings);
+            }
+            catch (HttpException ex)
+            {
+                if (ex.Response.StatusCode == HttpStatusCode.Unauthorized || ex.Response.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    _logger.Error(ex, "Authorization is required");
+                    return new ValidationFailure("UserName", "Authorization is required");
+                }
+
+                _logger.Error(ex, "Unable to send test message");
+                return new ValidationFailure("ServerUrl", "Unable to send test message");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Unable to send test message");
+                return new ValidationFailure("", "Unable to send test message");
+            }
+
+            return null;
+        }
+
+        private void SendNotification(string title, string message, HttpRequestBuilder requestBuilder, NtfySettings settings)
+        {
+            try
+            {
+                requestBuilder.Headers.Add("X-Title", title);
+                requestBuilder.Headers.Add("X-Message", message);
+                requestBuilder.Headers.Add("X-Priority", settings.Priority.ToString());
+
+                if (settings.Tags.Any())
+                {
+                    requestBuilder.Headers.Add("X-Tags", settings.Tags.Join(","));
+                }
+
+                if (!settings.ClickUrl.IsNullOrWhiteSpace())
+                {
+                    requestBuilder.Headers.Add("X-Click", settings.ClickUrl);
+                }
+
+                var request = requestBuilder.Build();
+
+                if (!settings.UserName.IsNullOrWhiteSpace() && !settings.Password.IsNullOrWhiteSpace())
+                {
+                    request.Credentials = new BasicNetworkCredential(settings.UserName, settings.Password);
+                }
+
+                _httpClient.Execute(request);
+            }
+            catch (HttpException ex)
+            {
+                if (ex.Response.StatusCode == HttpStatusCode.Unauthorized || ex.Response.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    _logger.Error(ex, "Authorization is required");
+                    throw;
+                }
+
+                throw new NtfyException("Unable to send text message: {0}", ex, ex.Message);
+            }
+        }
+    }
+}

Lidarr/Lidarr

src/NzbDrone.Core/Notifications/Ntfy/Ntfy.cs
@@ -1,0 +1,60 @@
+using System.Collections.Generic;
+
+using FluentValidation.Results;
+using NzbDrone.Common.Extensions;
+
+namespace NzbDrone.Core.Notifications.Ntfy
+{
+    public class Ntfy : NotificationBase<NtfySettings>
+    {
+        private readonly INtfyProxy _proxy;
+
+        public Ntfy(INtfyProxy proxy)
+        {
+            _proxy = proxy;
+        }
+
+        public override string Name => "ntfy.sh";
+
+        public override string Link => "https://ntfy.sh/";
+
+        public override void OnGrab(GrabMessage grabMessage)
+        {
+            _proxy.SendNotification(ALBUM_GRABBED_TITLE_BRANDED, grabMessage.Message, Settings);
+        }
+
+        public override void OnReleaseImport(AlbumDownloadMessage message)
+        {
+            _proxy.SendNotification(ALBUM_DOWNLOADED_TITLE_BRANDED, message.Message, Settings);
+        }
+
+        public override void OnHealthIssue(HealthCheck.HealthCheck healthCheck)
+        {
+            _proxy.SendNotification(HEALTH_ISSUE_TITLE_BRANDED, healthCheck.Message, Settings);
+        }
+
+        public override void OnDownloadFailure(DownloadFailedMessage message)
+        {
+            _proxy.SendNotification(DOWNLOAD_FAILURE_TITLE_BRANDED, message.Message, Settings);
+        }
+
+        public override void OnImportFailure(AlbumDownloadMessage message)
+        {
+            _proxy.SendNotification(IMPORT_FAILURE_TITLE_BRANDED, message.Message, Settings);
+        }
+
+        public override void OnApplicationUpdate(ApplicationUpdateMessage updateMessage)
+        {
+            _proxy.SendNotification(APPLICATION_UPDATE_TITLE_BRANDED, updateMessage.Message, Settings);
+        }
+
+        public override ValidationResult Test()
+        {
+            var failures = new List<ValidationFailure>();
+
+            failures.AddIfNotNull(_proxy.Test(Settings));
+
+            return new ValidationResult(failures);
+        }
+    }
+}

src/NzbDrone.Core/Notifications/Ntfy/NtfyProxy.cs
@@ -1,0 +1,138 @@
+using System;
+using System.Linq;
+using System.Net;
+
+using FluentValidation.Results;
+using NLog;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Common.Http;
+
+namespace NzbDrone.Core.Notifications.Ntfy
+{
+    public interface INtfyProxy
+    {
+        void SendNotification(string title, string message, NtfySettings settings);
+
+        ValidationFailure Test(NtfySettings settings);
+    }
+
+    public class NtfyProxy : INtfyProxy
+    {
+        private const string DEFAULT_PUSH_URL = "https://ntfy.sh";
+
+        private readonly IHttpClient _httpClient;
+
+        private readonly Logger _logger;
+
+        public NtfyProxy(IHttpClient httpClient, Logger logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+
+        public void SendNotification(string title, string message, NtfySettings settings)
+        {
+            var error = false;
+
+            var serverUrl = settings.ServerUrl.IsNullOrWhiteSpace() ? NtfyProxy.DEFAULT_PUSH_URL : settings.ServerUrl;
+
+            foreach (var topic in settings.Topics)
+            {
+                var request = BuildTopicRequest(serverUrl, topic);
+
+                try
+                {
+                    SendNotification(title, message, request, settings);
+                }
+                catch (NtfyException ex)
+                {
+                    _logger.Error(ex, "Unable to send test message to {0}", topic);
+                    error = true;
+                }
+            }
+
+            if (error)
+            {
+                throw new NtfyException("Unable to send Ntfy notifications to all topics");
+            }
+        }
+
+        private HttpRequestBuilder BuildTopicRequest(string serverUrl, string topic)
+        {
+            var trimServerUrl = serverUrl.TrimEnd('/');
+
+            var requestBuilder = new HttpRequestBuilder($"{trimServerUrl}/{topic}").Post();
+
+            return requestBuilder;
+        }
+
+        public ValidationFailure Test(NtfySettings settings)
+        {
+            try
+            {
+                const string title = "Lidarr - Test Notification";
+
+                const string body = "This is a test message from Lidarr";
+
+                SendNotification(title, body, settings);
+            }
+            catch (HttpException ex)
+            {
+                if (ex.Response.StatusCode == HttpStatusCode.Unauthorized || ex.Response.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    _logger.Error(ex, "Authorization is required");
+                    return new ValidationFailure("UserName", "Authorization is required");
+                }
+
+                _logger.Error(ex, "Unable to send test message");
+                return new ValidationFailure("ServerUrl", "Unable to send test message");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Unable to send test message");
+                return new ValidationFailure("", "Unable to send test message");
+            }
+
+            return null;
+        }
+
+        private void SendNotification(string title, string message, HttpRequestBuilder requestBuilder, NtfySettings settings)
+        {
+            try
+            {
+                requestBuilder.Headers.Add("X-Title", title);
+                requestBuilder.Headers.Add("X-Message", message);
+                requestBuilder.Headers.Add("X-Priority", settings.Priority.ToString());
+
+                if (settings.Tags.Any())
+                {
+                    requestBuilder.Headers.Add("X-Tags", settings.Tags.Join(","));
+                }
+
+                if (!settings.ClickUrl.IsNullOrWhiteSpace())
+                {
+                    requestBuilder.Headers.Add("X-Click", settings.ClickUrl);
+                }
+
+                var request = requestBuilder.Build();
+
+                if (!settings.UserName.IsNullOrWhiteSpace() && !settings.Password.IsNullOrWhiteSpace())
+                {
+                    request.AddBasicAuthentication(settings.UserName, settings.Password);
+                }
+
+                _httpClient.Execute(request);
+            }
+            catch (HttpException ex)
+            {
+                if (ex.Response.StatusCode == HttpStatusCode.Unauthorized || ex.Response.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    _logger.Error(ex, "Authorization is required");
+                    throw;
+                }
+
+                throw new NtfyException("Unable to send text message: {0}", ex, ex.Message);
+            }
+        }
+    }
+}