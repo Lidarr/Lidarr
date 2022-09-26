Radarr/Radarr

Added searchEngine support in Newznab/Torznab caps
Fixes #5849

Co-Authored-By: Taloth <Taloth@users.noreply.github.com>

src/NzbDrone.Core.Test/IndexerTests/NewznabTests/NewznabCapabilitiesProviderFixture.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Xml;
 using FluentAssertions;
 using Moq;
@@ -94,6 +94,29 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
             var result = Subject.GetCapabilities(_settings);
 
             result.Should().NotBeNull();
+        }
+
+        [Test]
+        public void should_use_default_searchengine_if_missing()
+        {
+            GivenCapsResponse(_caps);
+
+            var caps = Subject.GetCapabilities(_settings);
+
+            caps.TextSearchEngine.Should().Be("sphinx");
+            caps.MovieTextSearchEngine.Should().Be("sphinx");
+        }
+
+        [Test]
+        public void should_use_specified_searchengine()
+        {
+            GivenCapsResponse(_caps.Replace("<search ", "<search searchEngine=\"raw\" ")
+                                   .Replace("<movie-search ", "<movie-search searchEngine=\"raw2\" "));
+
+            var caps = Subject.GetCapabilities(_settings);
+
+            caps.TextSearchEngine.Should().Be("raw");
+            caps.MovieTextSearchEngine.Should().Be("raw2");
         }
     }
 }

src/NzbDrone.Core/Indexers/Newznab/NewznabCapabilities.cs
@@ -9,6 +9,8 @@ namespace NzbDrone.Core.Indexers.Newznab
         public string[] SupportedSearchParameters { get; set; }
         public string[] SupportedMovieSearchParameters { get; set; }
         public bool SupportsAggregateIdSearch { get; set; }
+        public string TextSearchEngine { get; set; }
+        public string MovieTextSearchEngine { get; set; }
         public List<NewznabCategory> Categories { get; set; }
 
         public NewznabCapabilities()
@@ -18,6 +20,8 @@ namespace NzbDrone.Core.Indexers.Newznab
             SupportedSearchParameters = new[] { "q" };
             SupportedMovieSearchParameters = new[] { "q", "imdbid", "imdbtitle", "imdbyear" };
             SupportsAggregateIdSearch = false;
+            TextSearchEngine = "sphinx"; // This should remain 'sphinx' for older newznab installs
+            MovieTextSearchEngine = "sphinx"; // This should remain 'sphinx' for older newznab installs
             Categories = new List<NewznabCategory>();
         }
     }

src/NzbDrone.Core/Indexers/Newznab/NewznabCapabilitiesProvider.cs
@@ -118,9 +118,14 @@ namespace NzbDrone.Core.Indexers.Newznab
                 {
                     capabilities.SupportedSearchParameters = null;
                 }
-                else if (xmlBasicSearch.Attribute("supportedParams") != null)
+                else
                 {
-                    capabilities.SupportedSearchParameters = xmlBasicSearch.Attribute("supportedParams").Value.Split(',');
+                    if (xmlBasicSearch.Attribute("supportedParams") != null)
+                    {
+                        capabilities.SupportedSearchParameters = xmlBasicSearch.Attribute("supportedParams").Value.Split(',');
+                    }
+
+                    capabilities.TextSearchEngine = xmlBasicSearch.Attribute("searchEngine")?.Value ?? capabilities.TextSearchEngine;
                 }
 
                 var xmlMovieSearch = xmlSearching.Element("movie-search");
@@ -128,10 +133,15 @@ namespace NzbDrone.Core.Indexers.Newznab
                 {
                     capabilities.SupportedMovieSearchParameters = null;
                 }
-                else if (xmlMovieSearch.Attribute("supportedParams") != null)
+                else
                 {
-                    capabilities.SupportedMovieSearchParameters = xmlMovieSearch.Attribute("supportedParams").Value.Split(',');
-                    capabilities.SupportsAggregateIdSearch = true;
+                    if (xmlMovieSearch.Attribute("supportedParams") != null)
+                    {
+                        capabilities.SupportedMovieSearchParameters = xmlMovieSearch.Attribute("supportedParams").Value.Split(',');
+                        capabilities.SupportsAggregateIdSearch = true;
+                    }
+
+                    capabilities.MovieTextSearchEngine = xmlMovieSearch.Attribute("searchEngine")?.Value ?? capabilities.MovieTextSearchEngine;
                 }
             }
 

src/NzbDrone.Core/Indexers/Newznab/NewznabRequestGenerator.cs
@@ -65,6 +65,26 @@ namespace NzbDrone.Core.Indexers.Newznab
             }
         }
 
+        private string TextSearchEngine
+        {
+            get
+            {
+                var capabilities = _capabilitiesProvider.GetCapabilities(Settings);
+
+                return capabilities.TextSearchEngine;
+            }
+        }
+
+        private string MovieTextSearchEngine
+        {
+            get
+            {
+                var capabilities = _capabilitiesProvider.GetCapabilities(Settings);
+
+                return capabilities.MovieTextSearchEngine;
+            }
+        }
+
         public virtual IndexerPageableRequestChain GetRecentRequests()
         {
             var pageableRequests = new IndexerPageableRequestChain();
@@ -135,7 +155,8 @@ namespace NzbDrone.Core.Indexers.Newznab
             if (SupportsSearch)
             {
                 chain.AddTier();
-                foreach (var queryTitle in searchCriteria.QueryTitles)
+                var queryTitles = TextSearchEngine == "raw" ? searchCriteria.SceneTitles : searchCriteria.QueryTitles;
+                foreach (var queryTitle in queryTitles)
                 {
                     var searchQuery = queryTitle;
 

Lidarr/Lidarr

(cherry picked from commit eb76dd5248988c8101d9414aef0215f01f81cf00)

src/NzbDrone.Core.Test/IndexerTests/NewznabTests/NewznabCapabilitiesProviderFixture.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Xml;
 using FluentAssertions;
 using Moq;
@@ -96,6 +96,29 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
             result.Should().NotBeNull();
 
             ExceptionVerification.ExpectedErrors(1);
+        }
+
+        [Test]
+        public void should_use_default_searchengine_if_missing()
+        {
+            GivenCapsResponse(_caps);
+
+            var caps = Subject.GetCapabilities(_settings);
+
+            caps.TextSearchEngine.Should().Be("sphinx");
+            caps.AudioTextSearchEngine.Should().Be("sphinx");
+        }
+
+        [Test]
+        public void should_use_specified_searchengine()
+        {
+            GivenCapsResponse(_caps.Replace("<search ", "<search searchEngine=\"raw\" ")
+                                   .Replace("<audio-search ", "<audio-search searchEngine=\"raw2\" "));
+
+            var caps = Subject.GetCapabilities(_settings);
+
+            caps.TextSearchEngine.Should().Be("raw");
+            caps.AudioTextSearchEngine.Should().Be("raw2");
         }
     }
 }

src/NzbDrone.Core/Indexers/Newznab/NewznabCapabilities.cs
@@ -10,6 +10,8 @@ namespace NzbDrone.Core.Indexers.Newznab
         public string[] SupportedTvSearchParameters { get; set; }
         public string[] SupportedAudioSearchParameters { get; set; }
         public bool SupportsAggregateIdSearch { get; set; }
+        public string TextSearchEngine { get; set; }
+        public string AudioTextSearchEngine { get; set; }
         public List<NewznabCategory> Categories { get; set; }
 
         public NewznabCapabilities()
@@ -20,6 +22,8 @@ namespace NzbDrone.Core.Indexers.Newznab
             SupportedTvSearchParameters = new[] { "q", "rid", "season", "ep" }; // This should remain 'rid' for older newznab installs.
             SupportedAudioSearchParameters = new[] { "q", "artist", "album" };
             SupportsAggregateIdSearch = false;
+            TextSearchEngine = "sphinx"; // This should remain 'sphinx' for older newznab installs
+            AudioTextSearchEngine = "sphinx"; // This should remain 'sphinx' for older newznab installs
             Categories = new List<NewznabCategory>();
         }
     }

src/NzbDrone.Core/Indexers/Newznab/NewznabCapabilitiesProvider.cs
@@ -118,9 +118,14 @@ namespace NzbDrone.Core.Indexers.Newznab
                 {
                     capabilities.SupportedSearchParameters = null;
                 }
-                else if (xmlBasicSearch.Attribute("supportedParams") != null)
+                else
                 {
-                    capabilities.SupportedSearchParameters = xmlBasicSearch.Attribute("supportedParams").Value.Split(',');
+                    if (xmlBasicSearch.Attribute("supportedParams") != null)
+                    {
+                        capabilities.SupportedSearchParameters = xmlBasicSearch.Attribute("supportedParams").Value.Split(',');
+                    }
+
+                    capabilities.TextSearchEngine = xmlBasicSearch.Attribute("searchEngine")?.Value ?? capabilities.TextSearchEngine;
                 }
 
                 var xmlTvSearch = xmlSearching.Element("tv-search");
@@ -139,9 +144,15 @@ namespace NzbDrone.Core.Indexers.Newznab
                 {
                     capabilities.SupportedAudioSearchParameters = null;
                 }
-                else if (xmlAudioSearch.Attribute("supportedParams") != null)
+                else
                 {
-                    capabilities.SupportedAudioSearchParameters = xmlAudioSearch.Attribute("supportedParams").Value.Split(',');
+                    if (xmlAudioSearch.Attribute("supportedParams") != null)
+                    {
+                        capabilities.SupportedAudioSearchParameters = xmlAudioSearch.Attribute("supportedParams").Value.Split(',');
+                        capabilities.SupportsAggregateIdSearch = true;
+                    }
+
+                    capabilities.AudioTextSearchEngine = xmlAudioSearch.Attribute("searchEngine")?.Value ?? capabilities.AudioTextSearchEngine;
                 }
             }
 

src/NzbDrone.Core/Indexers/Newznab/NewznabRequestGenerator.cs
@@ -45,6 +45,26 @@ namespace NzbDrone.Core.Indexers.Newznab
             }
         }
 
+        private string TextSearchEngine
+        {
+            get
+            {
+                var capabilities = _capabilitiesProvider.GetCapabilities(Settings);
+
+                return capabilities.TextSearchEngine;
+            }
+        }
+
+        private string AudioTextSearchEngine
+        {
+            get
+            {
+                var capabilities = _capabilitiesProvider.GetCapabilities(Settings);
+
+                return capabilities.AudioTextSearchEngine;
+            }
+        }
+
         public virtual IndexerPageableRequestChain GetRecentRequests()
         {
             var pageableRequests = new IndexerPageableRequestChain();
@@ -69,19 +89,25 @@ namespace NzbDrone.Core.Indexers.Newznab
 
             if (SupportsAudioSearch)
             {
+                var artistQuery = AudioTextSearchEngine == "raw" ? searchCriteria.ArtistQuery : searchCriteria.CleanArtistQuery;
+                var albumQuery = AudioTextSearchEngine == "raw" ? searchCriteria.AlbumQuery : searchCriteria.CleanAlbumQuery;
+
                 AddAudioPageableRequests(pageableRequests,
                     searchCriteria,
-                    NewsnabifyTitle($"&artist={searchCriteria.ArtistQuery}&album={searchCriteria.AlbumQuery}"));
+                    NewsnabifyTitle($"&artist={artistQuery}&album={albumQuery}"));
             }
 
             if (SupportsSearch)
             {
                 pageableRequests.AddTier();
 
+                var artistQuery = TextSearchEngine == "raw" ? searchCriteria.ArtistQuery : searchCriteria.CleanArtistQuery;
+                var albumQuery = TextSearchEngine == "raw" ? searchCriteria.AlbumQuery : searchCriteria.CleanAlbumQuery;
+
                 pageableRequests.Add(GetPagedRequests(MaxPages,
                     Settings.Categories,
                     "search",
-                    NewsnabifyTitle($"&q={searchCriteria.ArtistQuery}+{searchCriteria.AlbumQuery}")));
+                    NewsnabifyTitle($"&q={artistQuery}+{albumQuery}")));
             }
 
             return pageableRequests;
@@ -93,19 +119,22 @@ namespace NzbDrone.Core.Indexers.Newznab
 
             if (SupportsAudioSearch)
             {
+                var queryTitle = AudioTextSearchEngine == "raw" ? searchCriteria.ArtistQuery : searchCriteria.CleanArtistQuery;
+
                 AddAudioPageableRequests(pageableRequests,
                     searchCriteria,
-                    NewsnabifyTitle($"&artist={searchCriteria.ArtistQuery}"));
+                    NewsnabifyTitle($"&artist={queryTitle}"));
             }
 
             if (SupportsSearch)
             {
                 pageableRequests.AddTier();
+                var queryTitle = TextSearchEngine == "raw" ? searchCriteria.ArtistQuery : searchCriteria.CleanArtistQuery;
 
                 pageableRequests.Add(GetPagedRequests(MaxPages,
-                    Settings.Categories,
-                    "search",
-                    NewsnabifyTitle($"&q={searchCriteria.ArtistQuery}")));
+                        Settings.Categories,
+                        "search",
+                        NewsnabifyTitle($"&q={queryTitle}")));
             }
 
             return pageableRequests;