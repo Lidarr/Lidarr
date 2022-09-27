a215199c1fbf968634972d507b687f70aae4c21d
Qstick <qstick@gmail.com>
2022-09-26 20:02:46 -0500

    New: Send TrackFile in Webhook retag payload
    Fixes #2533

--- src/NzbDrone.Core/Notifications/Webhook/WebhookRetagPayload.cs
+++ src/NzbDrone.Core/Notifications/Webhook/WebhookRetagPayload.cs
@@ -3,5 +3,6 @@ namespace NzbDrone.Core.Notifications.Webhook
     public class WebhookRetagPayload : WebhookPayload
     {
         public WebhookArtist Artist { get; set; }
+        public WebhookTrackFile TrackFile { get; set; }
     }
 }
