221b892e2cd3f0602378ecb8572a1c4ac9d05877
Mark McDowall <mark@mcdowall.ca>
2022-12-12 22:44:08 -0800

    Fixed: Queue not showing items with conflicting titles
    (cherry picked from commit 789a8f53013a247cd195f864484089c27b5f3858)
    (cherry picked from commit 96b31e3c94f178cacabeb2bedd57fd4316e52380)

--- src/NzbDrone.Core/Music/MultipleArtistsFoundException.cs
+++ src/NzbDrone.Core/Music/MultipleArtistsFoundException.cs
@@ -1,27 +1,16 @@
+using System.Collections.Generic;
 using NzbDrone.Common.Exceptions;
 
 namespace NzbDrone.Core.Music
 {
     public class MultipleArtistsFoundException : NzbDroneException
     {
-        public MultipleArtistsFoundException(string message, params object[] args)
+        public List<Artist> Artists { get; }
+
+        public MultipleArtistsFoundException(List<Artist> artists, string message, params object[] args)
             : base(message, args)
         {
-        }
-
-        public MultipleArtistsFoundException(string message)
-            : base(message)
-        {
-        }
-
-        public MultipleArtistsFoundException(string message, System.Exception innerException)
-            : base(message, innerException)
-        {
-        }
-
-        protected MultipleArtistsFoundException(string message, System.Exception innerException, params object[] args)
-            : base(message, innerException, args)
-        {
+            Artists = artists;
         }
     }
 }
