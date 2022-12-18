097fa3ca5b9d2c73bb38fea04b9a310b7d943161
Qstick <qstick@gmail.com>
2022-12-18 10:18:40 -0600

    Fixed: Don't enforce minimum on single list fetch
    (cherry picked from commit 62354dfac83e36f2d6a8cf24fc96fa25392bc438)

--- src/NzbDrone.Core/ImportLists/FetchAndParseImportListService.cs
+++ src/NzbDrone.Core/ImportLists/FetchAndParseImportListService.cs
@@ -107,6 +107,8 @@ namespace NzbDrone.Core.ImportLists
 
                         result.AddRange(importListReports);
                     }
+
+                    _importListStatusService.UpdateListSyncStatus(importList.Definition.Id);
                 }
                 catch (Exception e)
                 {
