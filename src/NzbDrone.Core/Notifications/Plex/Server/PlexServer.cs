Sonarr/Sonarr

Fixed: Ping plex.tv to keep auth token active
Closes #5154

src/NzbDrone.Core/Notifications/Plex/Server/PlexServer.cs
@@ -64,6 +64,8 @@ namespace NzbDrone.Core.Notifications.Plex.Server
 
         private void UpdateIfEnabled(Series series)
         {
+            _plexTvService.Ping(Settings.AuthToken);
+
             if (Settings.UpdateLibrary)
             {
                 _logger.Debug("Scheduling library update for series {0} {1}", series.Id, series.Title);
@@ -77,7 +79,8 @@ namespace NzbDrone.Core.Notifications.Plex.Server
 
         public override void ProcessQueue()
         {
-            PlexUpdateQueue queue = _pendingSeriesCache.Find(Settings.Host);
+            var queue = _pendingSeriesCache.Find(Settings.Host);
+
             if (queue == null)
             {
                 return;
@@ -130,6 +133,8 @@ namespace NzbDrone.Core.Notifications.Plex.Server
 
         public override ValidationResult Test()
         {
+            _plexTvService.Ping(Settings.AuthToken);
+
             var failures = new List<ValidationFailure>();
 
             failures.AddIfNotNull(_plexServerService.Test(Settings));

Lidarr/Lidarr

Closes #2981
(cherry picked from commit 93bd8543b158c952b50e56d4339e4a3c699770b0)

src/NzbDrone.Core/Notifications/Plex/Server/PlexServer.cs
@@ -55,6 +55,8 @@ namespace NzbDrone.Core.Notifications.Plex.Server
 
         private void UpdateIfEnabled(Artist artist)
         {
+            _plexTvService.Ping(Settings.AuthToken);
+
             if (Settings.UpdateLibrary)
             {
                 _logger.Debug("Scheduling library update for artist {0} {1}", artist.Id, artist.Name);
@@ -68,7 +70,8 @@ namespace NzbDrone.Core.Notifications.Plex.Server
 
         public override void ProcessQueue()
         {
-            PlexUpdateQueue queue = _pendingArtistCache.Find(Settings.Host);
+            var queue = _pendingArtistCache.Find(Settings.Host);
+
             if (queue == null)
             {
                 return;
@@ -121,6 +124,8 @@ namespace NzbDrone.Core.Notifications.Plex.Server
 
         public override ValidationResult Test()
         {
+            _plexTvService.Ping(Settings.AuthToken);
+
             var failures = new List<ValidationFailure>();
 
             failures.AddIfNotNull(_plexServerService.Test(Settings));