9e1009bc851e5b8722f0f9152799f548c4eedb3b
Qstick <qstick@gmail.com>
2022-12-31 19:24:40 -0600

    Update DeploymentInfoProvider.cs

--- src/NzbDrone.Core/Configuration/DeploymentInfoProvider.cs
+++ src/NzbDrone.Core/Configuration/DeploymentInfoProvider.cs
@@ -34,7 +34,7 @@ namespace NzbDrone.Core.Configuration
             var releaseInfoPath = Path.Combine(bin, "release_info");
 
             PackageUpdateMechanism = UpdateMechanism.BuiltIn;
-            DefaultBranch = "aphrodite";
+            DefaultBranch = "master";
 
             if (Path.GetFileName(bin) == "bin" && diskProvider.FileExists(packageInfoPath))
             {
