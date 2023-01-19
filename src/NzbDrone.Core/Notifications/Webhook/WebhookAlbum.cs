3c2a74fe7f065373a7fc2bb65c7396af406ff35e
Qstick <qstick@gmail.com>
2023-01-18 18:30:34 -0600

    New: Additional Artist and Album properties on Webhook base

--- src/NzbDrone.Core/Notifications/Webhook/WebhookAlbum.cs
+++ src/NzbDrone.Core/Notifications/Webhook/WebhookAlbum.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using NzbDrone.Core.Music;
 
 namespace NzbDrone.Core.Notifications.Webhook
@@ -12,17 +14,24 @@ namespace NzbDrone.Core.Notifications.Webhook
         public WebhookAlbum(Album album)
         {
             Id = album.Id;
+            MBId = album.ForeignAlbumId;
             Title = album.Title;
+            Disambiguation = album.Disambiguation;
+            Overview = album.Overview;
+            AlbumType = album.AlbumType;
+            SecondaryAlbumTypes = album.SecondaryTypes.Select(x => x.Name).ToList();
+            Genres = album.Genres;
             ReleaseDate = album.ReleaseDate;
         }
 
         public int Id { get; set; }
+        public string MBId { get; set; }
         public string Title { get; set; }
+        public string Disambiguation { get; set; }
+        public string Overview { get; set; }
+        public string AlbumType { get; set; }
+        public List<string> SecondaryAlbumTypes { get; set; }
+        public List<string> Genres { get; set; }
         public DateTime? ReleaseDate { get; set; }
-
-        public string Quality { get; set; }
-        public int QualityVersion { get; set; }
-        public string ReleaseGroup { get; set; }
-        public string SceneName { get; set; }
     }
 }
--- src/NzbDrone.Core/Notifications/Webhook/WebhookArtist.cs
+++ src/NzbDrone.Core/Notifications/Webhook/WebhookArtist.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using NzbDrone.Core.Music;
 
 namespace NzbDrone.Core.Notifications.Webhook
@@ -6,9 +7,12 @@ namespace NzbDrone.Core.Notifications.Webhook
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Disambiguation { get; set; }
         public string Path { get; set; }
         public string MBId { get; set; }
         public string Type { get; set; }
+        public string Overview { get; set; }
+        public List<string> Genres { get; set; }
 
         public WebhookArtist()
         {
@@ -18,8 +22,11 @@ namespace NzbDrone.Core.Notifications.Webhook
         {
             Id = artist.Id;
             Name = artist.Name;
+            Disambiguation = artist.Metadata.Value.Disambiguation;
             Path = artist.Path;
             Type = artist.Metadata.Value.Type;
+            Overview = artist.Metadata.Value.Overview;
+            Genres = artist.Metadata.Value.Genres;
             MBId = artist.Metadata.Value.ForeignArtistId;
         }
     }
