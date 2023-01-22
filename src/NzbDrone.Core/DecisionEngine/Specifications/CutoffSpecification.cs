Qstick/Sonarr

New: Custom Formats
Co-Authored-By: ta264 <ta264@users.noreply.github.com>

src/NzbDrone.Core/DecisionEngine/Specifications/CutoffSpecification.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using NLog;
+using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.IndexerSearch.Definitions;
 using NzbDrone.Core.MediaFiles;
 using NzbDrone.Core.Parser.Model;
@@ -10,13 +11,13 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
     public class CutoffSpecification : IDecisionEngineSpecification
     {
         private readonly UpgradableSpecification _upgradableSpecification;
-        private readonly IEpisodeFilePreferredWordCalculator _episodeFilePreferredWordCalculator;
+        private readonly ICustomFormatCalculationService _formatService;
         private readonly Logger _logger;
 
-        public CutoffSpecification(UpgradableSpecification upgradableSpecification, IEpisodeFilePreferredWordCalculator episodeFilePreferredWordCalculator, Logger logger)
+        public CutoffSpecification(UpgradableSpecification upgradableSpecification, ICustomFormatCalculationService formatService, Logger logger)
         {
             _upgradableSpecification = upgradableSpecification;
-            _episodeFilePreferredWordCalculator = episodeFilePreferredWordCalculator;
+            _formatService = formatService;
             _logger = logger;
         }
 
@@ -38,13 +39,14 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
 
                 _logger.Debug("Comparing file quality and language with report. Existing file is {0} - {1}", file.Quality, file.Language);
 
+                var customFormats = _formatService.ParseCustomFormat(file);
+
                 if (!_upgradableSpecification.CutoffNotMet(qualityProfile,
                                                            languageProfile,
                                                            file.Quality,
                                                            file.Language,
-                                                           _episodeFilePreferredWordCalculator.Calculate(subject.Series, file),
-                                                           subject.ParsedEpisodeInfo.Quality,
-                                                           subject.PreferredWordScore))
+                                                           _formatService.ParseCustomFormat(file),
+                                                           subject.ParsedEpisodeInfo.Quality))
                 {
                     _logger.Debug("Cutoff already met, rejecting.");
 

Lidarr/Lidarr

src/NzbDrone.Core/DecisionEngine/Specifications/CutoffSpecification.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using NLog;
 using NzbDrone.Common.Cache;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.IndexerSearch.Definitions;
 using NzbDrone.Core.MediaFiles;
 using NzbDrone.Core.Music;
@@ -16,22 +17,22 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
         private readonly UpgradableSpecification _upgradableSpecification;
         private readonly IMediaFileService _mediaFileService;
         private readonly ITrackService _trackService;
+        private readonly ICustomFormatCalculationService _formatService;
         private readonly Logger _logger;
         private readonly ICached<bool> _missingFilesCache;
-        private readonly IPreferredWordService _preferredWordServiceCalculator;
 
         public CutoffSpecification(UpgradableSpecification upgradableSpecification,
                                    Logger logger,
                                    ICacheManager cacheManager,
                                    IMediaFileService mediaFileService,
-                                   IPreferredWordService preferredWordServiceCalculator,
+                                   ICustomFormatCalculationService formatService,
                                    ITrackService trackService)
         {
             _upgradableSpecification = upgradableSpecification;
             _mediaFileService = mediaFileService;
             _trackService = trackService;
             _missingFilesCache = cacheManager.GetCache<bool>(GetType());
-            _preferredWordServiceCalculator = preferredWordServiceCalculator;
+            _formatService = formatService;
             _logger = logger;
         }
 
@@ -56,11 +57,12 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
 
                     _logger.Debug("Comparing file quality with report. Existing files contain {0}", currentQualities.ConcatToString());
 
+                    var customFormats = _formatService.ParseCustomFormat(trackFiles[0]);
+
                     if (!_upgradableSpecification.CutoffNotMet(qualityProfile,
                                                                currentQualities,
-                                                               _preferredWordServiceCalculator.Calculate(subject.Artist, trackFiles[0].GetSceneOrFileName(), subject.Release.IndexerId),
-                                                               subject.ParsedAlbumInfo.Quality,
-                                                               subject.PreferredWordScore))
+                                                               customFormats,
+                                                               subject.ParsedAlbumInfo.Quality))
                     {
                         _logger.Debug("Cutoff already met by existing files, rejecting.");
 