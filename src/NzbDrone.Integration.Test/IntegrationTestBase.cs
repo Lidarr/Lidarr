Sonarr/Sonarr

Fixed cutoff unmet integration tests

src/NzbDrone.Integration.Test/IntegrationTestBase.cs
@@ -308,13 +308,25 @@ namespace NzbDrone.Integration.Test
             return result.EpisodeFile;
         }
 
-        public ProfileResource EnsureProfileCutoff(int profileId, Quality cutoff)
+        public ProfileResource EnsureProfileCutoff(int profileId, Quality cutoff, bool upgradeAllowed)
         {
+            var needsUpdate = false;
             var profile = Profiles.Get(profileId);
 
             if (profile.Cutoff != cutoff)
             {
                 profile.Cutoff = cutoff;
+                needsUpdate = true;
+            }
+
+            if (profile.UpgradeAllowed != upgradeAllowed)
+            {
+                profile.UpgradeAllowed = upgradeAllowed;
+                needsUpdate = true;
+            }
+
+            if (needsUpdate)
+            {
                 profile = Profiles.Put(profile);
             }
 

Lidarr/Lidarr

Closes #2822
(cherry picked from commit c1e5b7f642d03414f7c5587d4db377ef979f2067)

src/NzbDrone.Integration.Test/IntegrationTestBase.cs
@@ -331,14 +331,26 @@ namespace NzbDrone.Integration.Test
             }
         }
 
-        public QualityProfileResource EnsureProfileCutoff(int profileId, string cutoff)
+        public QualityProfileResource EnsureProfileCutoff(int profileId, string cutoff, bool upgradeAllowed)
         {
+            var needsUpdate = false;
             var profile = Profiles.Get(profileId);
             var cutoffItem = profile.Items.First(x => x.Name == cutoff);
 
             if (profile.Cutoff != cutoffItem.Id)
             {
                 profile.Cutoff = cutoffItem.Id;
+                needsUpdate = true;
+            }
+
+            if (profile.UpgradeAllowed != upgradeAllowed)
+            {
+                profile.UpgradeAllowed = upgradeAllowed;
+                needsUpdate = true;
+            }
+
+            if (needsUpdate)
+            {
                 profile = Profiles.Put(profile);
             }
 