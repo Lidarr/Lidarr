5f1394acfaf5eee0161e36819357dc8e4fa55c06
Qstick <qstick@gmail.com>
2023-01-26 21:07:51 -0600

    Fixup FileNameBuilder tests

--- src/NzbDrone.Core.Test/OrganizerTests/CleanFixture.cs
+++ src/NzbDrone.Core.Test/OrganizerTests/CleanFixture.cs
@@ -9,7 +9,7 @@ namespace NzbDrone.Core.Test.OrganizerTests
     public class CleanFixture : CoreTest
     {
         [TestCase("Law & Order: Criminal Intent - S10E07 - Icarus [HDTV-720p]",
-            "Law & Order- Criminal Intent - S10E07 - Icarus [HDTV-720p]")]
+            "Law & Order - Criminal Intent - S10E07 - Icarus [HDTV-720p]")]
         public void CleanFileName(string name, string expectedName)
         {
             FileNameBuilder.CleanFileName(name).Should().Be(expectedName);
