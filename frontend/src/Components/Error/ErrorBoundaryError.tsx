cabfae1f595811768e9511e47e87c2e050afbdb1
Mark McDowall <mark@mcdowall.ca>
2023-02-16 16:45:09 -0800

    Improved UI error messages (stack trace and version)
    (cherry picked from commit 37c355da51b654cea7309678c32a83a5cbe43d1f)
    
    Closes #3367

---
+++ frontend/src/Components/Error/ErrorBoundaryError.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import StackTrace from 'stacktrace-js';
+import translate from 'Utilities/String/translate';
+import styles from './ErrorBoundaryError.css';
+
+interface ErrorBoundaryErrorProps {
+  className: string;
+  messageClassName: string;
+  detailsClassName: string;
+  message: string;
+  error: Error;
+  info: {
+    componentStack: string;
+  };
+}
+
+function ErrorBoundaryError(props: ErrorBoundaryErrorProps) {
+  const {
+    className = styles.container,
+    messageClassName = styles.message,
+    detailsClassName = styles.details,
+    message = translate('ErrorLoadingContent'),
+    error,
+    info,
+  } = props;
+
+  const [detailedError, setDetailedError] = useState<
+    StackTrace.StackFrame[] | null
+  >(null);
+
+  useEffect(() => {
+    if (error) {
+      StackTrace.fromError(error).then((de) => {
+        setDetailedError(de);
+      });
+    } else {
+      setDetailedError(null);
+    }
+  }, [error, setDetailedError]);
+
+  return (
+    <div className={className}>
+      <div className={messageClassName}>{message}</div>
+
+      <div className={styles.imageContainer}>
+        <img
+          className={styles.image}
+          src={`${window.Lidarr.urlBase}/Content/Images/error.png`}
+        />
+      </div>
+
+      <details className={detailsClassName}>
+        {error ? <div>{error.message}</div> : null}
+
+        {detailedError ? (
+          detailedError.map((d, index) => {
+            return (
+              <div key={index}>
+                {`  at ${d.functionName} (${d.fileName}:${d.lineNumber}:${d.columnNumber})`}
+              </div>
+            );
+          })
+        ) : (
+          <div>{info.componentStack}</div>
+        )}
+
+        {<div className={styles.version}>Version: {window.Lidarr.version}</div>}
+      </details>
+    </div>
+  );
+}
+
+export default ErrorBoundaryError;
