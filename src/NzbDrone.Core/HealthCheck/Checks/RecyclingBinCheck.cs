ba4e3b404e3c25b1ab652d5abeb125233147639a
Mark McDowall <mark@mcdowall.ca>
2023-04-24 20:23:20 -0700

    New: Report health error if Recycling Bin folder is not writable
    (cherry picked from commit 8c50cd061e691914d9fcce119b9f838f1276950c)

---
+++ src/NzbDrone.Core/HealthCheck/Checks/RecyclingBinCheck.cs
@@ -0,0 +1,41 @@
+using NzbDrone.Common.Disk;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Localization;
+using NzbDrone.Core.MediaFiles.Events;
+
+namespace NzbDrone.Core.HealthCheck.Checks
+{
+    [CheckOn(typeof(TrackImportedEvent), CheckOnCondition.FailedOnly)]
+    [CheckOn(typeof(TrackImportFailedEvent), CheckOnCondition.SuccessfulOnly)]
+    public class RecyclingBinCheck : HealthCheckBase
+    {
+        private readonly IConfigService _configService;
+        private readonly IDiskProvider _diskProvider;
+        private readonly ILocalizationService _localizationService;
+
+        public RecyclingBinCheck(IConfigService configService, IDiskProvider diskProvider, ILocalizationService localizationService)
+        {
+            _configService = configService;
+            _diskProvider = diskProvider;
+            _localizationService = localizationService;
+        }
+
+        public override HealthCheck Check()
+        {
+            var recycleBin = _configService.RecycleBin;
+
+            if (recycleBin.IsNullOrWhiteSpace())
+            {
+                return new HealthCheck(GetType());
+            }
+
+            if (!_diskProvider.FolderWritable(recycleBin))
+            {
+                return new HealthCheck(GetType(), HealthCheckResult.Error, string.Format(_localizationService.GetLocalizedString("RecycleBinUnableToWriteHealthCheck"), recycleBin), "#cannot-write-recycle-bin");
+            }
+
+            return new HealthCheck(GetType());
+        }
+    }
+}
