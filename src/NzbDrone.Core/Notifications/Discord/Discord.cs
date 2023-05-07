Sonarr/Sonarr

New: Add indexer option for Discord on grab notifications

src/NzbDrone.Core/Notifications/Discord/Discord.cs
@@ -101,6 +101,10 @@ namespace NzbDrone.Core.Notifications.Discord
                         discordField.Name = "Links";
                         discordField.Value = GetLinksString(series);
                         break;
+                    case DiscordGrabFieldType.Indexer:
+                        discordField.Name = "Indexer";
+                        discordField.Value = message.Episode.Release.Indexer;
+                        break;
                 }
 
                 if (discordField.Name.IsNotNullOrWhiteSpace() && discordField.Value.IsNotNullOrWhiteSpace())

Lidarr/Lidarr

(cherry picked from commit 5d09b84b05ab70662830e910c8f5e98af7830d45)

src/NzbDrone.Core/Notifications/Discord/Discord.cs
@@ -101,6 +101,10 @@ namespace NzbDrone.Core.Notifications.Discord
                         discordField.Name = "Links";
                         discordField.Value = GetLinksString(artist);
                         break;
+                    case DiscordGrabFieldType.Indexer:
+                        discordField.Name = "Indexer";
+                        discordField.Value = message.RemoteAlbum.Release.Indexer;
+                        break;
                 }
 
                 if (discordField.Name.IsNotNullOrWhiteSpace() && discordField.Value.IsNotNullOrWhiteSpace())