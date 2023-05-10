Sonarr/Sonarr

Fixed: Automatic import of releases when file is not matched to series
Closes #4935

src/NzbDrone.Core/Download/CompletedDownloadService.cs
@@ -13,6 +13,7 @@ using NzbDrone.Core.MediaFiles;
 using NzbDrone.Core.MediaFiles.EpisodeImport;
 using NzbDrone.Core.Messaging.Events;
 using NzbDrone.Core.Parser;
+using NzbDrone.Core.Parser.Model;
 using NzbDrone.Core.Tv;
 
 namespace NzbDrone.Core.Download
@@ -97,9 +98,13 @@ namespace NzbDrone.Core.Download
                     return;
                 }
 
-                trackedDownload.Warn("Found matching series via grab history, but release title doesn't match series title. Automatic import is not possible.");
+                Enum.TryParse(historyItem.Data.GetValueOrDefault(EpisodeHistory.SERIES_MATCH_TYPE, SeriesMatchType.Unknown.ToString()), out SeriesMatchType seriesMatchType);
 
-                return;
+                if (seriesMatchType == SeriesMatchType.Id)
+                {
+                    trackedDownload.Warn("Found matching series via grab history, but release was matched to series by ID. Automatic import is not possible.");
+                    return;
+                }
             }
 
             trackedDownload.State = TrackedDownloadState.ImportPending;

src/NzbDrone.Core/Download/Pending/PendingRelease.cs
@@ -12,8 +12,14 @@ namespace NzbDrone.Core.Download.Pending
         public ParsedEpisodeInfo ParsedEpisodeInfo { get; set; }
         public ReleaseInfo Release { get; set; }
         public PendingReleaseReason Reason { get; set; }
+        public PendingReleaseAdditionalInfo AdditionalInfo { get; set; }
 
         //Not persisted
         public RemoteEpisode RemoteEpisode { get; set; }
+    }
+
+    public class PendingReleaseAdditionalInfo
+    {
+        public SeriesMatchType SeriesMatchType { get; set; }
     }
 }

src/NzbDrone.Core/Download/Pending/PendingReleaseService.cs
@@ -291,16 +291,15 @@ namespace NzbDrone.Core.Download.Pending
                 // Just in case the series was removed, but wasn't cleaned up yet (housekeeper will clean it up)
                 if (series == null) return null;
 
-
                 release.RemoteEpisode = new RemoteEpisode
                 {
                     Series = series,
+                    SeriesMatchType = release.AdditionalInfo?.SeriesMatchType ?? SeriesMatchType.Unknown,
                     ParsedEpisodeInfo = release.ParsedEpisodeInfo,
                     Release = release.Release
                 };
 
-                RemoteEpisode knownRemoteEpisode;
-                if (knownRemoteEpisodes != null && knownRemoteEpisodes.TryGetValue(release.Release.Title, out knownRemoteEpisode))
+                if (knownRemoteEpisodes != null && knownRemoteEpisodes.TryGetValue(release.Release.Title, out var knownRemoteEpisode))
                 {
                     release.RemoteEpisode.MappedSeasonNumber = knownRemoteEpisode.MappedSeasonNumber;
                     release.RemoteEpisode.Episodes = knownRemoteEpisode.Episodes;
@@ -333,7 +332,11 @@ namespace NzbDrone.Core.Download.Pending
                 Release = decision.RemoteEpisode.Release,
                 Title = decision.RemoteEpisode.Release.Title,
                 Added = DateTime.UtcNow,
-                Reason = reason
+                Reason = reason,
+                AdditionalInfo = new PendingReleaseAdditionalInfo
+                {
+                    SeriesMatchType = decision.RemoteEpisode.SeriesMatchType
+                }
             });
 
             _eventAggregator.PublishEvent(new PendingReleasesUpdatedEvent());

Lidarr/Lidarr

(cherry picked from commit e280897bc7f5c55d11714b0c25e7220477c18886)

src/NzbDrone.Core/Download/CompletedDownloadService.cs
@@ -14,6 +14,7 @@ using NzbDrone.Core.MediaFiles.TrackImport;
 using NzbDrone.Core.Messaging.Events;
 using NzbDrone.Core.Music;
 using NzbDrone.Core.Parser;
+using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.Download
 {
@@ -93,8 +94,13 @@ namespace NzbDrone.Core.Download
 
                 if (artist == null)
                 {
-                    trackedDownload.Warn("Artist name mismatch, automatic import is not possible.");
-                    return;
+                    Enum.TryParse(historyItem.Data.GetValueOrDefault(EntityHistory.ARTIST_MATCH_TYPE, ArtistMatchType.Unknown.ToString()), out ArtistMatchType artistMatchType);
+
+                    if (artistMatchType == ArtistMatchType.Id)
+                    {
+                        trackedDownload.Warn("Found matching artist via grab history, but release was matched to artist by ID. Automatic import is not possible.");
+                        return;
+                    }
                 }
             }
 

src/NzbDrone.Core/Download/Pending/PendingRelease.cs
@@ -12,8 +12,14 @@ namespace NzbDrone.Core.Download.Pending
         public ParsedAlbumInfo ParsedAlbumInfo { get; set; }
         public ReleaseInfo Release { get; set; }
         public PendingReleaseReason Reason { get; set; }
+        public PendingReleaseAdditionalInfo AdditionalInfo { get; set; }
 
         // Not persisted
         public RemoteAlbum RemoteAlbum { get; set; }
+    }
+
+    public class PendingReleaseAdditionalInfo
+    {
+        public ArtistMatchType ArtistMatchType { get; set; }
     }
 }

src/NzbDrone.Core/Download/Pending/PendingReleaseService.cs
@@ -300,8 +300,7 @@ namespace NzbDrone.Core.Download.Pending
 
                 List<Album> albums;
 
-                RemoteAlbum knownRemoteAlbum;
-                if (knownRemoteAlbums != null && knownRemoteAlbums.TryGetValue(release.Release.Title, out knownRemoteAlbum))
+                if (knownRemoteAlbums != null && knownRemoteAlbums.TryGetValue(release.Release.Title, out var knownRemoteAlbum))
                 {
                     albums = knownRemoteAlbum.Albums;
                 }
@@ -313,6 +312,7 @@ namespace NzbDrone.Core.Download.Pending
                 release.RemoteAlbum = new RemoteAlbum
                 {
                     Artist = artist,
+                    ArtistMatchType = release.AdditionalInfo?.ArtistMatchType ?? ArtistMatchType.Unknown,
                     Albums = albums,
                     ParsedAlbumInfo = release.ParsedAlbumInfo,
                     Release = release.Release
@@ -336,7 +336,11 @@ namespace NzbDrone.Core.Download.Pending
                 Release = decision.RemoteAlbum.Release,
                 Title = decision.RemoteAlbum.Release.Title,
                 Added = DateTime.UtcNow,
-                Reason = reason
+                Reason = reason,
+                AdditionalInfo = new PendingReleaseAdditionalInfo
+                {
+                    ArtistMatchType = decision.RemoteAlbum.ArtistMatchType
+                }
             });
 
             _eventAggregator.PublishEvent(new PendingReleasesUpdatedEvent());