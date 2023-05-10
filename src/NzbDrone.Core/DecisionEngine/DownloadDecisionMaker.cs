Sonarr/Sonarr

New: Don't block imports when release was matched by ID if they were grabbed via interactive search
Closes #5043

src/NzbDrone.Core/DecisionEngine/DownloadDecisionMaker.cs
@@ -17,7 +17,7 @@ namespace NzbDrone.Core.DecisionEngine
 {
     public interface IMakeDownloadDecision
     {
-        List<DownloadDecision> GetRssDecision(List<ReleaseInfo> reports);
+        List<DownloadDecision> GetRssDecision(List<ReleaseInfo> reports, bool pushedRelease = false);
         List<DownloadDecision> GetSearchDecision(List<ReleaseInfo> reports, SearchCriteriaBase searchCriteriaBase);
     }
 
@@ -45,17 +45,17 @@ namespace NzbDrone.Core.DecisionEngine
             _logger = logger;
         }
 
-        public List<DownloadDecision> GetRssDecision(List<ReleaseInfo> reports)
+        public List<DownloadDecision> GetRssDecision(List<ReleaseInfo> reports, bool pushedRelease = false)
         {
             return GetDecisions(reports).ToList();
         }
 
         public List<DownloadDecision> GetSearchDecision(List<ReleaseInfo> reports, SearchCriteriaBase searchCriteriaBase)
         {
-            return GetDecisions(reports, searchCriteriaBase).ToList();
+            return GetDecisions(reports, false, searchCriteriaBase).ToList();
         }
 
-        private IEnumerable<DownloadDecision> GetDecisions(List<ReleaseInfo> reports, SearchCriteriaBase searchCriteria = null)
+        private IEnumerable<DownloadDecision> GetDecisions(List<ReleaseInfo> reports, bool pushedRelease = false, SearchCriteriaBase searchCriteria = null)
         {
             if (reports.Any())
             {
@@ -156,6 +156,26 @@ namespace NzbDrone.Core.DecisionEngine
 
                 if (decision != null)
                 {
+                    var source = pushedRelease ? ReleaseSourceType.ReleasePush : ReleaseSourceType.Rss;
+
+                    if (searchCriteria != null)
+                    {
+                        if (searchCriteria.InteractiveSearch)
+                        {
+                            source = ReleaseSourceType.InteractiveSearch;
+                        }
+                        else if (searchCriteria.UserInvokedSearch)
+                        {
+                            source = ReleaseSourceType.UserInvokedSearch;
+                        }
+                        else
+                        {
+                            source = ReleaseSourceType.Search;
+                        }
+                    }
+
+                    decision.RemoteEpisode.ReleaseSource = source;
+
                     if (decision.Rejections.Any())
                     {
                         _logger.Debug("Release rejected for the following reasons: {0}", string.Join(", ", decision.Rejections));

Lidarr/Lidarr

(cherry picked from commit bc2942c28d3c48e9eea61a68baf032ba181aef1e)

src/NzbDrone.Core/DecisionEngine/DownloadDecisionMaker.cs
@@ -16,7 +16,7 @@ namespace NzbDrone.Core.DecisionEngine
 {
     public interface IMakeDownloadDecision
     {
-        List<DownloadDecision> GetRssDecision(List<ReleaseInfo> reports);
+        List<DownloadDecision> GetRssDecision(List<ReleaseInfo> reports, bool pushedRelease = false);
         List<DownloadDecision> GetSearchDecision(List<ReleaseInfo> reports, SearchCriteriaBase searchCriteriaBase);
     }
 
@@ -41,17 +41,17 @@ namespace NzbDrone.Core.DecisionEngine
             _logger = logger;
         }
 
-        public List<DownloadDecision> GetRssDecision(List<ReleaseInfo> reports)
+        public List<DownloadDecision> GetRssDecision(List<ReleaseInfo> reports, bool pushedRelease = false)
         {
-            return GetAlbumDecisions(reports).ToList();
+            return GetAlbumDecisions(reports, pushedRelease).ToList();
         }
 
         public List<DownloadDecision> GetSearchDecision(List<ReleaseInfo> reports, SearchCriteriaBase searchCriteriaBase)
         {
-            return GetAlbumDecisions(reports, searchCriteriaBase).ToList();
+            return GetAlbumDecisions(reports, false, searchCriteriaBase).ToList();
         }
 
-        private IEnumerable<DownloadDecision> GetAlbumDecisions(List<ReleaseInfo> reports, SearchCriteriaBase searchCriteria = null)
+        private IEnumerable<DownloadDecision> GetAlbumDecisions(List<ReleaseInfo> reports, bool pushedRelease, SearchCriteriaBase searchCriteria = null)
         {
             if (reports.Any())
             {
@@ -169,6 +169,26 @@ namespace NzbDrone.Core.DecisionEngine
 
                 if (decision != null)
                 {
+                    var source = pushedRelease ? ReleaseSourceType.ReleasePush : ReleaseSourceType.Rss;
+
+                    if (searchCriteria != null)
+                    {
+                        if (searchCriteria.InteractiveSearch)
+                        {
+                            source = ReleaseSourceType.InteractiveSearch;
+                        }
+                        else if (searchCriteria.UserInvokedSearch)
+                        {
+                            source = ReleaseSourceType.UserInvokedSearch;
+                        }
+                        else
+                        {
+                            source = ReleaseSourceType.Search;
+                        }
+                    }
+
+                    decision.RemoteAlbum.ReleaseSource = source;
+
                     if (decision.Rejections.Any())
                     {
                         _logger.Debug("Release rejected for the following reasons: {0}", string.Join(", ", decision.Rejections));