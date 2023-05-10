Sonarr/Sonarr

New: Option to include series image for Gotify notifications
Closes #4882

src/NzbDrone.Core/Notifications/Gotify/Gotify.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using FluentValidation.Results;
 using NLog;
+using NzbDrone.Core.MediaCover;
+using NzbDrone.Core.Tv;
 
 namespace NzbDrone.Core.Notifications.Gotify
 {
@@ -19,34 +23,34 @@ namespace NzbDrone.Core.Notifications.Gotify
         public override string Name => "Gotify";
         public override string Link => "https://gotify.net/";
 
-        public override void OnGrab(GrabMessage grabMessage)
+        public override void OnGrab(GrabMessage message)
         {
-            _proxy.SendNotification(EPISODE_GRABBED_TITLE, grabMessage.Message, Settings);
+            SendNotification(EPISODE_GRABBED_TITLE, message.Message, message.Series);
         }
 
         public override void OnDownload(DownloadMessage message)
         {
-            _proxy.SendNotification(EPISODE_DOWNLOADED_TITLE, message.Message, Settings);
+            SendNotification(EPISODE_DOWNLOADED_TITLE, message.Message, message.Series);
         }
 
-        public override void OnEpisodeFileDelete(EpisodeDeleteMessage deleteMessage)
+        public override void OnEpisodeFileDelete(EpisodeDeleteMessage message)
         {
-            _proxy.SendNotification(EPISODE_DELETED_TITLE, deleteMessage.Message, Settings);
+            SendNotification(EPISODE_DELETED_TITLE, message.Message, message.Series);
         }
 
-        public override void OnSeriesDelete(SeriesDeleteMessage deleteMessage)
+        public override void OnSeriesDelete(SeriesDeleteMessage message)
         {
-            _proxy.SendNotification(SERIES_DELETED_TITLE, deleteMessage.Message, Settings);
+            SendNotification(SERIES_DELETED_TITLE, message.Message, message.Series);
         }
 
         public override void OnHealthIssue(HealthCheck.HealthCheck healthCheck)
         {
-            _proxy.SendNotification(HEALTH_ISSUE_TITLE, healthCheck.Message, Settings);
+            SendNotification(HEALTH_ISSUE_TITLE, healthCheck.Message, null);
         }
 
-        public override void OnApplicationUpdate(ApplicationUpdateMessage updateMessage)
+        public override void OnApplicationUpdate(ApplicationUpdateMessage message)
         {
-            _proxy.SendNotification(APPLICATION_UPDATE_TITLE, updateMessage.Message, Settings);
+            SendNotification(APPLICATION_UPDATE_TITLE, message.Message, null);
         }
 
         public override ValidationResult Test()
@@ -55,10 +59,29 @@ namespace NzbDrone.Core.Notifications.Gotify
 
             try
             {
+                var isMarkdown = false;
                 const string title = "Test Notification";
-                const string body = "This is a test message from Sonarr";
 
-                _proxy.SendNotification(title, body, Settings);
+                var sb = new StringBuilder();
+                sb.AppendLine("This is a test message from Sonarr");
+
+                if (Settings.IncludeSeriesPoster)
+                {
+                    isMarkdown = true;
+
+                    sb.AppendLine("\r![](https://raw.githubusercontent.com/Sonarr/Sonarr/develop/Logo/128.png)");
+                }
+
+                var payload = new GotifyMessage
+                {
+                    Title = title,
+                    Message = sb.ToString(),
+                    Priority = Settings.Priority
+                };
+
+                payload.SetContentType(isMarkdown);
+
+                _proxy.SendNotification(payload, Settings);
             }
             catch (Exception ex)
             {
@@ -67,6 +90,36 @@ namespace NzbDrone.Core.Notifications.Gotify
             }
 
             return new ValidationResult(failures);
+        }
+
+        private void SendNotification(string title, string message, Series series)
+        {
+            var isMarkdown = false;
+            var sb = new StringBuilder();
+
+            sb.AppendLine(message);
+
+            if (Settings.IncludeSeriesPoster && series != null)
+            {
+                var poster = series.Images.FirstOrDefault(x => x.CoverType == MediaCoverTypes.Poster)?.Url;
+
+                if (poster != null)
+                {
+                    isMarkdown = true;
+                    sb.AppendLine($"\r![]({poster})");
+                }
+            }
+
+            var payload = new GotifyMessage
+            {
+                Title = title,
+                Message = sb.ToString(),
+                Priority = Settings.Priority
+            };
+
+            payload.SetContentType(isMarkdown);
+
+            _proxy.SendNotification(payload, Settings);
         }
     }
 }

src/NzbDrone.Core/Notifications/Gotify/GotifySettings.cs
@@ -32,6 +32,9 @@ namespace NzbDrone.Core.Notifications.Gotify
         [FieldDefinition(2, Label = "Priority", Type = FieldType.Select, SelectOptions = typeof(GotifyPriority), HelpText = "Priority of the notification")]
         public int Priority { get; set; }
 
+        [FieldDefinition(3, Label = "Include Series Poster", Type = FieldType.Checkbox, HelpText = "Include series poster in message")]
+        public bool IncludeSeriesPoster { get; set; }
+
         public NzbDroneValidationResult Validate()
         {
             return new NzbDroneValidationResult(Validator.Validate(this));

Lidarr/Lidarr

(cherry picked from commit e57e68c97a9d24f8344623ac8f731c2da220686b)
Closes #3240

src/NzbDrone.Core/Notifications/Gotify/Gotify.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using FluentValidation.Results;
 using NLog;
+using NzbDrone.Core.MediaCover;
+using NzbDrone.Core.Music;
 
 namespace NzbDrone.Core.Notifications.Gotify
 {
@@ -21,47 +25,47 @@ namespace NzbDrone.Core.Notifications.Gotify
 
         public override void OnGrab(GrabMessage grabMessage)
         {
-            _proxy.SendNotification(ALBUM_GRABBED_TITLE, grabMessage.Message, Settings);
+            SendNotification(ALBUM_GRABBED_TITLE, grabMessage.Message, grabMessage.Artist);
         }
 
         public override void OnReleaseImport(AlbumDownloadMessage message)
         {
-            _proxy.SendNotification(ALBUM_DOWNLOADED_TITLE, message.Message, Settings);
+            SendNotification(ALBUM_DOWNLOADED_TITLE, message.Message, message.Artist);
         }
 
         public override void OnAlbumDelete(AlbumDeleteMessage deleteMessage)
         {
-            _proxy.SendNotification(ALBUM_DELETED_TITLE, deleteMessage.Message, Settings);
+            SendNotification(ALBUM_DELETED_TITLE, deleteMessage.Message, deleteMessage.Album?.Artist);
         }
 
         public override void OnArtistDelete(ArtistDeleteMessage deleteMessage)
         {
-            _proxy.SendNotification(ARTIST_DELETED_TITLE, deleteMessage.Message, Settings);
+            SendNotification(ARTIST_DELETED_TITLE, deleteMessage.Message, deleteMessage.Artist);
         }
 
         public override void OnHealthIssue(HealthCheck.HealthCheck healthCheck)
         {
-            _proxy.SendNotification(HEALTH_ISSUE_TITLE, healthCheck.Message, Settings);
+            SendNotification(HEALTH_ISSUE_TITLE, healthCheck.Message, null);
         }
 
         public override void OnHealthRestored(HealthCheck.HealthCheck previousCheck)
         {
-            _proxy.SendNotification(HEALTH_RESTORED_TITLE, $"The following issue is now resolved: {previousCheck.Message}", null);
+            SendNotification(HEALTH_RESTORED_TITLE, $"The following issue is now resolved: {previousCheck.Message}", null);
         }
 
         public override void OnDownloadFailure(DownloadFailedMessage message)
         {
-            _proxy.SendNotification(DOWNLOAD_FAILURE_TITLE, message.Message, Settings);
+            SendNotification(DOWNLOAD_FAILURE_TITLE, message.Message, null);
         }
 
         public override void OnImportFailure(AlbumDownloadMessage message)
         {
-            _proxy.SendNotification(IMPORT_FAILURE_TITLE, message.Message, Settings);
+            SendNotification(IMPORT_FAILURE_TITLE, message.Message, message.Artist);
         }
 
         public override void OnApplicationUpdate(ApplicationUpdateMessage updateMessage)
         {
-            _proxy.SendNotification(APPLICATION_UPDATE_TITLE, updateMessage.Message, Settings);
+            SendNotification(APPLICATION_UPDATE_TITLE, updateMessage.Message, null);
         }
 
         public override ValidationResult Test()
@@ -70,10 +74,29 @@ namespace NzbDrone.Core.Notifications.Gotify
 
             try
             {
+                var isMarkdown = false;
                 const string title = "Test Notification";
-                const string body = "This is a test message from Lidarr";
 
-                _proxy.SendNotification(title, body, Settings);
+                var sb = new StringBuilder();
+                sb.AppendLine("This is a test message from Lidarr");
+
+                if (Settings.IncludeArtistPoster)
+                {
+                    isMarkdown = true;
+
+                    sb.AppendLine("\r![](https://raw.githubusercontent.com/Lidarr/Lidarr/develop/Logo/128.png)");
+                }
+
+                var payload = new GotifyMessage
+                {
+                    Title = title,
+                    Message = sb.ToString(),
+                    Priority = Settings.Priority
+                };
+
+                payload.SetContentType(isMarkdown);
+
+                _proxy.SendNotification(payload, Settings);
             }
             catch (Exception ex)
             {
@@ -82,6 +105,36 @@ namespace NzbDrone.Core.Notifications.Gotify
             }
 
             return new ValidationResult(failures);
+        }
+
+        private void SendNotification(string title, string message, Artist artist)
+        {
+            var isMarkdown = false;
+            var sb = new StringBuilder();
+
+            sb.AppendLine(message);
+
+            if (Settings.IncludeArtistPoster && artist != null)
+            {
+                var poster = artist.Metadata.Value.Images.FirstOrDefault(x => x.CoverType == MediaCoverTypes.Poster)?.Url;
+
+                if (poster != null)
+                {
+                    isMarkdown = true;
+                    sb.AppendLine($"\r![]({poster})");
+                }
+            }
+
+            var payload = new GotifyMessage
+            {
+                Title = title,
+                Message = sb.ToString(),
+                Priority = Settings.Priority
+            };
+
+            payload.SetContentType(isMarkdown);
+
+            _proxy.SendNotification(payload, Settings);
         }
     }
 }

src/NzbDrone.Core/Notifications/Gotify/GotifySettings.cs
@@ -32,6 +32,9 @@ namespace NzbDrone.Core.Notifications.Gotify
         [FieldDefinition(2, Label = "Priority", Type = FieldType.Select, SelectOptions = typeof(GotifyPriority), HelpText = "Priority of the notification")]
         public int Priority { get; set; }
 
+        [FieldDefinition(3, Label = "Include Artist Poster", Type = FieldType.Checkbox, HelpText = "Include artist poster in message")]
+        public bool IncludeArtistPoster { get; set; }
+
         public NzbDroneValidationResult Validate()
         {
             return new NzbDroneValidationResult(Validator.Validate(this));