platbr/Sonarr

New: Use release quality source if not in downloaded file and resolution matches

src/NzbDrone.Core.Test/MediaFiles/EpisodeImport/ImportDecisionMakerFixture.cs
@@ -25,6 +25,7 @@ namespace NzbDrone.Core.Test.MediaFiles.EpisodeImport
     {
         private List<string> _videoFiles;
         private LocalEpisode _localEpisode;
+        private DownloadClientItem _downloadClientItem;
         private Series _series;
         private QualityModel _quality;
 
@@ -92,7 +93,7 @@ namespace NzbDrone.Core.Test.MediaFiles.EpisodeImport
         private void GivenAugmentationSuccess()
         {
             Mocker.GetMock<IAggregationService>()
-                  .Setup(s => s.Augment(It.IsAny<LocalEpisode>(), It.IsAny<bool>()))
+                  .Setup(s => s.Augment(It.IsAny<LocalEpisode>(), It.IsAny<DownloadClientItem>(), It.IsAny<bool>()))
                   .Callback<LocalEpisode, bool>((localEpisode, otherFiles) =>
                   {
                       localEpisode.Episodes = _localEpisode.Episodes;
@@ -163,7 +164,7 @@ namespace NzbDrone.Core.Test.MediaFiles.EpisodeImport
             GivenSpecifications(_pass1);
 
             Mocker.GetMock<IAggregationService>()
-                  .Setup(c => c.Augment(It.IsAny<LocalEpisode>(), It.IsAny<bool>()))
+                  .Setup(c => c.Augment(It.IsAny<LocalEpisode>(), It.IsAny<DownloadClientItem>(), It.IsAny<bool>()))
                   .Throws<TestException>();
 
             _videoFiles = new List<string>
@@ -178,7 +179,7 @@ namespace NzbDrone.Core.Test.MediaFiles.EpisodeImport
             Subject.GetImportDecisions(_videoFiles, _series);
 
             Mocker.GetMock<IAggregationService>()
-                  .Verify(c => c.Augment(It.IsAny<LocalEpisode>(), It.IsAny<bool>()), Times.Exactly(_videoFiles.Count));
+                  .Verify(c => c.Augment(It.IsAny<LocalEpisode>(), It.IsAny<DownloadClientItem>(), It.IsAny<bool>()), Times.Exactly(_videoFiles.Count));
 
             ExceptionVerification.ExpectedErrors(3);
         }
@@ -199,7 +200,7 @@ namespace NzbDrone.Core.Test.MediaFiles.EpisodeImport
             var decisions = Subject.GetImportDecisions(_videoFiles, _series);
 
             Mocker.GetMock<IAggregationService>()
-                  .Verify(c => c.Augment(It.IsAny<LocalEpisode>(), It.IsAny<bool>()), Times.Exactly(_videoFiles.Count));
+                  .Verify(c => c.Augment(It.IsAny<LocalEpisode>(), It.IsAny<DownloadClientItem>(), It.IsAny<bool>()), Times.Exactly(_videoFiles.Count));
 
             decisions.Should().HaveCount(3);
             decisions.First().Rejections.Should().NotBeEmpty();
@@ -209,7 +210,7 @@ namespace NzbDrone.Core.Test.MediaFiles.EpisodeImport
         public void should_return_a_decision_when_exception_is_caught()
         {
             Mocker.GetMock<IAggregationService>()
-                  .Setup(c => c.Augment(It.IsAny<LocalEpisode>(), It.IsAny<bool>()))
+                  .Setup(c => c.Augment(It.IsAny<LocalEpisode>(), It.IsAny<DownloadClientItem>(), It.IsAny<bool>()))
                   .Throws<TestException>();
 
             _videoFiles = new List<string>

src/NzbDrone.Core/Extras/Metadata/ExistingMetadataImporter.cs
@@ -71,7 +71,7 @@ namespace NzbDrone.Core.Extras.Metadata
 
                         try
                         {
-                            _aggregationService.Augment(localEpisode, false);
+                            _aggregationService.Augment(localEpisode, null, false);
                         }
                         catch (AugmentingFailedException)
                         {

src/NzbDrone.Core/Extras/Others/ExistingOtherExtraImporter.cs
@@ -56,7 +56,7 @@ namespace NzbDrone.Core.Extras.Others
 
                 try
                 {
-                    _aggregationService.Augment(localEpisode, false);
+                    _aggregationService.Augment(localEpisode, null, false);
                 }
                 catch (AugmentingFailedException)
                 {

src/NzbDrone.Core/MediaFiles/EpisodeImport/Aggregation/AggregationService.cs
@@ -4,6 +4,7 @@ using System.IO;
 using NLog;
 using NzbDrone.Common.Disk;
 using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Download;
 using NzbDrone.Core.MediaFiles.EpisodeImport.Aggregation.Aggregators;
 using NzbDrone.Core.MediaFiles.MediaInfo;
 using NzbDrone.Core.Parser.Model;
@@ -12,7 +13,7 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Aggregation
 {
     public interface IAggregationService
     {
-        LocalEpisode Augment(LocalEpisode localEpisode, bool otherFiles);
+        LocalEpisode Augment(LocalEpisode localEpisode, DownloadClientItem downloadClientItem, bool otherFiles);
     }
 
     public class AggregationService : IAggregationService
@@ -36,7 +37,7 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Aggregation
             _logger = logger;
         }
 
-        public LocalEpisode Augment(LocalEpisode localEpisode, bool otherFiles)
+        public LocalEpisode Augment(LocalEpisode localEpisode, DownloadClientItem downloadClientItem, bool otherFiles)
         {
             var isMediaFile = MediaFileExtensions.Extensions.Contains(Path.GetExtension(localEpisode.Path));
 
@@ -61,7 +62,7 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Aggregation
             {
                 try
                 {
-                    augmenter.Aggregate(localEpisode, otherFiles);
+                    augmenter.Aggregate(localEpisode, downloadClientItem, otherFiles);
                 }
                 catch (Exception ex)
                 {

src/NzbDrone.Core/MediaFiles/EpisodeImport/Aggregation/Aggregators/AggregateQuality.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using NLog;
+using NzbDrone.Core.Download;
 using NzbDrone.Core.MediaFiles.EpisodeImport.Aggregation.Aggregators.Augmenters.Quality;
 using NzbDrone.Core.Parser.Model;
 using NzbDrone.Core.Qualities;
@@ -19,9 +20,9 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Aggregation.Aggregators
             _logger = logger;
         }
 
-        public LocalEpisode Aggregate(LocalEpisode localEpisode, bool otherFiles)
+        public LocalEpisode Aggregate(LocalEpisode localEpisode, DownloadClientItem downloadClientItem, bool otherFiles)
         {
-            var augmentedQualities = _augmentQualities.Select(a => a.AugmentQuality(localEpisode))
+            var augmentedQualities = _augmentQualities.Select(a => a.AugmentQuality(localEpisode, downloadClientItem))
                                                       .Where(a => a != null)
                                                       .OrderBy(a => a.SourceConfidence);
 

src/NzbDrone.Core/MediaFiles/EpisodeImport/Aggregation/Aggregators/AggregateReleaseGroup.cs
@@ -1,11 +1,12 @@
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Download;
 using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.MediaFiles.EpisodeImport.Aggregation.Aggregators
 {
     public class AggregateReleaseGroup : IAggregateLocalEpisode
     {
-        public LocalEpisode Aggregate(LocalEpisode localEpisode, bool otherFiles)
+        public LocalEpisode Aggregate(LocalEpisode localEpisode, DownloadClientItem downloadClientItem, bool otherFiles)
         {
             var releaseGroup = localEpisode.DownloadClientEpisodeInfo?.ReleaseGroup;
 

Lidarr/Lidarr

src/NzbDrone.Core.Test/MediaFiles/TrackImport/ImportDecisionMakerFixture.cs
@@ -151,8 +151,8 @@ namespace NzbDrone.Core.Test.MediaFiles.TrackImport
         private void GivenAugmentationSuccess()
         {
             Mocker.GetMock<IAugmentingService>()
-                  .Setup(s => s.Augment(It.IsAny<LocalTrack>(), It.IsAny<bool>()))
-                  .Callback<LocalTrack, bool>((localTrack, otherFiles) =>
+                  .Setup(s => s.Augment(It.IsAny<LocalTrack>(), It.IsAny<DownloadClientItem>(), It.IsAny<bool>()))
+                  .Callback<LocalTrack, DownloadClientItem, bool>((localTrack, downloadClientItem, otherFiles) =>
                   {
                       localTrack.Tracks = _localTrack.Tracks;
                   });
@@ -323,7 +323,7 @@ namespace NzbDrone.Core.Test.MediaFiles.TrackImport
             GivenSpecifications(_pass1);
 
             Mocker.GetMock<IAugmentingService>()
-                  .Setup(c => c.Augment(It.IsAny<LocalTrack>(), It.IsAny<bool>()))
+                  .Setup(c => c.Augment(It.IsAny<LocalTrack>(), It.IsAny<DownloadClientItem>(), It.IsAny<bool>()))
                   .Throws<TestException>();
 
             GivenAudioFiles(new[]
@@ -336,7 +336,7 @@ namespace NzbDrone.Core.Test.MediaFiles.TrackImport
             var decisions = Subject.GetImportDecisions(_fileInfos, _idOverrides, null, _idConfig);
 
             Mocker.GetMock<IAugmentingService>()
-                  .Verify(c => c.Augment(It.IsAny<LocalTrack>(), It.IsAny<bool>()), Times.Exactly(_fileInfos.Count));
+                  .Verify(c => c.Augment(It.IsAny<LocalTrack>(), It.IsAny<DownloadClientItem>(), It.IsAny<bool>()), Times.Exactly(_fileInfos.Count));
 
             ExceptionVerification.ExpectedErrors(3);
         }
@@ -363,7 +363,7 @@ namespace NzbDrone.Core.Test.MediaFiles.TrackImport
             var decisions = Subject.GetImportDecisions(_fileInfos, _idOverrides, null, _idConfig);
 
             Mocker.GetMock<IAugmentingService>()
-                  .Verify(c => c.Augment(It.IsAny<LocalTrack>(), It.IsAny<bool>()), Times.Exactly(_fileInfos.Count));
+                  .Verify(c => c.Augment(It.IsAny<LocalTrack>(), It.IsAny<DownloadClientItem>(), It.IsAny<bool>()), Times.Exactly(_fileInfos.Count));
 
             decisions.Should().HaveCount(3);
             decisions.First().Rejections.Should().NotBeEmpty();
@@ -384,7 +384,7 @@ namespace NzbDrone.Core.Test.MediaFiles.TrackImport
             var decisions = Subject.GetImportDecisions(_fileInfos, _idOverrides, null, _idConfig);
 
             Mocker.GetMock<IAugmentingService>()
-                  .Verify(c => c.Augment(It.IsAny<LocalTrack>(), It.IsAny<bool>()), Times.Exactly(_fileInfos.Count));
+                  .Verify(c => c.Augment(It.IsAny<LocalTrack>(), It.IsAny<DownloadClientItem>(), It.IsAny<bool>()), Times.Exactly(_fileInfos.Count));
 
             decisions.Should().HaveCount(3);
             decisions.First().Rejections.Should().NotBeEmpty();
@@ -394,7 +394,7 @@ namespace NzbDrone.Core.Test.MediaFiles.TrackImport
         public void should_return_a_decision_when_exception_is_caught()
         {
             Mocker.GetMock<IAugmentingService>()
-                  .Setup(c => c.Augment(It.IsAny<LocalTrack>(), It.IsAny<bool>()))
+                  .Setup(c => c.Augment(It.IsAny<LocalTrack>(), It.IsAny<DownloadClientItem>(), It.IsAny<bool>()))
                   .Throws<TestException>();
 
             GivenAudioFiles(new[]

src/NzbDrone.Core/Extras/Metadata/ExistingMetadataImporter.cs
@@ -85,7 +85,7 @@ namespace NzbDrone.Core.Extras.Metadata
 
                         try
                         {
-                            _augmentingService.Augment(localTrack, false);
+                            _augmentingService.Augment(localTrack, null, false);
                         }
                         catch (AugmentingFailedException)
                         {

src/NzbDrone.Core/Extras/Others/ExistingOtherExtraImporter.cs
@@ -54,7 +54,7 @@ namespace NzbDrone.Core.Extras.Others
 
                 try
                 {
-                    _augmentingService.Augment(localTrack, false);
+                    _augmentingService.Augment(localTrack, null, false);
                 }
                 catch (AugmentingFailedException)
                 {

src/NzbDrone.Core/MediaFiles/TrackImport/Aggregation/AggregationService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using NLog;
 using NzbDrone.Common.Disk;
+using NzbDrone.Core.Download;
 using NzbDrone.Core.MediaFiles.EpisodeImport;
 using NzbDrone.Core.MediaFiles.TrackImport.Aggregation.Aggregators;
 using NzbDrone.Core.Parser.Model;
@@ -11,7 +12,7 @@ namespace NzbDrone.Core.MediaFiles.TrackImport.Aggregation
 {
     public interface IAugmentingService
     {
-        LocalTrack Augment(LocalTrack localTrack, bool otherFiles);
+        LocalTrack Augment(LocalTrack localTrack, DownloadClientItem downloadClientItem, bool otherFiles);
         LocalAlbumRelease Augment(LocalAlbumRelease localAlbum);
     }
 
@@ -33,7 +34,7 @@ namespace NzbDrone.Core.MediaFiles.TrackImport.Aggregation
             _logger = logger;
         }
 
-        public LocalTrack Augment(LocalTrack localTrack, bool otherFiles)
+        public LocalTrack Augment(LocalTrack localTrack, DownloadClientItem downloadClientItem, bool otherFiles)
         {
             if (localTrack.DownloadClientAlbumInfo == null &&
                 localTrack.FolderAlbumInfo == null &&
@@ -52,7 +53,7 @@ namespace NzbDrone.Core.MediaFiles.TrackImport.Aggregation
             {
                 try
                 {
-                    augmenter.Aggregate(localTrack, otherFiles);
+                    augmenter.Aggregate(localTrack, downloadClientItem, otherFiles);
                 }
                 catch (Exception ex)
                 {
@@ -71,7 +72,7 @@ namespace NzbDrone.Core.MediaFiles.TrackImport.Aggregation
             {
                 try
                 {
-                    augmenter.Aggregate(localAlbum, false);
+                    augmenter.Aggregate(localAlbum, null, false);
                 }
                 catch (Exception ex)
                 {

src/NzbDrone.Core/MediaFiles/TrackImport/Aggregation/Aggregators/AggregateQuality.cs
@@ -1,10 +1,11 @@
+using NzbDrone.Core.Download;
 using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.MediaFiles.TrackImport.Aggregation.Aggregators
 {
     public class AggregateQuality : IAggregate<LocalTrack>
     {
-        public LocalTrack Aggregate(LocalTrack localTrack, bool otherFiles)
+        public LocalTrack Aggregate(LocalTrack localTrack, DownloadClientItem downloadClientItem, bool otherFiles)
         {
             var quality = localTrack.FileTrackInfo?.Quality;
 

src/NzbDrone.Core/MediaFiles/TrackImport/Aggregation/Aggregators/AggregateReleaseGroup.cs
@@ -1,11 +1,12 @@
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Download;
 using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.MediaFiles.TrackImport.Aggregation.Aggregators
 {
     public class AggregateReleaseGroup : IAggregate<LocalTrack>
     {
-        public LocalTrack Aggregate(LocalTrack localTrack, bool otherFiles)
+        public LocalTrack Aggregate(LocalTrack localTrack, DownloadClientItem downloadClientItem, bool otherFiles)
         {
             var releaseGroup = localTrack.DownloadClientAlbumInfo?.ReleaseGroup;
 