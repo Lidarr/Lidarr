Radarr/Radarr

Fixed: Register PostgresOptions when running in utility mode

src/NzbDrone.Host/Bootstrap.cs
@@ -53,6 +53,7 @@ namespace Radarr.Host
                 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
                 var appMode = GetApplicationMode(startupContext);
+                var config = GetConfiguration(startupContext);
 
                 switch (appMode)
                 {
@@ -81,12 +82,22 @@ namespace Radarr.Host
                     // Utility mode
                     default:
                     {
-                        new Container(rules => rules.WithNzbDroneRules())
-                            .AutoAddServices(ASSEMBLIES)
-                            .AddNzbDroneLogger()
-                            .AddStartupContext(startupContext)
-                            .Resolve<UtilityModeRouter>()
-                            .Route(appMode);
+                        new HostBuilder()
+                            .UseServiceProviderFactory(new DryIocServiceProviderFactory(new Container(rules => rules.WithNzbDroneRules())))
+                            .ConfigureContainer<IContainer>(c =>
+                            {
+                                c.AutoAddServices(Bootstrap.ASSEMBLIES)
+                                    .AddNzbDroneLogger()
+                                    .AddDatabase()
+                                    .AddStartupContext(startupContext)
+                                    .Resolve<UtilityModeRouter>()
+                                    .Route(appMode);
+                            })
+                            .ConfigureServices(services =>
+                            {
+                                services.Configure<PostgresOptions>(config.GetSection("Radarr:Postgres"));
+                            }).Build();
+
                         break;
                     }
                 }

Lidarr/Lidarr

(cherry picked from commit 3a1d848e59dda24b50220700b61db6046505c7a5)

src/NzbDrone.Host/Bootstrap.cs
@@ -54,6 +54,7 @@ namespace NzbDrone.Host
                 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
                 var appMode = GetApplicationMode(startupContext);
+                var config = GetConfiguration(startupContext);
 
                 switch (appMode)
                 {
@@ -82,12 +83,22 @@ namespace NzbDrone.Host
                     // Utility mode
                     default:
                     {
-                        new Container(rules => rules.WithNzbDroneRules())
-                            .AutoAddServices(ASSEMBLIES)
-                            .AddNzbDroneLogger()
-                            .AddStartupContext(startupContext)
-                            .Resolve<UtilityModeRouter>()
-                            .Route(appMode);
+                        new HostBuilder()
+                            .UseServiceProviderFactory(new DryIocServiceProviderFactory(new Container(rules => rules.WithNzbDroneRules())))
+                            .ConfigureContainer<IContainer>(c =>
+                            {
+                                c.AutoAddServices(Bootstrap.ASSEMBLIES)
+                                    .AddNzbDroneLogger()
+                                    .AddDatabase()
+                                    .AddStartupContext(startupContext)
+                                    .Resolve<UtilityModeRouter>()
+                                    .Route(appMode);
+                            })
+                            .ConfigureServices(services =>
+                            {
+                                services.Configure<PostgresOptions>(config.GetSection("Lidarr:Postgres"));
+                            }).Build();
+
                         break;
                     }
                 }