Sonarr/Sonarr

New: Improved Plex library updating

src/NzbDrone.Core/Notifications/Plex/Server/PlexServerService.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using FluentValidation.Results;
 using NLog;
 using NzbDrone.Common.Cache;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.RootFolders;
 using NzbDrone.Core.Tv;
 using NzbDrone.Core.Validation;
 
@@ -22,15 +24,15 @@ namespace NzbDrone.Core.Notifications.Plex.Server
     public class PlexServerService : IPlexServerService
     {
         private readonly ICached<Version> _versionCache;
-        private readonly ICached<bool> _partialUpdateCache;
         private readonly IPlexServerProxy _plexServerProxy;
+        private readonly IRootFolderService _rootFolderService;
         private readonly Logger _logger;
 
-        public PlexServerService(ICacheManager cacheManager, IPlexServerProxy plexServerProxy, Logger logger)
+        public PlexServerService(ICacheManager cacheManager, IPlexServerProxy plexServerProxy, IRootFolderService rootFolderService, Logger logger)
         {
             _versionCache = cacheManager.GetCache<Version>(GetType(), "versionCache");
-            _partialUpdateCache = cacheManager.GetCache<bool>(GetType(), "partialUpdateCache");
             _plexServerProxy = plexServerProxy;
+            _rootFolderService = rootFolderService;
             _logger = logger;
         }
 
@@ -50,32 +52,10 @@ namespace NzbDrone.Core.Notifications.Plex.Server
                 ValidateVersion(version);
 
                 var sections = GetSections(settings);
-                var partialUpdates = _partialUpdateCache.Get(settings.Host, () => PartialUpdatesAllowed(settings, version), TimeSpan.FromHours(2));
 
-                if (partialUpdates)
+                foreach (var series in multipleSeries)
                 {
-                    var partiallyUpdated = true;
-
-                    foreach (var series in multipleSeries)
-                    {
-                        partiallyUpdated &= UpdatePartialSection(series, sections, settings);
-
-                        if (!partiallyUpdated)
-                        {
-                            break;
-                        }
-                    }
-
-                    // Only update complete sections if all partial updates failed
-                    if (!partiallyUpdated)
-                    {
-                        _logger.Debug("Unable to update partial section, updating all TV sections");
-                        sections.ForEach(s => UpdateSection(s.Id, settings));
-                    }
-                }
-                else
-                {
-                    sections.ForEach(s => UpdateSection(s.Id, settings));
+                    UpdateSections(series, sections, settings);
                 }
 
                 _logger.Debug("Finished sending Update Request to Plex Server (took {0} ms)", watch.ElapsedMilliseconds);
@@ -92,31 +72,6 @@ namespace NzbDrone.Core.Notifications.Plex.Server
             _logger.Debug("Getting sections from Plex host: {0}", settings.Host);
 
             return _plexServerProxy.GetTvSections(settings).ToList();
-        }
-
-        private bool PartialUpdatesAllowed(PlexServerSettings settings, Version version)
-        {
-            try
-            {
-                if (version >= new Version(0, 9, 12, 0))
-                {
-                    var preferences = GetPreferences(settings);
-                    var partialScanPreference = preferences.SingleOrDefault(p => p.Id.Equals("FSEventLibraryPartialScanEnabled"));
-
-                    if (partialScanPreference == null)
-                    {
-                        return false;
-                    }
-
-                    return Convert.ToBoolean(partialScanPreference.Value);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.Warn(ex, "Unable to check if partial updates are allowed");
-            }
-
-            return false;
         }
 
         private void ValidateVersion(Version version)
@@ -137,45 +92,47 @@ namespace NzbDrone.Core.Notifications.Plex.Server
             return version;
         }
 
-        private List<PlexPreference> GetPreferences(PlexServerSettings settings)
+        private void UpdateSections(Series series, List<PlexSection> sections, PlexServerSettings settings)
         {
-            _logger.Debug("Getting preferences from Plex host: {0}", settings.Host);
+            var rootFolderPath = _rootFolderService.GetBestRootFolderPath(series.Path);
+            var seriesRelativePath = rootFolderPath.GetRelativePath(series.Path);
 
-            return _plexServerProxy.Preferences(settings);
-        }
-
-        private void UpdateSection(int sectionId, PlexServerSettings settings)
-        {
-            _logger.Debug("Updating Plex host: {0}, Section: {1}", settings.Host, sectionId);
-
-            _plexServerProxy.Update(sectionId, settings);
-        }
-
-        private bool UpdatePartialSection(Series series, List<PlexSection> sections, PlexServerSettings settings)
-        {
-            var partiallyUpdated = false;
-
+            // Try to update a matching section location before falling back to updating all section locations.
             foreach (var section in sections)
             {
-                var metadataId = GetMetadataId(section.Id, series, section.Language, settings);
-
-                if (metadataId.IsNotNullOrWhiteSpace())
+                foreach (var location in section.Locations)
                 {
-                    _logger.Debug("Updating Plex host: {0}, Section: {1}, Series: {2}", settings.Host, section.Id, series);
-                    _plexServerProxy.UpdateSeries(metadataId, settings);
+                    if (location.Path.PathEquals(rootFolderPath))
+                    {
+                        _logger.Debug("Updating matching section location, {0}", location.Path);
+                        UpdateSectionPath(seriesRelativePath, section, location, settings);
 
-                    partiallyUpdated = true;
+                        return;
+                    }
                 }
             }
 
-            return partiallyUpdated;
+            _logger.Debug("Unable to find matching section location, updating all TV sections");
+
+            foreach (var section in sections)
+            {
+                foreach (var location in section.Locations)
+                {
+                    UpdateSectionPath(seriesRelativePath, section, location, settings);
+                }
+            }
         }
 
-        private string GetMetadataId(int sectionId, Series series, string language, PlexServerSettings settings)
+        private void UpdateSectionPath(string seriesRelativePath, PlexSection section, PlexSectionLocation location, PlexServerSettings settings)
         {
-            _logger.Debug("Getting metadata from Plex host: {0} for series: {1}", settings.Host, series);
+            var separator = location.Path.Contains('\\') ? "\\" : "/";
+            var locationRelativePath = seriesRelativePath.Replace("\\", separator).Replace("/", separator);
 
-            return _plexServerProxy.GetMetadataId(sectionId, series.TvdbId, language, settings);
+            // Plex location paths trim trailing extraneous separator characters, so it doesn't need to be trimmed
+            var pathToUpdate = $"{location.Path}{separator}{locationRelativePath}";
+
+            _logger.Debug("Updating section location, {0}", location.Path);
+            _plexServerProxy.Update(section.Id, pathToUpdate, settings);
         }
 
         public ValidationFailure Test(PlexServerSettings settings)
@@ -183,7 +140,6 @@ namespace NzbDrone.Core.Notifications.Plex.Server
             try
             {
                 _versionCache.Remove(settings.Host);
-                _partialUpdateCache.Remove(settings.Host);
                 var sections = GetSections(settings);
 
                 if (sections.Empty())

Lidarr/Lidarr

(cherry picked from commit 98308737cfa78a41ec22a2b23a80bf316bd779ea)
Closes #3438
Closes #3445
Closes #3465

src/NzbDrone.Core/Notifications/Plex/Server/PlexServerService.cs
@@ -8,6 +8,7 @@ using NLog;
 using NzbDrone.Common.Cache;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Core.Music;
+using NzbDrone.Core.RootFolders;
 using NzbDrone.Core.Validation;
 
 namespace NzbDrone.Core.Notifications.Plex.Server
@@ -22,15 +23,15 @@ namespace NzbDrone.Core.Notifications.Plex.Server
     public class PlexServerService : IPlexServerService
     {
         private readonly ICached<Version> _versionCache;
-        private readonly ICached<bool> _partialUpdateCache;
         private readonly IPlexServerProxy _plexServerProxy;
+        private readonly IRootFolderService _rootFolderService;
         private readonly Logger _logger;
 
-        public PlexServerService(ICacheManager cacheManager, IPlexServerProxy plexServerProxy, Logger logger)
+        public PlexServerService(ICacheManager cacheManager, IPlexServerProxy plexServerProxy, IRootFolderService rootFolderService, Logger logger)
         {
             _versionCache = cacheManager.GetCache<Version>(GetType(), "versionCache");
-            _partialUpdateCache = cacheManager.GetCache<bool>(GetType(), "partialUpdateCache");
             _plexServerProxy = plexServerProxy;
+            _rootFolderService = rootFolderService;
             _logger = logger;
         }
 
@@ -50,32 +51,10 @@ namespace NzbDrone.Core.Notifications.Plex.Server
                 ValidateVersion(version);
 
                 var sections = GetSections(settings);
-                var partialUpdates = _partialUpdateCache.Get(settings.Host, () => PartialUpdatesAllowed(settings, version), TimeSpan.FromHours(2));
 
-                if (partialUpdates)
+                foreach (var artist in multipleArtist)
                 {
-                    var partiallyUpdated = true;
-
-                    foreach (var artist in multipleArtist)
-                    {
-                        partiallyUpdated &= UpdatePartialSection(artist, sections, settings);
-
-                        if (!partiallyUpdated)
-                        {
-                            break;
-                        }
-                    }
-
-                    // Only update complete sections if all partial updates failed
-                    if (!partiallyUpdated)
-                    {
-                        _logger.Debug("Unable to update partial section, updating all Music sections");
-                        sections.ForEach(s => UpdateSection(s.Id, settings));
-                    }
-                }
-                else
-                {
-                    sections.ForEach(s => UpdateSection(s.Id, settings));
+                    UpdateSections(artist, sections, settings);
                 }
 
                 _logger.Debug("Finished sending Update Request to Plex Server (took {0} ms)", watch.ElapsedMilliseconds);
@@ -92,31 +71,6 @@ namespace NzbDrone.Core.Notifications.Plex.Server
             _logger.Debug("Getting sections from Plex host: {0}", settings.Host);
 
             return _plexServerProxy.GetArtistSections(settings).ToList();
-        }
-
-        private bool PartialUpdatesAllowed(PlexServerSettings settings, Version version)
-        {
-            try
-            {
-                if (version >= new Version(0, 9, 12, 0))
-                {
-                    var preferences = GetPreferences(settings);
-                    var partialScanPreference = preferences.SingleOrDefault(p => p.Id.Equals("FSEventLibraryPartialScanEnabled"));
-
-                    if (partialScanPreference == null)
-                    {
-                        return false;
-                    }
-
-                    return Convert.ToBoolean(partialScanPreference.Value);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.Warn(ex, "Unable to check if partial updates are allowed");
-            }
-
-            return false;
         }
 
         private void ValidateVersion(Version version)
@@ -137,45 +91,47 @@ namespace NzbDrone.Core.Notifications.Plex.Server
             return version;
         }
 
-        private List<PlexPreference> GetPreferences(PlexServerSettings settings)
+        private void UpdateSections(Artist artist, List<PlexSection> sections, PlexServerSettings settings)
         {
-            _logger.Debug("Getting preferences from Plex host: {0}", settings.Host);
+            var rootFolderPath = _rootFolderService.GetBestRootFolderPath(artist.Path);
+            var artistRelativePath = rootFolderPath.GetRelativePath(artist.Path);
 
-            return _plexServerProxy.Preferences(settings);
-        }
-
-        private void UpdateSection(int sectionId, PlexServerSettings settings)
-        {
-            _logger.Debug("Updating Plex host: {0}, Section: {1}", settings.Host, sectionId);
-
-            _plexServerProxy.Update(sectionId, settings);
-        }
-
-        private bool UpdatePartialSection(Artist artist, List<PlexSection> sections, PlexServerSettings settings)
-        {
-            var partiallyUpdated = false;
-
+            // Try to update a matching section location before falling back to updating all section locations.
             foreach (var section in sections)
             {
-                var metadataId = GetMetadataId(section.Id, artist, section.Language, settings);
-
-                if (metadataId.HasValue)
+                foreach (var location in section.Locations)
                 {
-                    _logger.Debug("Updating Plex host: {0}, Section: {1}, Artist: {2}", settings.Host, section.Id, artist);
-                    _plexServerProxy.UpdateArtist(metadataId.Value, settings);
+                    if (location.Path.PathEquals(rootFolderPath))
+                    {
+                        _logger.Debug("Updating matching section location, {0}", location.Path);
+                        UpdateSectionPath(artistRelativePath, section, location, settings);
 
-                    partiallyUpdated = true;
+                        return;
+                    }
                 }
             }
 
-            return partiallyUpdated;
+            _logger.Debug("Unable to find matching section location, updating all Music sections");
+
+            foreach (var section in sections)
+            {
+                foreach (var location in section.Locations)
+                {
+                    UpdateSectionPath(artistRelativePath, section, location, settings);
+                }
+            }
         }
 
-        private int? GetMetadataId(int sectionId, Artist artist, string language, PlexServerSettings settings)
+        private void UpdateSectionPath(string artistRelativePath, PlexSection section, PlexSectionLocation location, PlexServerSettings settings)
         {
-            _logger.Debug("Getting metadata from Plex host: {0} for artist: {1}", settings.Host, artist);
+            var separator = location.Path.Contains('\\') ? "\\" : "/";
+            var locationRelativePath = artistRelativePath.Replace("\\", separator).Replace("/", separator);
 
-            return _plexServerProxy.GetMetadataId(sectionId, artist.Metadata.Value.ForeignArtistId, language, settings);
+            // Plex location paths trim trailing extraneous separator characters, so it doesn't need to be trimmed
+            var pathToUpdate = $"{location.Path}{separator}{locationRelativePath}";
+
+            _logger.Debug("Updating section location, {0}", location.Path);
+            _plexServerProxy.Update(section.Id, pathToUpdate, settings);
         }
 
         public ValidationFailure Test(PlexServerSettings settings)
@@ -183,7 +139,6 @@ namespace NzbDrone.Core.Notifications.Plex.Server
             try
             {
                 _versionCache.Remove(settings.Host);
-                _partialUpdateCache.Remove(settings.Host);
                 var sections = GetSections(settings);
 
                 if (sections.Empty())