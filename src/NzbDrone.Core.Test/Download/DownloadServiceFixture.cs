511dacd24a2249efc14ffca8b0b416c49815d18d
Mark McDowall <mark@mcdowall.ca>
2023-05-20 21:15:10 -0700

    Fixed tests in DownloadServiceFixture
    (cherry picked from commit d743a8f7e9eac348b4679919f60af5b27457acfd)
    
    Closes #3706

--- src/NzbDrone.Core.Test/Download/DownloadServiceFixture.cs
+++ src/NzbDrone.Core.Test/Download/DownloadServiceFixture.cs
@@ -32,7 +32,7 @@ namespace NzbDrone.Core.Test.Download
 
             Mocker.GetMock<IProvideDownloadClient>()
                 .Setup(v => v.GetDownloadClient(It.IsAny<DownloadProtocol>(), It.IsAny<int>(), It.IsAny<bool>()))
-                .Returns<DownloadProtocol, int>((v, i) => _downloadClients.FirstOrDefault(d => d.Protocol == v));
+                .Returns<DownloadProtocol, int, bool>((v, i, f) => _downloadClients.FirstOrDefault(d => d.Protocol == v));
 
             var episodes = Builder<Album>.CreateListOfSize(2)
                 .TheFirst(1).With(s => s.Id = 12)
