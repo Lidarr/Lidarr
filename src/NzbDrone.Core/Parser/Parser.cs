Sonarr/Sonarr

Inline 'out' variable declarations

src/NzbDrone.Core/Parser/Parser.cs
@@ -733,10 +733,8 @@ namespace NzbDrone.Core.Parser
 
         public static string CleanSeriesTitle(this string title)
         {
-            long number = 0;
-
             // If Title only contains numbers return it as is.
-            if (long.TryParse(title, out number))
+            if (long.TryParse(title, out _))
             {
                 return title;
             }
@@ -838,9 +836,8 @@ namespace NzbDrone.Core.Parser
             if (matches.Count != 0)
             {
                 var group = matches.OfType<Match>().Last().Groups["releasegroup"].Value;
-                int groupIsNumeric;
 
-                if (int.TryParse(group, out groupIsNumeric))
+                if (int.TryParse(group, out _))
                 {
                     return null;
                 }
@@ -904,8 +901,7 @@ namespace NzbDrone.Core.Parser
             var seriesName = matchCollection[0].Groups["title"].Value.Replace('.', ' ').Replace('_', ' ');
             seriesName = RequestInfoRegex.Replace(seriesName, "").Trim(' ');
 
-            int airYear;
-            int.TryParse(matchCollection[0].Groups["airyear"].Value, out airYear);
+            int.TryParse(matchCollection[0].Groups["airyear"].Value, out var airYear);
 
             int lastSeasonEpisodeStringIndex = matchCollection[0].Groups["title"].EndIndex();
 
@@ -1007,8 +1003,7 @@ namespace NzbDrone.Core.Parser
 
                 foreach (Capture seasonCapture in matchCollection[0].Groups["season"].Captures)
                 {
-                    int parsedSeason;
-                    if (int.TryParse(seasonCapture.Value, out parsedSeason))
+                    if (int.TryParse(seasonCapture.Value, out var parsedSeason))
                     {
                         seasons.Add(parsedSeason);
 
@@ -1168,11 +1163,9 @@ namespace NzbDrone.Core.Parser
 
         private static int ParseNumber(string value)
         {
-            int number;
-
             var normalized = value.Normalize(NormalizationForm.FormKC);
 
-            if (int.TryParse(normalized, out number))
+            if (int.TryParse(normalized, out var number))
             {
                 return number;
             }
@@ -1189,11 +1182,9 @@ namespace NzbDrone.Core.Parser
 
         private static decimal ParseDecimal(string value)
         {
-            decimal number;
-
             var normalized = value.Normalize(NormalizationForm.FormKC);
 
-            if (decimal.TryParse(normalized, NumberStyles.Float, CultureInfo.InvariantCulture, out number))
+            if (decimal.TryParse(normalized, NumberStyles.Float, CultureInfo.InvariantCulture, out var number))
             {
                 return number;
             }

Lidarr/Lidarr

(cherry picked from commit 281add47de1d3940990156c841362125dea9cc7d)
Closes #3748

src/NzbDrone.Core/Parser/Parser.cs
@@ -560,9 +560,8 @@ namespace NzbDrone.Core.Parser
             if (matches.Count != 0)
             {
                 var group = matches.OfType<Match>().Last().Groups["releasegroup"].Value;
-                int groupIsNumeric;
 
-                if (int.TryParse(group, out groupIsNumeric))
+                if (int.TryParse(group, out _))
                 {
                     return null;
                 }
@@ -706,8 +705,7 @@ namespace NzbDrone.Core.Parser
             albumTitle = RequestInfoRegex.Replace(albumTitle, "").Trim(' ');
             releaseVersion = RequestInfoRegex.Replace(releaseVersion, "").Trim(' ');
 
-            int releaseYear;
-            int.TryParse(matchCollection[0].Groups["releaseyear"].Value, out releaseYear);
+            int.TryParse(matchCollection[0].Groups["releaseyear"].Value, out var releaseYear);
 
             ParsedAlbumInfo result;
 
@@ -724,10 +722,8 @@ namespace NzbDrone.Core.Parser
 
             if (matchCollection[0].Groups["discography"].Success)
             {
-                int discStart;
-                int discEnd;
-                int.TryParse(matchCollection[0].Groups["startyear"].Value, out discStart);
-                int.TryParse(matchCollection[0].Groups["endyear"].Value, out discEnd);
+                int.TryParse(matchCollection[0].Groups["startyear"].Value, out var discStart);
+                int.TryParse(matchCollection[0].Groups["endyear"].Value, out var discEnd);
                 result.Discography = true;
 
                 if (discStart > 0 && discEnd > 0)
@@ -805,9 +801,7 @@ namespace NzbDrone.Core.Parser
 
         private static int ParseNumber(string value)
         {
-            int number;
-
-            if (int.TryParse(value, out number))
+            if (int.TryParse(value, out var number))
             {
                 return number;
             }