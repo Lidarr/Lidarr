Sonarr/Sonarr

Minor improvements in health checks

src/NzbDrone.Core/HealthCheck/Checks/MountCheck.cs
@@ -19,10 +19,10 @@ namespace NzbDrone.Core.HealthCheck.Checks
         {
             // Not best for optimization but due to possible symlinks and junctions, we get mounts based on series path so internals can handle mount resolution.
             var mounts = _seriesService.GetAllSeriesPaths()
-                                       .Select(s => _diskProvider.GetMount(s.Value))
-                                       .Where(m => m != null && m.MountOptions != null && m.MountOptions.IsReadOnly)
-                                       .DistinctBy(m => m.RootDirectory)
-                                       .ToList();
+                .Select(s => _diskProvider.GetMount(s.Value))
+                .Where(m => m is { MountOptions.IsReadOnly: true })
+                .DistinctBy(m => m.RootDirectory)
+                .ToList();
 
             if (mounts.Any())
             {

Lidarr/Lidarr

(cherry picked from commit a22f598b0c129110f2a3b663e9b40c84f3a1f02b)
Closes #3752

src/NzbDrone.Core/HealthCheck/Checks/MountCheck.cs
@@ -21,10 +21,10 @@ namespace NzbDrone.Core.HealthCheck.Checks
         {
             // Not best for optimization but due to possible symlinks and junctions, we get mounts based on series path so internals can handle mount resolution.
             var mounts = _artistService.AllArtistPaths()
-                                       .Select(path => _diskProvider.GetMount(path.Value))
-                                       .Where(m => m != null && m.MountOptions != null && m.MountOptions.IsReadOnly)
-                                       .DistinctBy(m => m.RootDirectory)
-                                       .ToList();
+                .Select(path => _diskProvider.GetMount(path.Value))
+                .Where(m => m is { MountOptions.IsReadOnly: true })
+                .DistinctBy(m => m.RootDirectory)
+                .ToList();
 
             if (mounts.Any())
             {