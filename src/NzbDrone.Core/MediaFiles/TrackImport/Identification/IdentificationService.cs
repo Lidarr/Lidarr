3066c80dc9a0f1c28dcacdbf3cc481288726c8b7
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-06-29 08:56:10 +0300

    Prevent `Sequence contains no elements` when album isn't synced to Lidarr

--- src/NzbDrone.Core/MediaFiles/TrackImport/Identification/IdentificationService.cs
+++ src/NzbDrone.Core/MediaFiles/TrackImport/Identification/IdentificationService.cs
@@ -155,6 +155,7 @@ namespace NzbDrone.Core.MediaFiles.TrackImport.Identification
         private bool ShouldFingerprint(LocalAlbumRelease localAlbumRelease)
         {
             var worstTrackMatchDist = localAlbumRelease.TrackMapping?.Mapping
+                .DefaultIfEmpty()
                 .MaxBy(x => x.Value.Item2.NormalizedDistance())
                 .Value.Item2.NormalizedDistance() ?? 1.0;
 
