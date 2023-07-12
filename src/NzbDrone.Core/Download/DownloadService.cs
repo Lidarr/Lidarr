Sonarr/Sonarr

New: Download Client Tags
Closes #402

src/NzbDrone.Core/Download/DownloadService.cs
@@ -53,9 +53,11 @@ namespace NzbDrone.Core.Download
         {
             var filterBlockedClients = remoteEpisode.Release.PendingReleaseReason == PendingReleaseReason.DownloadClientUnavailable;
 
+            var tags = remoteEpisode.Series?.Tags;
+
             var downloadClient = downloadClientId.HasValue
                 ? _downloadClientProvider.Get(downloadClientId.Value)
-                : _downloadClientProvider.GetDownloadClient(remoteEpisode.Release.DownloadProtocol, remoteEpisode.Release.IndexerId, filterBlockedClients);
+                : _downloadClientProvider.GetDownloadClient(remoteEpisode.Release.DownloadProtocol, remoteEpisode.Release.IndexerId, filterBlockedClients, tags);
 
             DownloadReport(remoteEpisode, downloadClient);
         }

src/NzbDrone.Core/Tags/TagDetails.cs
@@ -14,7 +14,15 @@ namespace NzbDrone.Core.Tags
         public List<int> ImportListIds { get; set; }
         public List<int> IndexerIds { get; set; }
         public List<int> AutoTagIds { get; set; }
+        public List<int> DownloadClientIds { get; set; }
 
-        public bool InUse => SeriesIds.Any() || NotificationIds.Any() || RestrictionIds.Any() || DelayProfileIds.Any() || ImportListIds.Any() || IndexerIds.Any() || AutoTagIds.Any();
+        public bool InUse => SeriesIds.Any() ||
+                             NotificationIds.Any() ||
+                             RestrictionIds.Any() ||
+                             DelayProfileIds.Any() ||
+                             ImportListIds.Any() ||
+                             IndexerIds.Any() ||
+                             AutoTagIds.Any() ||
+                             DownloadClientIds.Any();
     }
 }

Lidarr/Lidarr

(cherry picked from commit f6ae9fd6c5173cbf1540341fa99d2f120be1d28e)

src/NzbDrone.Core/Download/DownloadService.cs
@@ -56,7 +56,8 @@ namespace NzbDrone.Core.Download
 
             var downloadTitle = remoteAlbum.Release.Title;
             var filterBlockedClients = remoteAlbum.Release.PendingReleaseReason == PendingReleaseReason.DownloadClientUnavailable;
-            var downloadClient = _downloadClientProvider.GetDownloadClient(remoteAlbum.Release.DownloadProtocol, remoteAlbum.Release.IndexerId, filterBlockedClients);
+            var tags = remoteAlbum.Artist?.Tags;
+            var downloadClient = _downloadClientProvider.GetDownloadClient(remoteAlbum.Release.DownloadProtocol, remoteAlbum.Release.IndexerId, filterBlockedClients, tags);
 
             if (downloadClient == null)
             {

src/NzbDrone.Core/Tags/TagDetails.cs
@@ -14,13 +14,15 @@ namespace NzbDrone.Core.Tags
         public List<int> ImportListIds { get; set; }
         public List<int> RootFolderIds { get; set; }
         public List<int> IndexerIds { get; set; }
+        public List<int> DownloadClientIds { get; set; }
 
-        public bool InUse
-        {
-            get
-            {
-                return ArtistIds.Any() || NotificationIds.Any() || RestrictionIds.Any() || DelayProfileIds.Any() || ImportListIds.Any() || RootFolderIds.Any() || IndexerIds.Any();
-            }
-        }
+        public bool InUse => ArtistIds.Any() ||
+                             NotificationIds.Any() ||
+                             RestrictionIds.Any() ||
+                             DelayProfileIds.Any() ||
+                             ImportListIds.Any() ||
+                             RootFolderIds.Any() ||
+                             IndexerIds.Any() ||
+                             DownloadClientIds.Any();
     }
 }