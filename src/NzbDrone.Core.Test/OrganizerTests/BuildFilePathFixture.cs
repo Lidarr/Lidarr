Sonarr/Sonarr

Fixed: Include extension when calculating maximum episode title length when renaming files
Fixed: Option to override max filename length with MAX_NAME environment variable

Closes #3888

src/NzbDrone.Core.Test/OrganizerTests/BuildFilePathFixture.cs
@@ -1,9 +1,11 @@
+using System.Linq;
 using FizzWare.NBuilder;
 using FluentAssertions;
 using NUnit.Framework;
+using NzbDrone.Core.MediaFiles;
 using NzbDrone.Core.Organizer;
-using NzbDrone.Core.Tv;
 using NzbDrone.Core.Test.Framework;
+using NzbDrone.Core.Tv;
 using NzbDrone.Test.Common;
 
 namespace NzbDrone.Core.Test.OrganizerTests
@@ -32,16 +34,26 @@ namespace NzbDrone.Core.Test.OrganizerTests
         [TestCase("30 Rock - S00E05 - Episode Title", 0, true, "Season {season}", @"C:\Test\30 Rock\MySpecials\30 Rock - S00E05 - Episode Title.mkv")]
         public void CalculateFilePath_SeasonFolder_SingleNumber(string filename, int seasonNumber, bool useSeasonFolder, string seasonFolderFormat, string expectedPath)
         {
+            var fakeEpisodes = Builder<Episode>.CreateListOfSize(1)
+                .All()
+                .With(s => s.Title = "Episode Title")
+                .With(s => s.SeasonNumber = seasonNumber)
+                .With(s => s.EpisodeNumber = 5)
+                .Build().ToList();
             var fakeSeries = Builder<Series>.CreateNew()
                 .With(s => s.Title = "30 Rock")
                 .With(s => s.Path = @"C:\Test\30 Rock".AsOsAgnostic())
                 .With(s => s.SeasonFolder = useSeasonFolder)
+                .With(s => s.SeriesType = SeriesTypes.Standard)
+                .Build();
+            var fakeEpisodeFile = Builder<EpisodeFile>.CreateNew()
+                .With(s => s.SceneName = filename)
                 .Build();
 
             namingConfig.SeasonFolderFormat = seasonFolderFormat;
             namingConfig.SpecialsFolderFormat = "MySpecials";
 
-            Subject.BuildFilePath(fakeSeries, seasonNumber, filename, ".mkv").Should().Be(expectedPath.AsOsAgnostic());
+            Subject.BuildFilePath(fakeEpisodes, fakeSeries, fakeEpisodeFile, ".mkv").Should().Be(expectedPath.AsOsAgnostic());
         }
 
         [Test]
@@ -51,15 +63,25 @@ namespace NzbDrone.Core.Test.OrganizerTests
             var seasonNumber = 1;
             var expectedPath = @"C:\Test\NCIS - Los Angeles\NCIS - Los Angeles Season 1\S01E05 - Episode Title.mkv";
 
+            var fakeEpisodes = Builder<Episode>.CreateListOfSize(1)
+                .All()
+                .With(s => s.Title = "Episode Title")
+                .With(s => s.SeasonNumber = seasonNumber)
+                .With(s => s.EpisodeNumber = 5)
+                .Build().ToList();
             var fakeSeries = Builder<Series>.CreateNew()
                 .With(s => s.Title = "NCIS: Los Angeles")
                 .With(s => s.Path = @"C:\Test\NCIS - Los Angeles".AsOsAgnostic())
                 .With(s => s.SeasonFolder = true)
+                .With(s => s.SeriesType = SeriesTypes.Standard)
+                .Build();
+            var fakeEpisodeFile = Builder<EpisodeFile>.CreateNew()
+                .With(s => s.SceneName = filename)
                 .Build();
 
             namingConfig.SeasonFolderFormat = "{Series Title} Season {season:0}";
-
-            Subject.BuildFilePath(fakeSeries, seasonNumber, filename, ".mkv").Should().Be(expectedPath.AsOsAgnostic());
+            
+            Subject.BuildFilePath(fakeEpisodes, fakeSeries, fakeEpisodeFile, ".mkv").Should().Be(expectedPath.AsOsAgnostic());
         }
     }
 }

Lidarr/Lidarr

(cherry picked from commit 6efee036a826027391433b7d0c954ebc1a75c679)

src/NzbDrone.Core.Test/OrganizerTests/BuildFilePathFixture.cs
@@ -1,6 +1,8 @@
+using System.Linq;
 using FizzWare.NBuilder;
 using FluentAssertions;
 using NUnit.Framework;
+using NzbDrone.Core.MediaFiles;
 using NzbDrone.Core.Music;
 using NzbDrone.Core.Organizer;
 using NzbDrone.Core.Test.Framework;
@@ -9,7 +11,6 @@ using NzbDrone.Test.Common;
 namespace NzbDrone.Core.Test.OrganizerTests
 {
     [TestFixture]
-
     public class BuildFilePathFixture : CoreTest<FileNameBuilder>
     {
         private NamingConfig _namingConfig;
@@ -29,12 +30,24 @@ namespace NzbDrone.Core.Test.OrganizerTests
             var filename = @"02 - Track Title";
             var expectedPath = @"C:\Test\Fake- The Artist\02 - Track Title.flac";
 
+            var fakeTracks = Builder<Track>.CreateListOfSize(1)
+                .All()
+                .With(s => s.Title = "Episode Title")
+                .With(s => s.AbsoluteTrackNumber = 5)
+                .Build().ToList();
             var fakeArtist = Builder<Artist>.CreateNew()
                 .With(s => s.Name = "Fake: The Artist")
                 .With(s => s.Path = @"C:\Test\Fake- The Artist".AsOsAgnostic())
                 .Build();
+            var fakeAlbum = Builder<Album>.CreateNew()
+                .With(e => e.ArtistId = fakeArtist.Id)
+                .Build();
+            var fakeTrackFile = Builder<TrackFile>.CreateNew()
+                .With(s => s.SceneName = filename)
+                .With(f => f.Artist = fakeArtist)
+                .Build();
 
-            Subject.BuildTrackFilePath(fakeArtist, filename, ".flac").Should().Be(expectedPath.AsOsAgnostic());
+            Subject.BuildTrackFilePath(fakeTracks, fakeArtist, fakeAlbum, fakeTrackFile, ".flac").Should().Be(expectedPath.AsOsAgnostic());
         }
     }
 }