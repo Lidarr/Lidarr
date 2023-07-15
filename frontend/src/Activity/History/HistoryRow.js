ColinHebert/Sonarr

New: Show updated Custom Format Score in history

frontend/src/Activity/History/HistoryRow.js
@@ -63,6 +63,7 @@ class HistoryRow extends Component {
       languageCutoffNotMet,
       quality,
       customFormats,
+      customFormatScore,
       qualityCutoffNotMet,
       eventType,
       sourceTitle,
@@ -213,7 +214,7 @@ class HistoryRow extends Component {
                   key={name}
                   className={styles.customFormatScore}
                 >
-                  {formatPreferredWordScore(data.customFormatScore)}
+                  {formatPreferredWordScore(customFormatScore)}
                 </TableRowCell>
               );
             }
@@ -282,6 +283,7 @@ HistoryRow.propTypes = {
   languageCutoffNotMet: PropTypes.bool.isRequired,
   quality: PropTypes.object.isRequired,
   customFormats: PropTypes.arrayOf(PropTypes.object),
+  customFormatSSore: PropTypes.number.isRequired,
   qualityCutoffNotMet: PropTypes.bool.isRequired,
   eventType: PropTypes.string.isRequired,
   sourceTitle: PropTypes.string.isRequired,

src/Sonarr.Api.V3/History/HistoryResource.cs
@@ -19,6 +19,7 @@ namespace Sonarr.Api.V3.History
         public List<Language> Languages { get; set; }
         public QualityModel Quality { get; set; }
         public List<CustomFormatResource> CustomFormats { get; set; }
+        public int CustomFormatScore { get; set; }
         public bool QualityCutoffNotMet { get; set; }
         public DateTime Date { get; set; }
         public string DownloadId { get; set; }
@@ -40,6 +41,9 @@ namespace Sonarr.Api.V3.History
                 return null;
             }
 
+            var customFormats = formatCalculator.ParseCustomFormat(model, model.Series);
+            var customFormatScore = model.Series.QualityProfile.Value.CalculateCustomFormatScore(customFormats);
+
             return new HistoryResource
             {
                 Id = model.Id,
@@ -49,7 +53,8 @@ namespace Sonarr.Api.V3.History
                 SourceTitle = model.SourceTitle,
                 Languages = model.Languages,
                 Quality = model.Quality,
-                CustomFormats = formatCalculator.ParseCustomFormat(model, model.Series).ToResource(false),
+                CustomFormats = customFormats.ToResource(false),
+                CustomFormatScore = customFormatScore,
 
                 // QualityCutoffNotMet
                 Date = model.Date,

Lidarr/Lidarr

(cherry picked from commit 6dcfc661a12f4c6f03345235fb5531c866666a6a)
Closes #3291

frontend/src/Activity/History/HistoryRow.js
@@ -58,6 +58,7 @@ class HistoryRow extends Component {
       track,
       quality,
       customFormats,
+      customFormatScore,
       qualityCutoffNotMet,
       eventType,
       sourceTitle,
@@ -186,7 +187,7 @@ class HistoryRow extends Component {
                   key={name}
                   className={styles.customFormatScore}
                 >
-                  {formatPreferredWordScore(data.customFormatScore)}
+                  {formatPreferredWordScore(customFormatScore)}
                 </TableRowCell>
               );
             }
@@ -254,6 +255,7 @@ HistoryRow.propTypes = {
   track: PropTypes.object,
   quality: PropTypes.object.isRequired,
   customFormats: PropTypes.arrayOf(PropTypes.object),
+  customFormatScore: PropTypes.number.isRequired,
   qualityCutoffNotMet: PropTypes.bool.isRequired,
   eventType: PropTypes.string.isRequired,
   sourceTitle: PropTypes.string.isRequired,

src/Lidarr.Api.V1/History/HistoryResource.cs
@@ -19,6 +19,7 @@ namespace Lidarr.Api.V1.History
         public string SourceTitle { get; set; }
         public QualityModel Quality { get; set; }
         public List<CustomFormatResource> CustomFormats { get; set; }
+        public int CustomFormatScore { get; set; }
         public bool QualityCutoffNotMet { get; set; }
         public DateTime Date { get; set; }
         public string DownloadId { get; set; }
@@ -41,6 +42,9 @@ namespace Lidarr.Api.V1.History
                 return null;
             }
 
+            var customFormats = formatCalculator.ParseCustomFormat(model, model.Artist);
+            var customFormatScore = model.Artist?.QualityProfile?.Value?.CalculateCustomFormatScore(customFormats) ?? 0;
+
             return new HistoryResource
             {
                 Id = model.Id,
@@ -50,7 +54,8 @@ namespace Lidarr.Api.V1.History
                 TrackId = model.TrackId,
                 SourceTitle = model.SourceTitle,
                 Quality = model.Quality,
-                CustomFormats = formatCalculator.ParseCustomFormat(model, model.Artist).ToResource(false),
+                CustomFormats = customFormats.ToResource(false),
+                CustomFormatScore = customFormatScore,
 
                 // QualityCutoffNotMet
                 Date = model.Date,