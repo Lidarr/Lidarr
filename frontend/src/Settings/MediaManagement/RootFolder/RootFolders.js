eb9f70accb40ce00f3072fe45122074fde71d5f7
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-07-29 09:57:33 +0300

    Fix lint UI

--- frontend/src/Settings/MediaManagement/RootFolder/RootFolders.js
+++ frontend/src/Settings/MediaManagement/RootFolder/RootFolders.js
@@ -5,7 +5,6 @@ import FieldSet from 'Components/FieldSet';
 import Icon from 'Components/Icon';
 import PageSectionContent from 'Components/Page/PageSectionContent';
 import { icons } from 'Helpers/Props';
-import sortByName from 'Utilities/Array/sortByName';
 import translate from 'Utilities/String/translate';
 import EditRootFolderModalConnector from './EditRootFolderModalConnector';
 import RootFolder from './RootFolder';
