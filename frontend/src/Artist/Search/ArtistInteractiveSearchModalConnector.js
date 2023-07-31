86776e6d2e07a5b0c490b0ad12bbc90e792adf65
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-07-31 18:06:31 +0300

    Fixed: Clear pending releases to prevent stale results

--- frontend/src/Artist/Search/ArtistInteractiveSearchModalConnector.js
+++ frontend/src/Artist/Search/ArtistInteractiveSearchModalConnector.js
@@ -1,9 +1,19 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { cancelFetchReleases, clearReleases } from 'Store/Actions/releaseActions';
 import ArtistInteractiveSearchModal from './ArtistInteractiveSearchModal';
 
 function createMapDispatchToProps(dispatch, props) {
   return {
+    dispatchCancelFetchReleases() {
+      dispatch(cancelFetchReleases());
+    },
+
+    dispatchClearReleases() {
+      dispatch(clearReleases());
+    },
+
     onModalClose() {
       dispatch(cancelFetchReleases());
       dispatch(clearReleases());
@@ -12,4 +22,38 @@ function createMapDispatchToProps(dispatch, props) {
   };
 }
 
-export default connect(null, createMapDispatchToProps)(ArtistInteractiveSearchModal);
+class ArtistInteractiveSearchModalConnector extends Component {
+
+  //
+  // Lifecycle
+
+  componentWillUnmount() {
+    this.props.dispatchCancelFetchReleases();
+    this.props.dispatchClearReleases();
+  }
+
+  //
+  // Render
+
+  render() {
+    const {
+      dispatchCancelFetchReleases,
+      dispatchClearReleases,
+      ...otherProps
+    } = this.props;
+
+    return (
+      <ArtistInteractiveSearchModal
+        {...otherProps}
+      />
+    );
+  }
+}
+
+ArtistInteractiveSearchModalConnector.propTypes = {
+  ...ArtistInteractiveSearchModal.propTypes,
+  dispatchCancelFetchReleases: PropTypes.func.isRequired,
+  dispatchClearReleases: PropTypes.func.isRequired
+};
+
+export default connect(null, createMapDispatchToProps)(ArtistInteractiveSearchModalConnector);
