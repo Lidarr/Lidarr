Sonarr/Sonarr

New: Notifications (Connect) Status

src/NzbDrone.Core/Notifications/NotificationFactory.cs
@@ -9,85 +9,166 @@ namespace NzbDrone.Core.Notifications
 {
     public interface INotificationFactory : IProviderFactory<INotification, NotificationDefinition>
     {
-        List<INotification> OnGrabEnabled();
-        List<INotification> OnDownloadEnabled();
-        List<INotification> OnUpgradeEnabled();
-        List<INotification> OnRenameEnabled();
-        List<INotification> OnSeriesAddEnabled();
-        List<INotification> OnSeriesDeleteEnabled();
-        List<INotification> OnEpisodeFileDeleteEnabled();
-        List<INotification> OnEpisodeFileDeleteForUpgradeEnabled();
-        List<INotification> OnHealthIssueEnabled();
-        List<INotification> OnHealthRestoredEnabled();
-        List<INotification> OnApplicationUpdateEnabled();
-        List<INotification> OnManualInteractionEnabled();
+        List<INotification> OnGrabEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnDownloadEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnUpgradeEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnRenameEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnSeriesAddEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnSeriesDeleteEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnEpisodeFileDeleteEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnEpisodeFileDeleteForUpgradeEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnHealthIssueEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnHealthRestoredEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnApplicationUpdateEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnManualInteractionEnabled(bool filterBlockedNotifications = true);
     }
 
     public class NotificationFactory : ProviderFactory<INotification, NotificationDefinition>, INotificationFactory
     {
-        public NotificationFactory(INotificationRepository providerRepository, IEnumerable<INotification> providers, IServiceProvider container, IEventAggregator eventAggregator, Logger logger)
+        private readonly INotificationStatusService _notificationStatusService;
+        private readonly Logger _logger;
+
+        public NotificationFactory(INotificationStatusService notificationStatusService, INotificationRepository providerRepository, IEnumerable<INotification> providers, IServiceProvider container, IEventAggregator eventAggregator, Logger logger)
             : base(providerRepository, providers, container, eventAggregator, logger)
         {
+            _notificationStatusService = notificationStatusService;
+            _logger = logger;
         }
 
-        public List<INotification> OnGrabEnabled()
+        public List<INotification> OnGrabEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnGrab)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnGrab).ToList();
         }
 
-        public List<INotification> OnDownloadEnabled()
+        public List<INotification> OnDownloadEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnDownload)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnDownload).ToList();
         }
 
-        public List<INotification> OnUpgradeEnabled()
+        public List<INotification> OnUpgradeEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnUpgrade)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnUpgrade).ToList();
         }
 
-        public List<INotification> OnRenameEnabled()
+        public List<INotification> OnRenameEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnRename)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnRename).ToList();
         }
 
-        public List<INotification> OnSeriesAddEnabled()
+        public List<INotification> OnSeriesAddEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnSeriesAdd)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnSeriesAdd).ToList();
         }
 
-        public List<INotification> OnSeriesDeleteEnabled()
+        public List<INotification> OnSeriesDeleteEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnSeriesDelete)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnSeriesDelete).ToList();
         }
 
-        public List<INotification> OnEpisodeFileDeleteEnabled()
+        public List<INotification> OnEpisodeFileDeleteEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnEpisodeFileDelete)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnEpisodeFileDelete).ToList();
         }
 
-        public List<INotification> OnEpisodeFileDeleteForUpgradeEnabled()
+        public List<INotification> OnEpisodeFileDeleteForUpgradeEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnEpisodeFileDeleteForUpgrade)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnEpisodeFileDeleteForUpgrade).ToList();
         }
 
-        public List<INotification> OnHealthIssueEnabled()
+        public List<INotification> OnHealthIssueEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnHealthIssue)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnHealthIssue).ToList();
         }
 
-        public List<INotification> OnHealthRestoredEnabled()
+        public List<INotification> OnHealthRestoredEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnHealthRestored)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnHealthRestored).ToList();
         }
 
-        public List<INotification> OnApplicationUpdateEnabled()
+        public List<INotification> OnApplicationUpdateEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnApplicationUpdate)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnApplicationUpdate).ToList();
         }
 
-        public List<INotification> OnManualInteractionEnabled()
+        public List<INotification> OnManualInteractionEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnManualInteractionRequired)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnManualInteractionRequired).ToList();
+        }
+
+        private IEnumerable<INotification> FilterBlockedNotifications(IEnumerable<INotification> notifications)
+        {
+            var blockedNotifications = _notificationStatusService.GetBlockedProviders().ToDictionary(v => v.ProviderId, v => v);
+
+            foreach (var notification in notifications)
+            {
+                if (blockedNotifications.TryGetValue(notification.Definition.Id, out var notificationStatus))
+                {
+                    _logger.Debug("Temporarily ignoring notification {0} till {1} due to recent failures.", notification.Definition.Name, notificationStatus.DisabledTill.Value.ToLocalTime());
+                    continue;
+                }
+
+                yield return notification;
+            }
         }
 
         public override void SetProviderCharacteristics(INotification provider, NotificationDefinition definition)

Lidarr/Lidarr

(cherry picked from commit e3545801721e00d4e5cac3fa534e66dcbe9d2d05)

src/NzbDrone.Core/Notifications/NotificationFactory.cs
@@ -9,85 +9,166 @@ namespace NzbDrone.Core.Notifications
 {
     public interface INotificationFactory : IProviderFactory<INotification, NotificationDefinition>
     {
-        List<INotification> OnGrabEnabled();
-        List<INotification> OnReleaseImportEnabled();
-        List<INotification> OnUpgradeEnabled();
-        List<INotification> OnRenameEnabled();
-        List<INotification> OnAlbumDeleteEnabled();
-        List<INotification> OnArtistDeleteEnabled();
-        List<INotification> OnHealthIssueEnabled();
-        List<INotification> OnHealthRestoredEnabled();
-        List<INotification> OnDownloadFailureEnabled();
-        List<INotification> OnImportFailureEnabled();
-        List<INotification> OnTrackRetagEnabled();
-        List<INotification> OnApplicationUpdateEnabled();
+        List<INotification> OnGrabEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnReleaseImportEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnUpgradeEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnRenameEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnAlbumDeleteEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnArtistDeleteEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnHealthIssueEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnHealthRestoredEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnDownloadFailureEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnImportFailureEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnTrackRetagEnabled(bool filterBlockedNotifications = true);
+        List<INotification> OnApplicationUpdateEnabled(bool filterBlockedNotifications = true);
     }
 
     public class NotificationFactory : ProviderFactory<INotification, NotificationDefinition>, INotificationFactory
     {
-        public NotificationFactory(INotificationRepository providerRepository, IEnumerable<INotification> providers, IServiceProvider container, IEventAggregator eventAggregator, Logger logger)
+        private readonly INotificationStatusService _notificationStatusService;
+        private readonly Logger _logger;
+
+        public NotificationFactory(INotificationStatusService notificationStatusService, INotificationRepository providerRepository, IEnumerable<INotification> providers, IServiceProvider container, IEventAggregator eventAggregator, Logger logger)
             : base(providerRepository, providers, container, eventAggregator, logger)
         {
+            _notificationStatusService = notificationStatusService;
+            _logger = logger;
         }
 
-        public List<INotification> OnGrabEnabled()
+        public List<INotification> OnGrabEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnGrab)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnGrab).ToList();
         }
 
-        public List<INotification> OnReleaseImportEnabled()
+        public List<INotification> OnReleaseImportEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnReleaseImport)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnReleaseImport).ToList();
         }
 
-        public List<INotification> OnUpgradeEnabled()
+        public List<INotification> OnUpgradeEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnUpgrade)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnUpgrade).ToList();
         }
 
-        public List<INotification> OnRenameEnabled()
+        public List<INotification> OnRenameEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnRename)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnRename).ToList();
         }
 
-        public List<INotification> OnAlbumDeleteEnabled()
+        public List<INotification> OnAlbumDeleteEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnAlbumDelete)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnAlbumDelete).ToList();
         }
 
-        public List<INotification> OnArtistDeleteEnabled()
+        public List<INotification> OnArtistDeleteEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnArtistDelete)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnArtistDelete).ToList();
         }
 
-        public List<INotification> OnHealthIssueEnabled()
+        public List<INotification> OnHealthIssueEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnHealthIssue)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnHealthIssue).ToList();
         }
 
-        public List<INotification> OnHealthRestoredEnabled()
+        public List<INotification> OnHealthRestoredEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnHealthRestored)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnHealthRestored).ToList();
         }
 
-        public List<INotification> OnDownloadFailureEnabled()
+        public List<INotification> OnDownloadFailureEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnDownloadFailure)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnDownloadFailure).ToList();
         }
 
-        public List<INotification> OnImportFailureEnabled()
+        public List<INotification> OnImportFailureEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnImportFailure)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnImportFailure).ToList();
         }
 
-        public List<INotification> OnTrackRetagEnabled()
+        public List<INotification> OnTrackRetagEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnTrackRetag)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnTrackRetag).ToList();
         }
 
-        public List<INotification> OnApplicationUpdateEnabled()
+        public List<INotification> OnApplicationUpdateEnabled(bool filterBlockedNotifications = true)
         {
+            if (filterBlockedNotifications)
+            {
+                return FilterBlockedNotifications(GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnApplicationUpdate)).ToList();
+            }
+
             return GetAvailableProviders().Where(n => ((NotificationDefinition)n.Definition).OnApplicationUpdate).ToList();
+        }
+
+        private IEnumerable<INotification> FilterBlockedNotifications(IEnumerable<INotification> notifications)
+        {
+            var blockedNotifications = _notificationStatusService.GetBlockedProviders().ToDictionary(v => v.ProviderId, v => v);
+
+            foreach (var notification in notifications)
+            {
+                if (blockedNotifications.TryGetValue(notification.Definition.Id, out var notificationStatus))
+                {
+                    _logger.Debug("Temporarily ignoring notification {0} till {1} due to recent failures.", notification.Definition.Name, notificationStatus.DisabledTill.Value.ToLocalTime());
+                    continue;
+                }
+
+                yield return notification;
+            }
         }
 
         public override void SetProviderCharacteristics(INotification provider, NotificationDefinition definition)