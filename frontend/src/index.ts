Sonarr/Sonarr

UI loading improvements
Fixed: Caching for dynamically loaded JS files
Fixed: Incorrect caching of initialize.js

frontend/src/index.ts
@@ -1,0 +1,19 @@
+import './polyfills';
+import 'Styles/globals.css';
+import './index.css';
+
+const initializeUrl = `${
+  window.Sonarr.urlBase
+}/initialize.json?t=${Date.now()}`;
+const response = await fetch(initializeUrl);
+
+window.Sonarr = await response.json();
+
+/* eslint-disable no-undef, @typescript-eslint/ban-ts-comment */
+// @ts-ignore 2304
+__webpack_public_path__ = `${window.Sonarr.urlBase}/`;
+/* eslint-enable no-undef, @typescript-eslint/ban-ts-comment */
+
+const { bootstrap } = await import('./bootstrap');
+
+await bootstrap();

src/Sonarr.Http/Frontend/InitializeJsonController.cs
@@ -1,0 +1,65 @@
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Common;
+using NzbDrone.Common.EnvironmentInfo;
+using NzbDrone.Core.Analytics;
+using NzbDrone.Core.Configuration;
+
+namespace Sonarr.Http.Frontend
+{
+    [Authorize(Policy = "UI")]
+    [ApiController]
+    public class InitializeJsonController : Controller
+    {
+        private readonly IConfigFileProvider _configFileProvider;
+        private readonly IAnalyticsService _analyticsService;
+
+        private static string _apiKey;
+        private static string _urlBase;
+        private string _generatedContent;
+
+        public InitializeJsonController(IConfigFileProvider configFileProvider,
+                                      IAnalyticsService analyticsService)
+        {
+            _configFileProvider = configFileProvider;
+            _analyticsService = analyticsService;
+
+            _apiKey = configFileProvider.ApiKey;
+            _urlBase = configFileProvider.UrlBase;
+        }
+
+        [HttpGet("/initialize.json")]
+        public IActionResult Index()
+        {
+            return Content(GetContent(), "application/json");
+        }
+
+        private string GetContent()
+        {
+            if (RuntimeInfo.IsProduction && _generatedContent != null)
+            {
+                return _generatedContent;
+            }
+
+            var builder = new StringBuilder();
+            builder.AppendLine("{");
+            builder.AppendLine($"  \"apiRoot\": \"{_urlBase}/api/v3\",");
+            builder.AppendLine($"  \"apiKey\": \"{_apiKey}\",");
+            builder.AppendLine($"  \"release\": \"{BuildInfo.Release}\",");
+            builder.AppendLine($"  \"version\": \"{BuildInfo.Version.ToString()}\",");
+            builder.AppendLine($"  \"instanceName\": \"{_configFileProvider.InstanceName.ToString()}\",");
+            builder.AppendLine($"  \"theme\": \"{_configFileProvider.Theme.ToString()}\",");
+            builder.AppendLine($"  \"branch\": \"{_configFileProvider.Branch.ToLower()}\",");
+            builder.AppendLine($"  \"analytics\": {_analyticsService.IsEnabled.ToString().ToLowerInvariant()},");
+            builder.AppendLine($"  \"userHash\": \"{HashUtil.AnonymousToken()}\",");
+            builder.AppendLine($"  \"urlBase\": \"{_urlBase}\",");
+            builder.AppendLine($"  \"isProduction\": {RuntimeInfo.IsProduction.ToString().ToLowerInvariant()}");
+            builder.AppendLine("}");
+
+            _generatedContent = builder.ToString();
+
+            return _generatedContent;
+        }
+    }
+}

Lidarr/Lidarr

(cherry picked from commit f0cb5b81f140c67fa84162e094cc4e0f3476f5da)
Closes #3883
Closes #3890
Closes #3893

frontend/src/index.ts
@@ -1,0 +1,19 @@
+import './polyfills';
+import 'Styles/globals.css';
+import './index.css';
+
+const initializeUrl = `${
+  window.Lidarr.urlBase
+}/initialize.json?t=${Date.now()}`;
+const response = await fetch(initializeUrl);
+
+window.Lidarr = await response.json();
+
+/* eslint-disable no-undef, @typescript-eslint/ban-ts-comment */
+// @ts-ignore 2304
+__webpack_public_path__ = `${window.Lidarr.urlBase}/`;
+/* eslint-enable no-undef, @typescript-eslint/ban-ts-comment */
+
+const { bootstrap } = await import('./bootstrap');
+
+await bootstrap();

src/Lidarr.Http/Frontend/InitializeJsonController.cs
@@ -1,0 +1,66 @@
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Common;
+using NzbDrone.Common.EnvironmentInfo;
+using NzbDrone.Core.Analytics;
+using NzbDrone.Core.Configuration;
+
+namespace Lidarr.Http.Frontend
+{
+    [Authorize(Policy = "UI")]
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class InitializeJsonController : Controller
+    {
+        private readonly IConfigFileProvider _configFileProvider;
+        private readonly IAnalyticsService _analyticsService;
+
+        private static string _apiKey;
+        private static string _urlBase;
+        private string _generatedContent;
+
+        public InitializeJsonController(IConfigFileProvider configFileProvider,
+                                      IAnalyticsService analyticsService)
+        {
+            _configFileProvider = configFileProvider;
+            _analyticsService = analyticsService;
+
+            _apiKey = configFileProvider.ApiKey;
+            _urlBase = configFileProvider.UrlBase;
+        }
+
+        [HttpGet("/initialize.json")]
+        public IActionResult Index()
+        {
+            return Content(GetContent(), "application/json");
+        }
+
+        private string GetContent()
+        {
+            if (RuntimeInfo.IsProduction && _generatedContent != null)
+            {
+                return _generatedContent;
+            }
+
+            var builder = new StringBuilder();
+            builder.AppendLine("{");
+            builder.AppendLine($"  \"apiRoot\": \"{_urlBase}/api/v1\",");
+            builder.AppendLine($"  \"apiKey\": \"{_apiKey}\",");
+            builder.AppendLine($"  \"release\": \"{BuildInfo.Release}\",");
+            builder.AppendLine($"  \"version\": \"{BuildInfo.Version.ToString()}\",");
+            builder.AppendLine($"  \"instanceName\": \"{_configFileProvider.InstanceName.ToString()}\",");
+            builder.AppendLine($"  \"theme\": \"{_configFileProvider.Theme.ToString()}\",");
+            builder.AppendLine($"  \"branch\": \"{_configFileProvider.Branch.ToLower()}\",");
+            builder.AppendLine($"  \"analytics\": {_analyticsService.IsEnabled.ToString().ToLowerInvariant()},");
+            builder.AppendLine($"  \"userHash\": \"{HashUtil.AnonymousToken()}\",");
+            builder.AppendLine($"  \"urlBase\": \"{_urlBase}\",");
+            builder.AppendLine($"  \"isProduction\": {RuntimeInfo.IsProduction.ToString().ToLowerInvariant()}");
+            builder.AppendLine("}");
+
+            _generatedContent = builder.ToString();
+
+            return _generatedContent;
+        }
+    }
+}