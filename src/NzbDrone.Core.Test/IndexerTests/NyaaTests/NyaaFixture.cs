Sonarr/Sonarr

New: Support for new Nyaa RSS Feed format
Closes #4614

src/NzbDrone.Core.Test/IndexerTests/NyaaTests/NyaaFixture.cs
@@ -18,13 +18,15 @@ namespace NzbDrone.Core.Test.IndexerTests.NyaaTests
         public void Setup()
         {
             Subject.Definition = new IndexerDefinition()
-                {
-                    Name = "Nyaa",
-                    Settings = new NyaaSettings()
-                };
+            {
+                Name = "Nyaa",
+                Settings = new NyaaSettings()
+            };
         }
 
-        [Test]
+/*        [Test]
+        // Legacy Nyaa feed test
+
         public void should_parse_recent_feed_from_Nyaa()
         {
             var recentFeed = ReadAllText(@"Files/Indexers/Nyaa/Nyaa.xml");
@@ -50,8 +52,37 @@ namespace NzbDrone.Core.Test.IndexerTests.NyaaTests
             torrentInfo.Size.Should().Be(2523293286); //2.35 GiB
             torrentInfo.InfoHash.Should().Be(null);
             torrentInfo.MagnetUrl.Should().Be(null);
-            torrentInfo.Peers.Should().Be(2+1);
+            torrentInfo.Peers.Should().Be(2 + 1);
             torrentInfo.Seeders.Should().Be(1);
+        }*/
+
+        [Test]
+        public void should_parse_2021_recent_feed_from_Nyaa()
+        {
+            var recentFeed = ReadAllText(@"Files/Indexers/Nyaa/Nyaa2021.xml");
+
+            Mocker.GetMock<IHttpClient>()
+                .Setup(o => o.Execute(It.Is<HttpRequest>(v => v.Method == HttpMethod.GET)))
+                .Returns<HttpRequest>(r => new HttpResponse(r, new HttpHeader(), recentFeed));
+
+            var releases = Subject.FetchRecent();
+
+            releases.Should().HaveCount(3);
+            releases.First().Should().BeOfType<TorrentInfo>();
+
+            var torrentInfo = releases.First() as TorrentInfo;
+
+            torrentInfo.Title.Should().Be("[Foxy-Subs] Mahouka Koukou no Yuutousei - 08 [720p] [3194D881].mkv");
+            torrentInfo.DownloadProtocol.Should().Be(DownloadProtocol.Torrent);
+            torrentInfo.DownloadUrl.Should().Be("https://nyaa.si/download/1424896.torrent");
+            torrentInfo.InfoUrl.Should().Be("https://nyaa.si/view/1424896");
+            torrentInfo.CommentUrl.Should().BeNullOrEmpty();
+            torrentInfo.Indexer.Should().Be(Subject.Definition.Name);
+            torrentInfo.PublishDate.Should().Be(DateTime.Parse("Tue, 24 Aug 2021 22:18:46"));
+            torrentInfo.Size.Should().Be(639211930); //609.6 MiB
+            torrentInfo.MagnetUrl.Should().Be(null);
+            torrentInfo.Seeders.Should().Be(4);
+            torrentInfo.Peers.Should().Be(3+4);
         }
     }
 }

Lidarr/Lidarr

(cherry picked from commit 40ecdbc12de8b320a4d650aea65a36e8edea77d8)
Closes #2738

src/NzbDrone.Core.Test/IndexerTests/NyaaTests/NyaaFixture.cs
@@ -26,16 +26,18 @@ namespace NzbDrone.Core.Test.IndexerTests.NyaaTests
             };
         }
 
-        [Test]
-        public async Task should_parse_recent_feed_from_Nyaa()
+/*        [Test]
+        // Legacy Nyaa feed test
+
+        public void should_parse_recent_feed_from_Nyaa()
         {
             var recentFeed = ReadAllText(@"Files/Indexers/Nyaa/Nyaa.xml");
 
             Mocker.GetMock<IHttpClient>()
-                .Setup(o => o.ExecuteAsync(It.Is<HttpRequest>(v => v.Method == HttpMethod.Get)))
-                .Returns<HttpRequest>(r => Task.FromResult(new HttpResponse(r, new HttpHeader(), recentFeed)));
+                .Setup(o => o.Execute(It.Is<HttpRequest>(v => v.Method == HttpMethod.Get)))
+                .Returns<HttpRequest>(r => new HttpResponse(r, new HttpHeader(), recentFeed));
 
-            var releases = await Subject.FetchRecent();
+            var releases = Subject.FetchRecent();
 
             releases.Should().HaveCount(4);
             releases.First().Should().BeOfType<TorrentInfo>();
@@ -49,11 +51,40 @@ namespace NzbDrone.Core.Test.IndexerTests.NyaaTests
             torrentInfo.CommentUrl.Should().BeNullOrEmpty();
             torrentInfo.Indexer.Should().Be(Subject.Definition.Name);
             torrentInfo.PublishDate.Should().Be(DateTime.Parse("2014/08/14 18:10:36"));
-            torrentInfo.Size.Should().Be(2523293286); // 2.35 GiB
+            torrentInfo.Size.Should().Be(2523293286); //2.35 GiB
             torrentInfo.InfoHash.Should().Be(null);
             torrentInfo.MagnetUrl.Should().Be(null);
             torrentInfo.Peers.Should().Be(2 + 1);
             torrentInfo.Seeders.Should().Be(1);
+        }*/
+
+        [Test]
+        public async Task should_parse_2021_recent_feed_from_Nyaa()
+        {
+            var recentFeed = ReadAllText(@"Files/Indexers/Nyaa/Nyaa2021.xml");
+
+            Mocker.GetMock<IHttpClient>()
+                .Setup(o => o.ExecuteAsync(It.Is<HttpRequest>(v => v.Method == HttpMethod.Get)))
+                .Returns<HttpRequest>(r => Task.FromResult(new HttpResponse(r, new HttpHeader(), recentFeed)));
+
+            var releases = await Subject.FetchRecent();
+
+            releases.Should().HaveCount(3);
+            releases.First().Should().BeOfType<TorrentInfo>();
+
+            var torrentInfo = releases.First() as TorrentInfo;
+
+            torrentInfo.Title.Should().Be("[Foxy-Subs] Mahouka Koukou no Yuutousei - 08 [720p] [3194D881].mkv");
+            torrentInfo.DownloadProtocol.Should().Be(DownloadProtocol.Torrent);
+            torrentInfo.DownloadUrl.Should().Be("https://nyaa.si/download/1424896.torrent");
+            torrentInfo.InfoUrl.Should().Be("https://nyaa.si/view/1424896");
+            torrentInfo.CommentUrl.Should().BeNullOrEmpty();
+            torrentInfo.Indexer.Should().Be(Subject.Definition.Name);
+            torrentInfo.PublishDate.Should().Be(DateTime.Parse("Tue, 24 Aug 2021 22:18:46"));
+            torrentInfo.Size.Should().Be(639211930); // 609.6 MiB
+            torrentInfo.MagnetUrl.Should().Be(null);
+            torrentInfo.Seeders.Should().Be(4);
+            torrentInfo.Peers.Should().Be(3 + 4);
         }
     }
 }