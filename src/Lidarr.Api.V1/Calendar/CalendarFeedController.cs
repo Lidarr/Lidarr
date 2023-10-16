Sonarr/Sonarr

Fixed: Tag filtering on iCal feed

src/Sonarr.Api.V3/Calendar/CalendarFeedController.cs
@@ -28,15 +28,15 @@ namespace Sonarr.Api.V3.Calendar
         }
 
         [HttpGet("Sonarr.ics")]
-        public IActionResult GetCalendarFeed(int pastDays = 7, int futureDays = 28, string tagList = "", bool unmonitored = false, bool premieresOnly = false, bool asAllDay = false)
+        public IActionResult GetCalendarFeed(int pastDays = 7, int futureDays = 28, string tags = "", bool unmonitored = false, bool premieresOnly = false, bool asAllDay = false)
         {
             var start = DateTime.Today.AddDays(-pastDays);
             var end = DateTime.Today.AddDays(futureDays);
-            var tags = new List<int>();
+            var parsedTags = new List<int>();
 
-            if (tagList.IsNotNullOrWhiteSpace())
+            if (tags.IsNotNullOrWhiteSpace())
             {
-                tags.AddRange(tagList.Split(',').Select(_tagService.GetTag).Select(t => t.Id));
+                parsedTags.AddRange(tags.Split(',').Select(_tagService.GetTag).Select(t => t.Id));
             }
 
             var episodes = _episodeService.EpisodesBetweenDates(start, end, unmonitored);
@@ -59,7 +59,7 @@ namespace Sonarr.Api.V3.Calendar
                     continue;
                 }
 
-                if (tags.Any() && tags.None(series.Tags.Contains))
+                if (parsedTags.Any() && parsedTags.None(series.Tags.Contains))
                 {
                     continue;
                 }

Lidarr/Lidarr

Closes #3547
(cherry picked from commit a989c84260923485b4baa81ba47929979cfe9aa5)

src/Lidarr.Api.V1/Calendar/CalendarFeedController.cs
@@ -28,15 +28,15 @@ namespace Lidarr.Api.V1.Calendar
         }
 
         [HttpGet("Lidarr.ics")]
-        public IActionResult GetCalendarFeed(int pastDays = 7, int futureDays = 28, string tagList = "", bool unmonitored = false)
+        public IActionResult GetCalendarFeed(int pastDays = 7, int futureDays = 28, string tags = "", bool unmonitored = false)
         {
             var start = DateTime.Today.AddDays(-pastDays);
             var end = DateTime.Today.AddDays(futureDays);
-            var tags = new List<int>();
+            var parsedTags = new List<int>();
 
-            if (tagList.IsNotNullOrWhiteSpace())
+            if (tags.IsNotNullOrWhiteSpace())
             {
-                tags.AddRange(tagList.Split(',').Select(_tagService.GetTag).Select(t => t.Id));
+                parsedTags.AddRange(tags.Split(',').Select(_tagService.GetTag).Select(t => t.Id));
             }
 
             var albums = _albumService.AlbumsBetweenDates(start, end, unmonitored);
@@ -53,7 +53,7 @@ namespace Lidarr.Api.V1.Calendar
             {
                 var artist = _artistService.GetArtist(album.ArtistId); // Temp fix TODO: Figure out why Album.Artist is not populated during AlbumsBetweenDates Query
 
-                if (tags.Any() && tags.None(artist.Tags.Contains))
+                if (parsedTags.Any() && parsedTags.None(artist.Tags.Contains))
                 {
                     continue;
                 }