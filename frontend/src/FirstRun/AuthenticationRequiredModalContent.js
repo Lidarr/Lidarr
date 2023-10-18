Radarr/Radarr

Improve messaging in Authentication Required modal

frontend/src/FirstRun/AuthenticationRequiredModalContent.js
@@ -63,20 +63,22 @@ function AuthenticationRequiredModalContent(props) {
           className={styles.authRequiredAlert}
           kind={kinds.WARNING}
         >
-          {translate('AuthenticationRequiredWarning')}
+          {translate('AuthenticationRequiredWarning', { appName: 'Radarr' })}
         </Alert>
 
         {
           isPopulated && !error ?
             <div>
               <FormGroup>
-                <FormLabel>{translate('Authentication')}</FormLabel>
+                <FormLabel>{translate('AuthenticationMethod')}</FormLabel>
 
                 <FormInputGroup
                   type={inputTypes.SELECT}
                   name="authenticationMethod"
                   values={authenticationMethodOptions}
-                  helpText={translate('AuthenticationMethodHelpText')}
+                  helpText={translate('AuthenticationMethodHelpText', { appName: 'Radarr' })}
+                  helpTextWarning={authenticationMethod.value === 'none' ? translate('AuthenticationMethodHelpTextWarning') : undefined}
+                  helpLink="https://wiki.servarr.com/radarr/faq#forced-authentication"
                   onChange={onInputChange}
                   {...authenticationMethod}
                 />
@@ -102,6 +104,7 @@ function AuthenticationRequiredModalContent(props) {
                   type={inputTypes.TEXT}
                   name="username"
                   onChange={onInputChange}
+                  helpTextWarning={username.value === '' ? translate('AuthenticationRequiredUsernameHelpTextWarning') : undefined}
                   {...username}
                 />
               </FormGroup>
@@ -113,6 +116,7 @@ function AuthenticationRequiredModalContent(props) {
                   type={inputTypes.PASSWORD}
                   name="password"
                   onChange={onInputChange}
+                  helpTextWarning={password.value === '' ? translate('AuthenticationRequiredPasswordHelpTextWarning') : undefined}
                   {...password}
                 />
               </FormGroup>

Lidarr/Lidarr

Co-authored-by: bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
(cherry picked from commit 340740377eb83527814f6c9b6f3602321d4a4344)

frontend/src/FirstRun/AuthenticationRequiredModalContent.js
@@ -63,20 +63,22 @@ function AuthenticationRequiredModalContent(props) {
           className={styles.authRequiredAlert}
           kind={kinds.WARNING}
         >
-          {translate('AuthenticationRequiredWarning')}
+          {translate('AuthenticationRequiredWarning', { appName: 'Lidarr' })}
         </Alert>
 
         {
           isPopulated && !error ?
             <div>
               <FormGroup>
-                <FormLabel>{translate('Authentication')}</FormLabel>
+                <FormLabel>{translate('AuthenticationMethod')}</FormLabel>
 
                 <FormInputGroup
                   type={inputTypes.SELECT}
                   name="authenticationMethod"
                   values={authenticationMethodOptions}
-                  helpText={translate('AuthenticationMethodHelpText')}
+                  helpText={translate('AuthenticationMethodHelpText', { appName: 'Lidarr' })}
+                  helpTextWarning={authenticationMethod.value === 'none' ? translate('AuthenticationMethodHelpTextWarning') : undefined}
+                  helpLink="https://wiki.servarr.com/lidarr/faq#forced-authentication"
                   onChange={onInputChange}
                   {...authenticationMethod}
                 />
@@ -102,6 +104,7 @@ function AuthenticationRequiredModalContent(props) {
                   type={inputTypes.TEXT}
                   name="username"
                   onChange={onInputChange}
+                  helpTextWarning={username.value === '' ? translate('AuthenticationRequiredUsernameHelpTextWarning') : undefined}
                   {...username}
                 />
               </FormGroup>
@@ -113,6 +116,7 @@ function AuthenticationRequiredModalContent(props) {
                   type={inputTypes.PASSWORD}
                   name="password"
                   onChange={onInputChange}
+                  helpTextWarning={password.value === '' ? translate('AuthenticationRequiredPasswordHelpTextWarning') : undefined}
                   {...password}
                 />
               </FormGroup>