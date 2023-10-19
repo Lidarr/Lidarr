3790c65c9f2d8025b42f8828016319a5500f8474
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-10-19 13:34:43 +0300

    Fix typo in visible for Queue size
    (cherry picked from commit a171132f989437e3cd85a9689d42e862c6335d1b)

--- frontend/src/Store/Actions/queueActions.js
+++ frontend/src/Store/Actions/queueActions.js
@@ -132,7 +132,7 @@ export const defaultState = {
         name: 'size',
         label: () => translate('Size'),
         isSortable: true,
-        isVisibile: false
+        isVisible: false
       },
       {
         name: 'outputPath',
