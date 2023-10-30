Radarr/Radarr

New: Add Monitored specification to Auto Tagging

src/NzbDrone.Core/AutoTagging/Specifications/MonitoredSpecification.cs
@@ -1,0 +1,28 @@
+using FluentValidation;
+using NzbDrone.Core.Movies;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.AutoTagging.Specifications
+{
+    public class MonitoredSpecificationValidator : AbstractValidator<MonitoredSpecification>
+    {
+    }
+
+    public class MonitoredSpecification : AutoTaggingSpecificationBase
+    {
+        private static readonly MonitoredSpecificationValidator Validator = new ();
+
+        public override int Order => 1;
+        public override string ImplementationName => "Monitored";
+
+        protected override bool IsSatisfiedByWithoutNegate(Movie movie)
+        {
+            return movie.Monitored;
+        }
+
+        public override NzbDroneValidationResult Validate()
+        {
+            return new NzbDroneValidationResult(Validator.Validate(this));
+        }
+    }
+}

Lidarr/Lidarr

(cherry picked from commit 8bd91bd86b5955da5d19c9f866e7de701b843e7b)

src/NzbDrone.Core/AutoTagging/Specifications/MonitoredSpecification.cs
@@ -1,0 +1,28 @@
+using FluentValidation;
+using NzbDrone.Core.Music;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.AutoTagging.Specifications
+{
+    public class MonitoredSpecificationValidator : AbstractValidator<MonitoredSpecification>
+    {
+    }
+
+    public class MonitoredSpecification : AutoTaggingSpecificationBase
+    {
+        private static readonly MonitoredSpecificationValidator Validator = new ();
+
+        public override int Order => 1;
+        public override string ImplementationName => "Monitored";
+
+        protected override bool IsSatisfiedByWithoutNegate(Artist artist)
+        {
+            return artist.Monitored;
+        }
+
+        public override NzbDroneValidationResult Validate()
+        {
+            return new NzbDroneValidationResult(Validator.Validate(this));
+        }
+    }
+}