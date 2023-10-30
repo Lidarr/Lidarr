Sonarr/Sonarr

Paging params in API docs
Closes #6003

src/Sonarr.Api.V3/Queue/QueueController.cs
@@ -135,9 +135,9 @@ namespace Sonarr.Api.V3.Queue
 
         [HttpGet]
         [Produces("application/json")]
-        public PagingResource<QueueResource> GetQueue(bool includeUnknownSeriesItems = false, bool includeSeries = false, bool includeEpisode = false)
+        public PagingResource<QueueResource> GetQueue([FromQuery] PagingRequestResource paging, bool includeUnknownSeriesItems = false, bool includeSeries = false, bool includeEpisode = false)
         {
-            var pagingResource = Request.ReadPagingResourceFromRequest<QueueResource>();
+            var pagingResource = new PagingResource<QueueResource>(paging);
             var pagingSpec = pagingResource.MapToPagingSpec<QueueResource, NzbDrone.Core.Queue.Queue>("timeleft", SortDirection.Ascending);
 
             return pagingSpec.ApplyToPage((spec) => GetQueue(spec, includeUnknownSeriesItems), (q) => MapToResource(q, includeSeries, includeEpisode));

src/NzbDrone.Integration.Test/ApiTests/WantedTests/CutoffUnmetFixture.cs
@@ -56,7 +56,7 @@ namespace NzbDrone.Integration.Test.ApiTests.WantedTests
             var series = EnsureSeries(266189, "The Blacklist", false);
             EnsureEpisodeFile(series, 1, 1, Quality.SDTV);
 
-            var result = WantedCutoffUnmet.GetPaged(0, 15, "airDateUtc", "desc", "monitored", "false");
+            var result = WantedCutoffUnmet.GetPaged(0, 15, "airDateUtc", "desc", "monitored", false);
 
             result.Records.Should().NotBeEmpty();
         }

src/NzbDrone.Integration.Test/ApiTests/WantedTests/MissingFixture.cs
@@ -58,7 +58,7 @@ namespace NzbDrone.Integration.Test.ApiTests.WantedTests
         {
             EnsureSeries(266189, "The Blacklist", false);
 
-            var result = WantedMissing.GetPaged(0, 15, "airDateUtc", "desc", "monitored", "false");
+            var result = WantedMissing.GetPaged(0, 15, "airDateUtc", "desc", "monitored", false);
 
             result.Records.Should().NotBeEmpty();
         }

Lidarr/Lidarr

(cherry picked from commit bfaa7291e14a8d3847ef2154a52c363944560803)
Closes #4178
Closes #4196

src/Lidarr.Api.V1/Queue/QueueController.cs
@@ -129,9 +129,9 @@ namespace Lidarr.Api.V1.Queue
 
         [HttpGet]
         [Produces("application/json")]
-        public PagingResource<QueueResource> GetQueue(bool includeUnknownArtistItems = false, bool includeArtist = false, bool includeAlbum = false)
+        public PagingResource<QueueResource> GetQueue([FromQuery] PagingRequestResource paging, bool includeUnknownArtistItems = false, bool includeArtist = false, bool includeAlbum = false)
         {
-            var pagingResource = Request.ReadPagingResourceFromRequest<QueueResource>();
+            var pagingResource = new PagingResource<QueueResource>(paging);
             var pagingSpec = pagingResource.MapToPagingSpec<QueueResource, NzbDrone.Core.Queue.Queue>("timeleft", SortDirection.Ascending);
 
             return pagingSpec.ApplyToPage((spec) => GetQueue(spec, includeUnknownArtistItems), (q) => MapToResource(q, includeArtist, includeAlbum));

src/NzbDrone.Integration.Test/ApiTests/WantedTests/CutoffUnmetFixture.cs
@@ -72,7 +72,7 @@ namespace NzbDrone.Integration.Test.ApiTests.WantedTests
             var artist = EnsureArtist("8ac6cc32-8ddf-43b1-9ac4-4b04f9053176", "Alien Ant Farm", false);
             EnsureTrackFile(artist, 1, 1, 1, Quality.MP3_192);
 
-            var result = WantedCutoffUnmet.GetPaged(0, 15, "releaseDate", "desc", "monitored", "false");
+            var result = WantedCutoffUnmet.GetPaged(0, 15, "releaseDate", "desc", "monitored", false);
 
             result.Records.Should().NotBeEmpty();
         }

src/NzbDrone.Integration.Test/ApiTests/WantedTests/MissingFixture.cs
@@ -74,7 +74,7 @@ namespace NzbDrone.Integration.Test.ApiTests.WantedTests
         {
             EnsureArtist("8ac6cc32-8ddf-43b1-9ac4-4b04f9053176", "Alien Ant Farm", false);
 
-            var result = WantedMissing.GetPaged(0, 15, "releaseDate", "desc", "monitored", "false");
+            var result = WantedMissing.GetPaged(0, 15, "releaseDate", "desc", "monitored", false);
 
             result.Records.Should().NotBeEmpty();
         }