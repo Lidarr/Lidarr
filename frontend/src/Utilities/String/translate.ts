Sonarr/Sonarr

Fixed: Replacing 'appName' translation token

frontend/src/Utilities/String/translate.ts
@@ -25,15 +25,13 @@ export async function fetchTranslations(): Promise<boolean> {
 
 export default function translate(
   key: string,
-  tokens: Record<string, string | number | boolean> = { appName: 'Sonarr' }
+  tokens: Record<string, string | number | boolean> = {}
 ) {
   const translation = translations[key] || key;
 
-  if (tokens) {
-    return translation.replace(/\{([a-z0-9]+?)\}/gi, (match, tokenMatch) =>
-      String(tokens[tokenMatch] ?? match)
-    );
-  }
+  tokens.appName = 'Sonarr';
 
-  return translation;
+  return translation.replace(/\{([a-z0-9]+?)\}/gi, (match, tokenMatch) =>
+    String(tokens[tokenMatch] ?? match)
+  );
 }

Lidarr/Lidarr

(cherry picked from commit 2e51b8792db0d3ec402672dc92c95f3cb886ef44)
Closes #4284

frontend/src/Utilities/String/translate.ts
@@ -25,20 +25,18 @@ export async function fetchTranslations(): Promise<boolean> {
 
 export default function translate(
   key: string,
-  tokens: Record<string, string | number | boolean> = { appName: 'Lidarr' }
+  tokens: Record<string, string | number | boolean> = {}
 ) {
   const translation = translations[key] || key;
 
-  if (tokens) {
-    // Fallback to the old behaviour for translations not yet updated to use named tokens
-    Object.values(tokens).forEach((value, index) => {
-      tokens[index] = value;
-    });
+  tokens.appName = 'Lidarr';
 
-    return translation.replace(/\{([a-z0-9]+?)\}/gi, (match, tokenMatch) =>
-      String(tokens[tokenMatch] ?? match)
-    );
-  }
+  // Fallback to the old behaviour for translations not yet updated to use named tokens
+  Object.values(tokens).forEach((value, index) => {
+    tokens[index] = value;
+  });
 
-  return translation;
+  return translation.replace(/\{([a-z0-9]+?)\}/gi, (match, tokenMatch) =>
+    String(tokens[tokenMatch] ?? match)
+  );
 }