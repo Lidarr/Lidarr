Sonarr/Sonarr

New: Indexer flags
Closes #2782

src/NzbDrone.Core/Download/TrackedDownloads/TrackedDownloadService.cs
@@ -10,6 +10,7 @@ using NzbDrone.Core.Download.History;
 using NzbDrone.Core.History;
 using NzbDrone.Core.Messaging.Events;
 using NzbDrone.Core.Parser;
+using NzbDrone.Core.Parser.Model;
 using NzbDrone.Core.Tv;
 using NzbDrone.Core.Tv.Events;
 
@@ -109,10 +110,11 @@ namespace NzbDrone.Core.Download.TrackedDownloads
 
             try
             {
-                var parsedEpisodeInfo = Parser.Parser.ParseTitle(trackedDownload.DownloadItem.Title);
                 var historyItems = _historyService.FindByDownloadId(downloadItem.DownloadId)
-                                                  .OrderByDescending(h => h.Date)
-                                                  .ToList();
+                    .OrderByDescending(h => h.Date)
+                    .ToList();
+
+                var parsedEpisodeInfo = Parser.Parser.ParseTitle(trackedDownload.DownloadItem.Title);
 
                 if (parsedEpisodeInfo != null)
                 {
@@ -134,12 +136,11 @@ namespace NzbDrone.Core.Download.TrackedDownloads
                     var firstHistoryItem = historyItems.First();
                     var grabbedEvent = historyItems.FirstOrDefault(v => v.EventType == EpisodeHistoryEventType.Grabbed);
 
-                    trackedDownload.Indexer = grabbedEvent?.Data["indexer"];
+                    trackedDownload.Indexer = grabbedEvent?.Data?.GetValueOrDefault("indexer");
                     trackedDownload.Added = grabbedEvent?.Date;
 
                     if (parsedEpisodeInfo == null ||
-                        trackedDownload.RemoteEpisode == null ||
-                        trackedDownload.RemoteEpisode.Series == null ||
+                        trackedDownload.RemoteEpisode?.Series == null ||
                         trackedDownload.RemoteEpisode.Episodes.Empty())
                     {
                         // Try parsing the original source title and if that fails, try parsing it as a special
@@ -154,6 +155,13 @@ namespace NzbDrone.Core.Download.TrackedDownloads
                                 historyItems.Where(v => v.EventType == EpisodeHistoryEventType.Grabbed)
                                     .Select(h => h.EpisodeId).Distinct());
                         }
+                    }
+
+                    if (trackedDownload.RemoteEpisode != null &&
+                        Enum.TryParse(grabbedEvent?.Data?.GetValueOrDefault("indexerFlags"), true, out IndexerFlags flags))
+                    {
+                        trackedDownload.RemoteEpisode.Release ??= new ReleaseInfo();
+                        trackedDownload.RemoteEpisode.Release.IndexerFlags = flags;
                     }
                 }
 

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -89,6 +89,7 @@ namespace NzbDrone.Core.Notifications.CustomScript
             environmentVariables.Add("Sonarr_Release_Quality", remoteEpisode.ParsedEpisodeInfo.Quality.Quality.Name);
             environmentVariables.Add("Sonarr_Release_QualityVersion", remoteEpisode.ParsedEpisodeInfo.Quality.Revision.Version.ToString());
             environmentVariables.Add("Sonarr_Release_ReleaseGroup", releaseGroup ?? string.Empty);
+            environmentVariables.Add("Sonarr_Release_IndexerFlags", remoteEpisode.Release.IndexerFlags.ToString());
             environmentVariables.Add("Sonarr_Download_Client", message.DownloadClientName ?? string.Empty);
             environmentVariables.Add("Sonarr_Download_Client_Type", message.DownloadClientType ?? string.Empty);
             environmentVariables.Add("Sonarr_Download_Id", message.DownloadId ?? string.Empty);

Lidarr/Lidarr

(cherry picked from commit 7a768b5d0faf9aa57e78aee19cefee8fb19a42d5)

src/NzbDrone.Core/Download/TrackedDownloads/TrackedDownloadService.cs
@@ -12,6 +12,7 @@ using NzbDrone.Core.Messaging.Events;
 using NzbDrone.Core.Music;
 using NzbDrone.Core.Music.Events;
 using NzbDrone.Core.Parser;
+using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.Download.TrackedDownloads
 {
@@ -144,12 +145,11 @@ namespace NzbDrone.Core.Download.TrackedDownloads
                     var firstHistoryItem = historyItems.First();
                     var grabbedEvent = historyItems.FirstOrDefault(v => v.EventType == EntityHistoryEventType.Grabbed);
 
-                    trackedDownload.Indexer = grabbedEvent?.Data["indexer"];
+                    trackedDownload.Indexer = grabbedEvent?.Data?.GetValueOrDefault("indexer");
                     trackedDownload.Added = grabbedEvent?.Date;
 
                     if (parsedAlbumInfo == null ||
-                        trackedDownload.RemoteAlbum == null ||
-                        trackedDownload.RemoteAlbum.Artist == null ||
+                        trackedDownload.RemoteAlbum?.Artist == null ||
                         trackedDownload.RemoteAlbum.Albums.Empty())
                     {
                         // Try parsing the original source title and if that fails, try parsing it as a special
@@ -180,6 +180,13 @@ namespace NzbDrone.Core.Download.TrackedDownloads
                                         .Distinct());
                             }
                         }
+                    }
+
+                    if (trackedDownload.RemoteAlbum != null &&
+                        Enum.TryParse(grabbedEvent?.Data?.GetValueOrDefault("indexerFlags"), true, out IndexerFlags flags))
+                    {
+                        trackedDownload.RemoteAlbum.Release ??= new ReleaseInfo();
+                        trackedDownload.RemoteAlbum.Release.IndexerFlags = flags;
                     }
                 }
 

src/NzbDrone.Core/Notifications/CustomScript/CustomScript.cs
@@ -75,6 +75,7 @@ namespace NzbDrone.Core.Notifications.CustomScript
             environmentVariables.Add("Lidarr_Release_Quality", remoteAlbum.ParsedAlbumInfo.Quality.Quality.Name);
             environmentVariables.Add("Lidarr_Release_QualityVersion", remoteAlbum.ParsedAlbumInfo.Quality.Revision.Version.ToString());
             environmentVariables.Add("Lidarr_Release_ReleaseGroup", releaseGroup ?? string.Empty);
+            environmentVariables.Add("Lidarr_Release_IndexerFlags", remoteAlbum.Release.IndexerFlags.ToString());
             environmentVariables.Add("Lidarr_Download_Client", message.DownloadClientName ?? string.Empty);
             environmentVariables.Add("Lidarr_Download_Client_Type", message.DownloadClientType ?? string.Empty);
             environmentVariables.Add("Lidarr_Download_Id", message.DownloadId ?? string.Empty);