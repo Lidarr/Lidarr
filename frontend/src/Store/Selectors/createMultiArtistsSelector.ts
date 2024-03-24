28f2eb974d72148e79b388e11be0edc78d95a870
Mark McDowall <mark@mcdowall.ca>
2024-03-23 21:42:54 -0700

    Fixed: Task with removed artists causing error
    (cherry picked from commit fc6494c569324c839debdb1d08dde23b8f1b8d76)
    
    Closes #4696

--- frontend/src/Store/Selectors/createMultiArtistsSelector.ts
+++ frontend/src/Store/Selectors/createMultiArtistsSelector.ts
@@ -1,12 +1,21 @@
 import { createSelector } from 'reselect';
 import AppState from 'App/State/AppState';
+import Artist from 'Artist/Artist';
 
 function createMultiArtistsSelector(artistIds: number[]) {
   return createSelector(
     (state: AppState) => state.artist.itemMap,
     (state: AppState) => state.artist.items,
     (itemMap, allArtists) => {
-      return artistIds.map((artistId) => allArtists[itemMap[artistId]]);
+      return artistIds.reduce((acc: Artist[], artistId) => {
+        const artist = allArtists[itemMap[artistId]];
+
+        if (artist) {
+          acc.push(artist);
+        }
+
+        return acc;
+      }, []);
     }
   );
 }
