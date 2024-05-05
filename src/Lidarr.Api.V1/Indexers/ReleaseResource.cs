a25e5aae1094f5158a8afe0a1a61cdd1ac637ad0
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-05-05 04:56:52 +0300

    Fixed: Indexer flags for torrent release pushes
    (cherry picked from commit 47ba002806fe2c2004a649aa193ae318343a84e4)

--- src/Lidarr.Api.V1/Indexers/ReleaseResource.cs
+++ src/Lidarr.Api.V1/Indexers/ReleaseResource.cs
@@ -132,7 +132,8 @@ namespace Lidarr.Api.V1.Indexers
                     MagnetUrl = resource.MagnetUrl,
                     InfoHash = resource.InfoHash,
                     Seeders = resource.Seeders,
-                    Peers = (resource.Seeders.HasValue && resource.Leechers.HasValue) ? (resource.Seeders + resource.Leechers) : null
+                    Peers = (resource.Seeders.HasValue && resource.Leechers.HasValue) ? (resource.Seeders + resource.Leechers) : null,
+                    IndexerFlags = (IndexerFlags)resource.IndexerFlags
                 };
             }
             else
