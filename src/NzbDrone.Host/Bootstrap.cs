30d9891bf0ee4dd126cb5a4211cd85c68c9156c0
sillock1 <jprest97@gmail.com>
2024-05-10 20:04:05 +0100

    New: Optionally use Environment Variables for settings in config.xml

--- src/NzbDrone.Host/Bootstrap.cs
+++ src/NzbDrone.Host/Bootstrap.cs
@@ -21,6 +21,7 @@ using NzbDrone.Common.Exceptions;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Instrumentation;
 using NzbDrone.Common.Instrumentation.Extensions;
+using NzbDrone.Common.Options;
 using NzbDrone.Core.Configuration;
 using NzbDrone.Core.Datastore.Extensions;
 using PostgresOptions = NzbDrone.Core.Datastore.PostgresOptions;
@@ -97,6 +98,11 @@ namespace NzbDrone.Host
                             .ConfigureServices(services =>
                             {
                                 services.Configure<PostgresOptions>(config.GetSection("Lidarr:Postgres"));
+                                services.Configure<AppOptions>(config.GetSection("Lidarr:App"));
+                                services.Configure<AuthOptions>(config.GetSection("Lidarr:Auth"));
+                                services.Configure<ServerOptions>(config.GetSection("Lidarr:Server"));
+                                services.Configure<LogOptions>(config.GetSection("Lidarr:Log"));
+                                services.Configure<UpdateOptions>(config.GetSection("Lidarr:Update"));
                             }).Build();
 
                         break;
@@ -127,12 +133,12 @@ namespace NzbDrone.Host
         {
             var config = GetConfiguration(context);
 
-            var bindAddress = config.GetValue(nameof(ConfigFileProvider.BindAddress), "*");
-            var port = config.GetValue(nameof(ConfigFileProvider.Port), 8686);
-            var sslPort = config.GetValue(nameof(ConfigFileProvider.SslPort), 6868);
-            var enableSsl = config.GetValue(nameof(ConfigFileProvider.EnableSsl), false);
-            var sslCertPath = config.GetValue<string>(nameof(ConfigFileProvider.SslCertPath));
-            var sslCertPassword = config.GetValue<string>(nameof(ConfigFileProvider.SslCertPassword));
+            var bindAddress = config.GetValue<string>($"Lidarr:Server:{nameof(ServerOptions.BindAddress)}") ?? config.GetValue(nameof(ConfigFileProvider.BindAddress), "*");
+            var port = config.GetValue<int?>($"Lidarr:Server:{nameof(ServerOptions.Port)}") ?? config.GetValue(nameof(ConfigFileProvider.Port), 8686);
+            var sslPort = config.GetValue<int?>($"Lidarr:Server:{nameof(ServerOptions.SslPort)}") ?? config.GetValue(nameof(ConfigFileProvider.SslPort), 6868);
+            var enableSsl = config.GetValue<bool?>($"Lidarr:Server:{nameof(ServerOptions.EnableSsl)}") ?? config.GetValue(nameof(ConfigFileProvider.EnableSsl), false);
+            var sslCertPath = config.GetValue<string>($"Lidarr:Server:{nameof(ServerOptions.SslCertPath)}") ?? config.GetValue<string>(nameof(ConfigFileProvider.SslCertPath));
+            var sslCertPassword = config.GetValue<string>($"Lidarr:Server:{nameof(ServerOptions.SslCertPassword)}") ?? config.GetValue<string>(nameof(ConfigFileProvider.SslCertPassword));
 
             var urls = new List<string> { BuildUrl("http", bindAddress, port) };
 
@@ -154,6 +160,11 @@ namespace NzbDrone.Host
                 .ConfigureServices(services =>
                 {
                     services.Configure<PostgresOptions>(config.GetSection("Lidarr:Postgres"));
+                    services.Configure<AppOptions>(config.GetSection("Lidarr:App"));
+                    services.Configure<AuthOptions>(config.GetSection("Lidarr:Auth"));
+                    services.Configure<ServerOptions>(config.GetSection("Lidarr:Server"));
+                    services.Configure<LogOptions>(config.GetSection("Lidarr:Log"));
+                    services.Configure<UpdateOptions>(config.GetSection("Lidarr:Update"));
                 })
                 .ConfigureWebHost(builder =>
                 {
