4832860cce89cb019fb05d79b6bf2226e97ae8d0
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-06-11 07:41:54 +0300

    Rename Sonarr to Lidarr

--- src/NzbDrone.Core/Configuration/ConfigFileProvider.cs
+++ src/NzbDrone.Core/Configuration/ConfigFileProvider.cs
@@ -408,7 +408,7 @@ namespace NzbDrone.Core.Configuration
 
                         if (config.Count != 1)
                         {
-                            throw new InvalidConfigFileException($"{_configFile} is invalid. Please delete the config file and Sonarr will recreate it.");
+                            throw new InvalidConfigFileException($"{_configFile} is invalid. Please delete the config file and Lidarr will recreate it.");
                         }
 
                         return xDoc;
