nicksen/Sonarr

New: Added UI for parsing release names
Closes #5263

frontend/src/System/Parse/ParseResult.css.d.ts
@@ -1,0 +1,9 @@
+// This file is automatically generated.
+// Please do not change this file!
+interface CssExports {
+  'description': string;
+  'item': string;
+  'title': string;
+}
+export const cssExports: CssExports;
+export default cssExports;

Lidarr/Lidarr

(cherry picked from commit 85e285598106346099ceae676599c5cb4b789c92)

frontend/src/Parse/ParseResult.css.d.ts
@@ -1,0 +1,8 @@
+// This file is automatically generated.
+// Please do not change this file!
+interface CssExports {
+  'column': string;
+  'container': string;
+}
+export const cssExports: CssExports;
+export default cssExports;