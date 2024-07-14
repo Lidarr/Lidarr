1794a6d1748adeec23b3c43179081a858af1e366
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-07-14 23:31:39 +0300

    Fixed: Linking Next/Last albums for bulk edit artists
    Clear statistics cache for ArtistEditedEvent

--- src/Lidarr.Api.V1/Artist/ArtistResource.cs
+++ src/Lidarr.Api.V1/Artist/ArtistResource.cs
@@ -108,9 +108,7 @@ namespace Lidarr.Api.V1.Artist
                 Tags = model.Tags,
                 Added = model.Added,
                 AddOptions = model.AddOptions,
-                Ratings = model.Metadata.Value.Ratings,
-
-                Statistics = new ArtistStatisticsResource()
+                Ratings = model.Metadata.Value.Ratings
             };
         }
 
