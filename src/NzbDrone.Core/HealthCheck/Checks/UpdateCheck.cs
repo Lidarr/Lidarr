51d22bed2a19fc244d3f3320d34305f731e149e9
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-08-18 18:16:58 +0300

    Include available version in update health check
    (cherry picked from commit 15e3c3efb18242caf28b9bfc77a72a78296018bf)
    (cherry picked from commit 3b29096e402c9a738b12ca8085b97924c52577f9)
    
    Closes #4949
    Closes #5000

--- src/NzbDrone.Core/HealthCheck/Checks/UpdateCheck.cs
+++ src/NzbDrone.Core/HealthCheck/Checks/UpdateCheck.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using NzbDrone.Common.Disk;
 using NzbDrone.Common.EnvironmentInfo;
@@ -68,9 +69,20 @@ namespace NzbDrone.Core.HealthCheck.Checks
                 }
             }
 
-            if (BuildInfo.BuildDateTime < DateTime.UtcNow.AddDays(-14) && _checkUpdateService.AvailableUpdate() != null)
+            if (BuildInfo.BuildDateTime < DateTime.UtcNow.AddDays(-14))
             {
-                return new HealthCheck(GetType(), HealthCheckResult.Warning, _localizationService.GetLocalizedString("UpdateAvailable"), "#new-update-is-available");
+                var latestAvailable = _checkUpdateService.AvailableUpdate();
+
+                if (latestAvailable != null)
+                {
+                    return new HealthCheck(GetType(),
+                        HealthCheckResult.Warning,
+                        _localizationService.GetLocalizedString("UpdateAvailableHealthCheckMessage", new Dictionary<string, object>
+                        {
+                            { "version", $"v{latestAvailable.Version}" }
+                        }),
+                        "#new-update-is-available");
+                }
             }
 
             return new HealthCheck(GetType());
