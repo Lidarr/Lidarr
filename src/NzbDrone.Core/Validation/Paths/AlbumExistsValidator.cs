1c054d1ca4b1b3eec4944a5e0aac8390cbecc5cd
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-09-22 04:38:55 +0300

    Fixed: Improve validation for individual album adding

---
+++ src/NzbDrone.Core/Validation/Paths/AlbumExistsValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation.Validators;
+using NzbDrone.Core.Music;
+
+namespace NzbDrone.Core.Validation.Paths
+{
+    public class AlbumExistsValidator : PropertyValidator
+    {
+        private readonly IAlbumService _albumService;
+
+        public AlbumExistsValidator(IAlbumService albumService)
+        {
+            _albumService = albumService;
+        }
+
+        protected override string GetDefaultMessageTemplate() => "This album has already been added.";
+
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            if (context.PropertyValue == null)
+            {
+                return true;
+            }
+
+            var foreignAlbumId = context.PropertyValue.ToString();
+
+            return _albumService.FindById(foreignAlbumId) == null;
+        }
+    }
+}
