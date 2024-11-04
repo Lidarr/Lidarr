55da8d37b5f511c66ea5115d2e24c901ff5b43e1
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-11-04 18:54:11 +0200

    Show an artist path as example in Mount Health Check
    Closes #5225

--- src/NzbDrone.Core/HealthCheck/Checks/MountCheck.cs
+++ src/NzbDrone.Core/HealthCheck/Checks/MountCheck.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using NzbDrone.Common.Disk;
 using NzbDrone.Core.Localization;
@@ -19,16 +20,19 @@ namespace NzbDrone.Core.HealthCheck.Checks
 
         public override HealthCheck Check()
         {
-            // Not best for optimization but due to possible symlinks and junctions, we get mounts based on series path so internals can handle mount resolution.
+            // Not best for optimization but due to possible symlinks and junctions, we get mounts based on artist paths so internals can handle mount resolution.
             var mounts = _artistService.AllArtistPaths()
-                .Select(path => _diskProvider.GetMount(path.Value))
-                .Where(m => m is { MountOptions.IsReadOnly: true })
-                .DistinctBy(m => m.RootDirectory)
+                .Select(p => new Tuple<IMount, string>(_diskProvider.GetMount(p.Value), p.Value))
+                .Where(m => m.Item1 is { MountOptions.IsReadOnly: true })
+                .DistinctBy(m => m.Item1.RootDirectory)
                 .ToList();
 
             if (mounts.Any())
             {
-                return new HealthCheck(GetType(), HealthCheckResult.Error, _localizationService.GetLocalizedString("MountCheckMessage") + string.Join(", ", mounts.Select(m => m.Name)), "#movie-mount-ro");
+                return new HealthCheck(GetType(),
+                    HealthCheckResult.Error,
+                    $"{_localizationService.GetLocalizedString("MountArtistHealthCheckMessage")}{string.Join(", ", mounts.Select(m => $"{m.Item1.Name} ({m.Item2})"))}",
+                    "#artist-mount-ro");
             }
 
             return new HealthCheck(GetType());
