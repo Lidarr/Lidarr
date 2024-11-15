0fc25a3f9c7c20d88621902fb8d90412edd86937
Mark McDowall <mark@mcdowall.ca>
2024-11-14 19:01:38 -0800

    Fixed: Allow files to be moved from Torrent Blackhole even when remove is disabled
    (cherry picked from commit f739fd0900695e2ff312d13985c87d84ae00ea75)

--- src/NzbDrone.Core/Download/Clients/Blackhole/UsenetBlackhole.cs
+++ src/NzbDrone.Core/Download/Clients/Blackhole/UsenetBlackhole.cs
@@ -57,7 +57,7 @@ namespace NzbDrone.Core.Download.Clients.Blackhole
         {
             foreach (var item in _scanWatchFolder.GetItems(Settings.WatchFolder, ScanGracePeriod))
             {
-                yield return new DownloadClientItem
+                var queueItem = new DownloadClientItem
                 {
                     DownloadClientInfo = DownloadClientItemClientInfo.FromDownloadClient(this, false),
                     DownloadId = Definition.Name + "_" + item.DownloadId,
@@ -70,10 +70,12 @@ namespace NzbDrone.Core.Download.Clients.Blackhole
                     OutputPath = item.OutputPath,
 
                     Status = item.Status,
-
-                    CanBeRemoved = true,
-                    CanMoveFiles = true
                 };
+
+                queueItem.CanMoveFiles = true;
+                queueItem.CanBeRemoved = queueItem.DownloadClientInfo.RemoveCompletedDownloads;
+
+                yield return queueItem;
             }
         }
 
