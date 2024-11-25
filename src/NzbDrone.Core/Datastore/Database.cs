230654f3f70416d65bef2965b749efd39652922c
Mark McDowall <mark@mcdowall.ca>
2024-11-24 20:51:33 -0800

    Support Postgres with non-standard version string
    (cherry picked from commit 40f4ef27b22113c1dae0d0cbdee8205132bed68a)
    
    Closes #5267

--- src/NzbDrone.Core/Datastore/Database.cs
+++ src/NzbDrone.Core/Datastore/Database.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Data;
+using System.Data.Common;
 using System.Data.SQLite;
-using System.Text.RegularExpressions;
 using Dapper;
 using NLog;
 using NzbDrone.Common.Instrumentation;
@@ -39,10 +39,9 @@ namespace NzbDrone.Core.Datastore
         {
             get
             {
-                using (var db = _datamapperFactory())
-                {
-                    return db is SQLiteConnection ? DatabaseType.SQLite : DatabaseType.PostgreSQL;
-                }
+                using var db = _datamapperFactory();
+
+                return db is SQLiteConnection ? DatabaseType.SQLite : DatabaseType.PostgreSQL;
             }
         }
 
@@ -50,24 +49,10 @@ namespace NzbDrone.Core.Datastore
         {
             get
             {
-                using (var db = _datamapperFactory())
-                {
-                    string version;
+                using var db = _datamapperFactory();
+                var dbConnection = db as DbConnection;
 
-                    try
-                    {
-                        version = db.QueryFirstOrDefault<string>("SHOW server_version");
-
-                        // Postgres can return extra info about operating system on version call, ignore this
-                        version = Regex.Replace(version, @"\(.*?\)", "");
-                    }
-                    catch
-                    {
-                        version = db.QueryFirstOrDefault<string>("SELECT sqlite_version()");
-                    }
-
-                    return new Version(version);
-                }
+                return DatabaseVersionParser.ParseServerVersion(dbConnection.ServerVersion);
             }
         }
 
