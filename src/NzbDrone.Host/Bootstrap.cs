Sonarr/Sonarr

New: Config file setting to disable log database
Closes #6743

src/NzbDrone.Host/Bootstrap.cs
@@ -95,6 +95,15 @@ namespace NzbDrone.Host
                                     .AddStartupContext(startupContext)
                                     .Resolve<UtilityModeRouter>()
                                     .Route(appMode);
+
+                                if (config.GetValue(nameof(ConfigFileProvider.LogDbEnabled), true))
+                                {
+                                    c.AddLogDatabase();
+                                }
+                                else
+                                {
+                                    c.AddDummyLogDatabase();
+                                }
                             })
                             .ConfigureServices(services =>
                             {
@@ -131,6 +140,7 @@ namespace NzbDrone.Host
             var enableSsl = config.GetValue<bool?>($"Sonarr:Server:{nameof(ServerOptions.EnableSsl)}") ?? config.GetValue(nameof(ConfigFileProvider.EnableSsl), false);
             var sslCertPath = config.GetValue<string>($"Sonarr:Server:{nameof(ServerOptions.SslCertPath)}") ?? config.GetValue<string>(nameof(ConfigFileProvider.SslCertPath));
             var sslCertPassword = config.GetValue<string>($"Sonarr:Server:{nameof(ServerOptions.SslCertPassword)}") ?? config.GetValue<string>(nameof(ConfigFileProvider.SslCertPassword));
+            var logDbEnabled = config.GetValue<bool?>($"Sonarr:Log:{nameof(LogOptions.DbEnabled)}") ?? config.GetValue(nameof(ConfigFileProvider.LogDbEnabled), true);
 
             var urls = new List<string> { BuildUrl("http", bindAddress, port) };
 
@@ -152,6 +162,15 @@ namespace NzbDrone.Host
                         .AddNzbDroneLogger()
                         .AddDatabase()
                         .AddStartupContext(context);
+
+                    if (logDbEnabled)
+                    {
+                        c.AddLogDatabase();
+                    }
+                    else
+                    {
+                        c.AddDummyLogDatabase();
+                    }
 
                     SchemaBuilder.Initialize(c);
                 })

Lidarr/Lidarr

(cherry picked from commit 92eab4b2e26741b7f20b6b7177418402cb15a3aa)

src/NzbDrone.Host/Bootstrap.cs
@@ -94,6 +94,15 @@ namespace NzbDrone.Host
                                     .AddStartupContext(startupContext)
                                     .Resolve<UtilityModeRouter>()
                                     .Route(appMode);
+
+                                if (config.GetValue(nameof(ConfigFileProvider.LogDbEnabled), true))
+                                {
+                                    c.AddLogDatabase();
+                                }
+                                else
+                                {
+                                    c.AddDummyLogDatabase();
+                                }
                             })
                             .ConfigureServices(services =>
                             {
@@ -139,6 +148,7 @@ namespace NzbDrone.Host
             var enableSsl = config.GetValue<bool?>($"Lidarr:Server:{nameof(ServerOptions.EnableSsl)}") ?? config.GetValue(nameof(ConfigFileProvider.EnableSsl), false);
             var sslCertPath = config.GetValue<string>($"Lidarr:Server:{nameof(ServerOptions.SslCertPath)}") ?? config.GetValue<string>(nameof(ConfigFileProvider.SslCertPath));
             var sslCertPassword = config.GetValue<string>($"Lidarr:Server:{nameof(ServerOptions.SslCertPassword)}") ?? config.GetValue<string>(nameof(ConfigFileProvider.SslCertPassword));
+            var logDbEnabled = config.GetValue<bool?>($"Lidarr:Log:{nameof(LogOptions.DbEnabled)}") ?? config.GetValue(nameof(ConfigFileProvider.LogDbEnabled), true);
 
             var urls = new List<string> { BuildUrl("http", bindAddress, port) };
 
@@ -156,6 +166,15 @@ namespace NzbDrone.Host
                         .AddNzbDroneLogger()
                         .AddDatabase()
                         .AddStartupContext(context);
+
+                    if (logDbEnabled)
+                    {
+                        c.AddLogDatabase();
+                    }
+                    else
+                    {
+                        c.AddDummyLogDatabase();
+                    }
                 })
                 .ConfigureServices(services =>
                 {