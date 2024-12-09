467894e4e69a814cb590077588abb2c1ef6130c7
Mark McDowall <mark@mcdowall.ca>
2024-12-08 17:24:47 -0800

    Upgrade TypeScript and core-js
    (cherry picked from commit 148480909917f69ff3b2ca547ccb4716dd56606e)
    
    Closes #5306

--- frontend/src/Store/Selectors/createArtistMetadataProfileSelector.ts
+++ frontend/src/Store/Selectors/createArtistMetadataProfileSelector.ts
@@ -1,13 +1,14 @@
 import { createSelector } from 'reselect';
 import AppState from 'App/State/AppState';
 import Artist from 'Artist/Artist';
+import MetadataProfile from 'typings/MetadataProfile';
 import { createArtistSelectorForHook } from './createArtistSelector';
 
 function createArtistMetadataProfileSelector(artistId: number) {
   return createSelector(
     (state: AppState) => state.settings.metadataProfiles.items,
     createArtistSelectorForHook(artistId),
-    (metadataProfiles, artist = {} as Artist) => {
+    (metadataProfiles: MetadataProfile[], artist = {} as Artist) => {
       return metadataProfiles.find((profile) => {
         return profile.id === artist.metadataProfileId;
       });
--- frontend/src/Store/Selectors/createArtistQualityProfileSelector.ts
+++ frontend/src/Store/Selectors/createArtistQualityProfileSelector.ts
@@ -1,13 +1,14 @@
 import { createSelector } from 'reselect';
 import AppState from 'App/State/AppState';
 import Artist from 'Artist/Artist';
+import QualityProfile from 'typings/QualityProfile';
 import { createArtistSelectorForHook } from './createArtistSelector';
 
 function createArtistQualityProfileSelector(artistId: number) {
   return createSelector(
     (state: AppState) => state.settings.qualityProfiles.items,
     createArtistSelectorForHook(artistId),
-    (qualityProfiles, artist = {} as Artist) => {
+    (qualityProfiles: QualityProfile[], artist = {} as Artist) => {
       return qualityProfiles.find(
         (profile) => profile.id === artist.qualityProfileId
       );
