08c82cfd56693f4022bb3ed31ec754d50dcfe4e2
Bogdan <mynameisbogdan@users.noreply.github.com>
2024-12-15 18:50:16 +0200

    Set minor version for core-js in babel/preset-env
    (cherry picked from commit 2e83d59f61957cbc2171bef097fe2410e72729ad)

--- frontend/build/webpack.config.js
+++ frontend/build/webpack.config.js
@@ -188,7 +188,7 @@ module.exports = (env) => {
                       loose: true,
                       debug: false,
                       useBuiltIns: 'entry',
-                      corejs: 3
+                      corejs: '3.39'
                     }
                   ]
                 ]
