0cafd23728676db946e5c387d569ac21df1822a3
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-03-11 18:09:19 +0200

    Fixed: Inherit indexer, size and release group for marked as failed history
    (cherry picked from commit e08c9d5501e65aabce3456b2dd7571867508d88f)

--- src/NzbDrone.Core/History/EntityHistory.cs
+++ src/NzbDrone.Core/History/EntityHistory.cs
@@ -10,6 +10,9 @@ namespace NzbDrone.Core.History
     {
         public const string DOWNLOAD_CLIENT = "downloadClient";
         public const string RELEASE_SOURCE = "releaseSource";
+        public const string RELEASE_GROUP = "releaseGroup";
+        public const string SIZE = "size";
+        public const string INDEXER = "indexer";
 
         public EntityHistory()
         {
