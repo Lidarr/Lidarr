3b5538450c84a69432dd5a428022ff7d536e05c7
Stevie Robinson <stevie.robinson@gmail.com>
2025-05-22 02:56:15 +0200

    New: Don't allow remote path to start with space
    (cherry picked from commit 5ba3ff598770fdf9e5a53d490c8bcbdd6a59c4cc)
    
    Fixed validation for Remote Path Mapping
    
    (cherry picked from commit bf34b4309402ce529a8c04de70f44b28948761f4)

--- src/Lidarr.Api.V1/RemotePathMappings/RemotePathMappingController.cs
+++ src/Lidarr.Api.V1/RemotePathMappings/RemotePathMappingController.cs
@@ -4,6 +4,7 @@ using Lidarr.Http;
 using Lidarr.Http.REST;
 using Lidarr.Http.REST.Attributes;
 using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Common.Extensions;
 using NzbDrone.Core.RemotePathMappings;
 using NzbDrone.Core.Validation.Paths;
 
@@ -21,11 +22,19 @@ namespace Lidarr.Api.V1.RemotePathMappings
             _remotePathMappingService = remotePathMappingService;
 
             SharedValidator.RuleFor(c => c.Host)
-                           .NotEmpty();
+                .NotEmpty();
 
             // We cannot use IsValidPath here, because it's a remote path, possibly other OS.
             SharedValidator.RuleFor(c => c.RemotePath)
-                           .NotEmpty();
+                .NotEmpty();
+
+            SharedValidator.RuleFor(c => c.RemotePath)
+                .Must(remotePath => remotePath.IsNotNullOrWhiteSpace() && !remotePath.StartsWith(" "))
+                .WithMessage("Remote Path '{PropertyValue}' must not start with a space");
+
+            SharedValidator.RuleFor(c => c.RemotePath)
+                .Must(remotePath => remotePath.IsNotNullOrWhiteSpace() && !remotePath.EndsWith(" "))
+                .WithMessage("Remote Path '{PropertyValue}' must not end with a space");
 
             SharedValidator.RuleFor(c => c.LocalPath)
                 .Cascade(CascadeMode.Stop)
