675e6c1bd666f3cfaf8d3bfb3288ce0a8def54c5
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-06-12 11:02:35 +0300

    Update default log level message
    (cherry picked from commit 817d13e85c89d1f10abab09a8f63272a46f5d0b6)

--- frontend/src/System/Logs/Files/LogFiles.js
+++ frontend/src/System/Logs/Files/LogFiles.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Alert from 'Components/Alert';
-import Link from 'Components/Link/Link';
 import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import InlineMarkdown from 'Components/Markdown/InlineMarkdown';
 import PageContent from 'Components/Page/PageContent';
 import PageContentBody from 'Components/Page/PageContentBody';
 import PageToolbar from 'Components/Page/Toolbar/PageToolbar';
@@ -77,15 +77,16 @@ class LogFiles extends Component {
         <PageContentBody>
           <Alert>
             <div>
-              Log files are located in: {location}
+              {translate('LogFilesLocation', {
+                location
+              })}
             </div>
 
-            {
-              currentLogView === 'Log Files' &&
-                <div>
-                  The log level defaults to 'Info' and can be changed in <Link to="/settings/general">General Settings</Link>
-                </div>
-            }
+            {currentLogView === 'Log Files' ? (
+              <div>
+                <InlineMarkdown data={translate('TheLogLevelDefault')} />
+              </div>
+            ) : null}
           </Alert>
 
           {
