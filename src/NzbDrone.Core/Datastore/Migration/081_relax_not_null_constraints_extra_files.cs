1dff84826ab4abdb4b088007e07f7afb99b73ee8
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-06-16 13:08:22 +0300

    fixup! Import album extras for manual imports and downloads

--- src/NzbDrone.Core/Datastore/Migration/081_relax_not_null_constraints_extra_files.cs
+++ src/NzbDrone.Core/Datastore/Migration/081_relax_not_null_constraints_extra_files.cs
@@ -3,7 +3,7 @@ using NzbDrone.Core.Datastore.Migration.Framework;
 
 namespace NzbDrone.Core.Datastore.Migration
 {
-    [Migration(079)]
+    [Migration(081)]
     public class relax_not_null_constraints_extra_files : NzbDroneMigrationBase
     {
         protected override void MainDbUpgrade()
