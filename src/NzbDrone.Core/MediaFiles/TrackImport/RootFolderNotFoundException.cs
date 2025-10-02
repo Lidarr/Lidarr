3053c6da05e82f71613fd6990fc5465e91e89833
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-10-02 22:17:40 +0300

    New: Bump to .NET 8
    (cherry picked from commit d6cee5094cdd3ad8342eda56632f5a66316e390c)

--- src/NzbDrone.Core/MediaFiles/TrackImport/RootFolderNotFoundException.cs
+++ src/NzbDrone.Core/MediaFiles/TrackImport/RootFolderNotFoundException.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Runtime.Serialization;
 
 namespace NzbDrone.Core.MediaFiles.TrackImport
 {
@@ -17,11 +16,6 @@ namespace NzbDrone.Core.MediaFiles.TrackImport
 
         public RootFolderNotFoundException(string message, Exception innerException)
             : base(message, innerException)
-        {
-        }
-
-        protected RootFolderNotFoundException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
         {
         }
     }
