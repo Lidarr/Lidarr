454d654f6f8fd1d6d56be899690e5a64152aa813
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2025-10-02 19:14:06 -0500

    Skip tests temporally

--- src/NzbDrone.Integration.Test/ApiTests/TrackFixture.cs
+++ src/NzbDrone.Integration.Test/ApiTests/TrackFixture.cs
@@ -7,7 +7,7 @@ using NUnit.Framework;
 namespace NzbDrone.Integration.Test.ApiTests
 {
     [TestFixture]
-    [Ignore("Waiting for metadata to be back again", Until = "2025-10-01 00:00:00Z")]
+    [Ignore("Waiting for metadata to be back again", Until = "2025-11-15 00:00:00Z")]
     public class TrackFixture : IntegrationTest
     {
         private ArtistResource _artist;
