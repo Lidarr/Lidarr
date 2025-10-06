Sonarr/Sonarr

Convert System to TypeScript

frontend/src/System/Status/About/About.tsx
@@ -1,0 +1,103 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import AppState from 'App/State/AppState';
+import DescriptionList from 'Components/DescriptionList/DescriptionList';
+import DescriptionListItem from 'Components/DescriptionList/DescriptionListItem';
+import FieldSet from 'Components/FieldSet';
+import InlineMarkdown from 'Components/Markdown/InlineMarkdown';
+import { fetchStatus } from 'Store/Actions/systemActions';
+import titleCase from 'Utilities/String/titleCase';
+import translate from 'Utilities/String/translate';
+import StartTime from './StartTime';
+import styles from './About.css';
+
+function About() {
+  const dispatch = useDispatch();
+  const { item } = useSelector((state: AppState) => state.system.status);
+
+  const {
+    version,
+    packageVersion,
+    packageAuthor,
+    isNetCore,
+    isDocker,
+    runtimeVersion,
+    databaseVersion,
+    databaseType,
+    migrationVersion,
+    appData,
+    startupPath,
+    mode,
+    startTime,
+  } = item;
+
+  useEffect(() => {
+    dispatch(fetchStatus());
+  }, [dispatch]);
+
+  return (
+    <FieldSet legend={translate('About')}>
+      <DescriptionList className={styles.descriptionList}>
+        <DescriptionListItem title={translate('Version')} data={version} />
+
+        {packageVersion && (
+          <DescriptionListItem
+            title={translate('PackageVersion')}
+            data={
+              packageAuthor ? (
+                <InlineMarkdown
+                  data={translate('PackageVersionInfo', {
+                    packageVersion,
+                    packageAuthor,
+                  })}
+                />
+              ) : (
+                packageVersion
+              )
+            }
+          />
+        )}
+
+        {isNetCore ? (
+          <DescriptionListItem
+            title={translate('DotNetVersion')}
+            data={`Yes (${runtimeVersion})`}
+          />
+        ) : null}
+
+        {isDocker ? (
+          <DescriptionListItem title={translate('Docker')} data={'Yes'} />
+        ) : null}
+
+        <DescriptionListItem
+          title={translate('Database')}
+          data={`${titleCase(databaseType)} ${databaseVersion}`}
+        />
+
+        <DescriptionListItem
+          title={translate('DatabaseMigration')}
+          data={migrationVersion}
+        />
+
+        <DescriptionListItem
+          title={translate('AppDataDirectory')}
+          data={appData}
+        />
+
+        <DescriptionListItem
+          title={translate('StartupDirectory')}
+          data={startupPath}
+        />
+
+        <DescriptionListItem title={translate('Mode')} data={titleCase(mode)} />
+
+        <DescriptionListItem
+          title={translate('Uptime')}
+          data={<StartTime startTime={startTime} />}
+        />
+      </DescriptionList>
+    </FieldSet>
+  );
+}
+
+export default About;

frontend/src/System/Status/Health/HealthItemLink.tsx
@@ -1,0 +1,65 @@
+import React from 'react';
+import IconButton from 'Components/Link/IconButton';
+import { icons } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
+
+interface HealthItemLinkProps {
+  source: string;
+}
+
+function HealthItemLink(props: HealthItemLinkProps) {
+  const { source } = props;
+
+  switch (source) {
+    case 'IndexerRssCheck':
+    case 'IndexerSearchCheck':
+    case 'IndexerStatusCheck':
+    case 'IndexerJackettAllCheck':
+    case 'IndexerLongTermStatusCheck':
+      return (
+        <IconButton
+          name={icons.SETTINGS}
+          title={translate('Settings')}
+          to="/settings/indexers"
+        />
+      );
+    case 'DownloadClientCheck':
+    case 'DownloadClientStatusCheck':
+    case 'ImportMechanismCheck':
+      return (
+        <IconButton
+          name={icons.SETTINGS}
+          title={translate('Settings')}
+          to="/settings/downloadclients"
+        />
+      );
+    case 'NotificationStatusCheck':
+      return (
+        <IconButton
+          name={icons.SETTINGS}
+          title={translate('Settings')}
+          to="/settings/connect"
+        />
+      );
+    case 'RootFolderCheck':
+      return (
+        <IconButton
+          name={icons.SERIES_CONTINUING}
+          title={translate('SeriesEditor')}
+          to="/serieseditor"
+        />
+      );
+    case 'UpdateCheck':
+      return (
+        <IconButton
+          name={icons.UPDATE}
+          title={translate('Updates')}
+          to="/system/updates"
+        />
+      );
+    default:
+      return null;
+  }
+}
+
+export default HealthItemLink;

Lidarr/Lidarr

(cherry picked from commit 72db8099e0f4abc3176e397f8dda3b2b69026daf)

frontend/src/System/Status/About/About.tsx
@@ -1,0 +1,103 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import AppState from 'App/State/AppState';
+import DescriptionList from 'Components/DescriptionList/DescriptionList';
+import DescriptionListItem from 'Components/DescriptionList/DescriptionListItem';
+import FieldSet from 'Components/FieldSet';
+import InlineMarkdown from 'Components/Markdown/InlineMarkdown';
+import { fetchStatus } from 'Store/Actions/systemActions';
+import titleCase from 'Utilities/String/titleCase';
+import translate from 'Utilities/String/translate';
+import StartTime from './StartTime';
+import styles from './About.css';
+
+function About() {
+  const dispatch = useDispatch();
+  const { item } = useSelector((state: AppState) => state.system.status);
+
+  const {
+    version,
+    packageVersion,
+    packageAuthor,
+    isNetCore,
+    isDocker,
+    runtimeVersion,
+    databaseVersion,
+    databaseType,
+    migrationVersion,
+    appData,
+    startupPath,
+    mode,
+    startTime,
+  } = item;
+
+  useEffect(() => {
+    dispatch(fetchStatus());
+  }, [dispatch]);
+
+  return (
+    <FieldSet legend={translate('About')}>
+      <DescriptionList className={styles.descriptionList}>
+        <DescriptionListItem title={translate('Version')} data={version} />
+
+        {packageVersion && (
+          <DescriptionListItem
+            title={translate('PackageVersion')}
+            data={
+              packageAuthor ? (
+                <InlineMarkdown
+                  data={translate('PackageVersionInfo', {
+                    packageVersion,
+                    packageAuthor,
+                  })}
+                />
+              ) : (
+                packageVersion
+              )
+            }
+          />
+        )}
+
+        {isNetCore ? (
+          <DescriptionListItem
+            title={translate('NETCore')}
+            data={`Yes (${runtimeVersion})`}
+          />
+        ) : null}
+
+        {isDocker ? (
+          <DescriptionListItem title={translate('Docker')} data={'Yes'} />
+        ) : null}
+
+        <DescriptionListItem
+          title={translate('Database')}
+          data={`${titleCase(databaseType)} ${databaseVersion}`}
+        />
+
+        <DescriptionListItem
+          title={translate('DatabaseMigration')}
+          data={migrationVersion}
+        />
+
+        <DescriptionListItem
+          title={translate('AppDataDirectory')}
+          data={appData}
+        />
+
+        <DescriptionListItem
+          title={translate('StartupDirectory')}
+          data={startupPath}
+        />
+
+        <DescriptionListItem title={translate('Mode')} data={titleCase(mode)} />
+
+        <DescriptionListItem
+          title={translate('Uptime')}
+          data={<StartTime startTime={startTime} />}
+        />
+      </DescriptionList>
+    </FieldSet>
+  );
+}
+
+export default About;

frontend/src/System/Status/Health/HealthItemLink.tsx
@@ -1,0 +1,66 @@
+import React from 'react';
+import IconButton from 'Components/Link/IconButton';
+import { icons } from 'Helpers/Props';
+import translate from 'Utilities/String/translate';
+
+interface HealthItemLinkProps {
+  source: string;
+}
+
+function HealthItemLink(props: HealthItemLinkProps) {
+  const { source } = props;
+
+  switch (source) {
+    case 'IndexerRssCheck':
+    case 'IndexerSearchCheck':
+    case 'IndexerStatusCheck':
+    case 'IndexerJackettAllCheck':
+    case 'IndexerLongTermStatusCheck':
+      return (
+        <IconButton
+          name={icons.SETTINGS}
+          title={translate('Settings')}
+          to="/settings/indexers"
+        />
+      );
+    case 'DownloadClientCheck':
+    case 'DownloadClientStatusCheck':
+    case 'ImportMechanismCheck':
+    case 'RemotePathMappingCheck':
+      return (
+        <IconButton
+          name={icons.SETTINGS}
+          title={translate('Settings')}
+          to="/settings/downloadclients"
+        />
+      );
+    case 'NotificationStatusCheck':
+      return (
+        <IconButton
+          name={icons.SETTINGS}
+          title={translate('Settings')}
+          to="/settings/connect"
+        />
+      );
+    case 'RootFolderCheck':
+      return (
+        <IconButton
+          name={icons.ARTIST_CONTINUING}
+          title={translate('ArtistEditor')}
+          to="/artisteditor"
+        />
+      );
+    case 'UpdateCheck':
+      return (
+        <IconButton
+          name={icons.UPDATE}
+          title={translate('Updates')}
+          to="/system/updates"
+        />
+      );
+    default:
+      return null;
+  }
+}
+
+export default HealthItemLink;