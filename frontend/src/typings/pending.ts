4c993b9627e605e46b6030e749b24986e773701c
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2025-10-11 23:57:40 +0100

    Form, FormGroup typescript conversion

---
+++ frontend/src/typings/pending.ts
@@ -0,0 +1,25 @@
+export interface ValidationFailure {
+  propertyName: string;
+  errorMessage: string;
+  infoLink?: string;
+  detailedDescription?: string;
+  severity: 'error' | 'warning';
+}
+
+export interface ValidationError extends ValidationFailure {
+  isWarning: false;
+}
+
+export interface ValidationWarning extends ValidationFailure {
+  isWarning: true;
+}
+
+export interface Pending<T> {
+  value: T;
+  errors: ValidationError[];
+  warnings: ValidationWarning[];
+}
+
+export type PendingSection<T> = {
+  [K in keyof T]: Pending<T[K]>;
+};
