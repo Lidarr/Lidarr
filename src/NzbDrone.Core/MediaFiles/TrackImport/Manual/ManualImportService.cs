Sonarr/Sonarr

New: Improve Manual Import logging when not parsing files

src/NzbDrone.Core/MediaFiles/EpisodeImport/Manual/ManualImportService.cs
@@ -246,6 +246,7 @@ namespace NzbDrone.Core.MediaFiles.EpisodeImport.Manual
 
                 if (files.Count() > 100)
                 {
+                    _logger.Warn("Unable to determine series from folder name and found more than 100 files. Skipping parsing");
                     return ProcessDownloadDirectory(rootFolder, files);
                 }
 

Lidarr/Lidarr

(cherry picked from commit 83f63590630ae0728fd9f9f03567a294934eebcc)

src/NzbDrone.Core/MediaFiles/TrackImport/Manual/ManualImportService.cs
@@ -170,6 +170,7 @@ namespace NzbDrone.Core.MediaFiles.TrackImport.Manual
 
             if (artist == null && artistFiles.Count > 100)
             {
+                _logger.Warn("Unable to determine artist from folder name and found more than 100 files. Skipping parsing");
                 return ProcessDownloadDirectory(folder, artistFiles);
             }
 