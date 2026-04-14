Sonarr/Sonarr

Fixed: Removing pending release without blocklisting

src/Sonarr.Api.V3/Queue/QueueController.cs
@@ -77,7 +77,7 @@ namespace Sonarr.Api.V3.Queue
 
             if (pendingRelease != null)
             {
-                Remove(pendingRelease);
+                Remove(pendingRelease, blocklist);
 
                 return;
             }
@@ -120,7 +120,7 @@ namespace Sonarr.Api.V3.Queue
 
             foreach (var pendingRelease in pendingToRemove.DistinctBy(p => p.Id))
             {
-                Remove(pendingRelease);
+                Remove(pendingRelease, blocklist);
             }
 
             foreach (var trackedDownload in trackedToRemove.DistinctBy(t => t.DownloadItem.DownloadId))
@@ -286,9 +286,13 @@ namespace Sonarr.Api.V3.Queue
             }
         }
 
-        private void Remove(NzbDrone.Core.Queue.Queue pendingRelease)
+        private void Remove(NzbDrone.Core.Queue.Queue pendingRelease, bool blocklist)
         {
-            _blocklistService.Block(pendingRelease.RemoteEpisode, "Pending release manually blocklisted");
+            if (blocklist)
+            {
+                _blocklistService.Block(pendingRelease.RemoteEpisode, "Pending release manually blocklisted");
+            }
+
             _pendingReleaseService.RemovePendingQueueItems(pendingRelease.Id);
         }
 

Lidarr/Lidarr

(cherry picked from commit 0c883f78862f88ff37cd5539da4f569fbe3c93ed)
Closes #4892

src/Lidarr.Api.V1/Queue/QueueController.cs
@@ -71,7 +71,7 @@ namespace Lidarr.Api.V1.Queue
 
             if (pendingRelease != null)
             {
-                Remove(pendingRelease);
+                Remove(pendingRelease, blocklist);
 
                 return;
             }
@@ -114,7 +114,7 @@ namespace Lidarr.Api.V1.Queue
 
             foreach (var pendingRelease in pendingToRemove.DistinctBy(p => p.Id))
             {
-                Remove(pendingRelease);
+                Remove(pendingRelease, blocklist);
             }
 
             foreach (var trackedDownload in trackedToRemove.DistinctBy(t => t.DownloadItem.DownloadId))
@@ -263,9 +263,13 @@ namespace Lidarr.Api.V1.Queue
             }
         }
 
-        private void Remove(NzbDrone.Core.Queue.Queue pendingRelease)
+        private void Remove(NzbDrone.Core.Queue.Queue pendingRelease, bool blocklist)
         {
-            _blocklistService.Block(pendingRelease.RemoteAlbum, "Pending release manually blocklisted");
+            if (blocklist)
+            {
+                _blocklistService.Block(pendingRelease.RemoteAlbum, "Pending release manually blocklisted");
+            }
+
             _pendingReleaseService.RemovePendingQueueItems(pendingRelease.Id);
         }
 