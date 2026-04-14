Sonarr/Sonarr

Skip duplicate import list exclusions

src/Sonarr.Api.V3/ImportLists/ImportListExclusionController.cs
@@ -21,7 +21,10 @@ namespace Sonarr.Api.V3.ImportLists
         {
             _importListExclusionService = importListExclusionService;
 
-            SharedValidator.RuleFor(c => c.TvdbId).NotEmpty().SetValidator(importListExclusionExistsValidator);
+            SharedValidator.RuleFor(c => c.TvdbId).Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .SetValidator(importListExclusionExistsValidator);
+
             SharedValidator.RuleFor(c => c.Title).NotEmpty();
         }
 

Lidarr/Lidarr

(cherry picked from commit 9af2f137f41867a29d544fad77551672a79f24b6)
Closes #5003

src/Lidarr.Api.V1/ImportLists/ImportListExclusionController.cs
@@ -20,7 +20,11 @@ namespace Lidarr.Api.V1.ImportLists
         {
             _importListExclusionService = importListExclusionService;
 
-            SharedValidator.RuleFor(c => c.ForeignId).NotEmpty().SetValidator(guidValidator).SetValidator(importListExclusionExistsValidator);
+            SharedValidator.RuleFor(c => c.ForeignId).Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .SetValidator(guidValidator)
+                .SetValidator(importListExclusionExistsValidator);
+
             SharedValidator.RuleFor(c => c.ArtistName).NotEmpty();
         }
 