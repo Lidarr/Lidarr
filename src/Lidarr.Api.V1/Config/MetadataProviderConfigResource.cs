2724edcd009cdfe0fef16e74f6d5c69d3955b7fe
chodeus <dhoges90@icloud.com>
2026-08-07 10:17:42 +0800

    Keep deprecated metadata provider config fields backed by the default tagging profile

--- src/Lidarr.Api.V1/Config/MetadataProviderConfigResource.cs
+++ src/Lidarr.Api.V1/Config/MetadataProviderConfigResource.cs
@@ -1,3 +1,4 @@
+using System;
 using Lidarr.Http.REST;
 using NzbDrone.Core.Configuration;
 
@@ -6,6 +7,15 @@ namespace Lidarr.Api.V1.Config
     public class MetadataProviderConfigResource : RestResource
     {
         public string MetadataSource { get; set; }
+
+        [Obsolete("Use tagging profiles instead")]
+        public WriteAudioTagsType WriteAudioTags { get; set; }
+
+        [Obsolete("Use tagging profiles instead")]
+        public bool ScrubAudioTags { get; set; }
+
+        [Obsolete("Use tagging profiles instead")]
+        public bool EmbedCoverArt { get; set; }
     }
 
     public static class MetadataProviderConfigResourceMapper
