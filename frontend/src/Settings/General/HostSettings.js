45d68ee9a8ce3d78683370af71320feee80a8441
Mark McDowall <mark@mcdowall.ca>
2026-08-24 15:48:37 -0700

    New: Add hostname validation
    (cherry picked from commit 1579a8bb7f89803d0850369f3ff7980d6edaf31d)

--- frontend/src/Settings/General/HostSettings.js
+++ frontend/src/Settings/General/HostSettings.js
@@ -22,6 +22,7 @@ function HostSettings(props) {
     urlBase,
     instanceName,
     applicationUrl,
+    allowedHosts,
     enableSsl,
     sslPort,
     sslCertPath,
@@ -78,6 +79,19 @@ function HostSettings(props) {
           helpTextWarning={translate('UrlBaseHelpTextWarning')}
           onChange={onInputChange}
           {...urlBase}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <FormLabel>{translate('AllowedHosts')}</FormLabel>
+        <FormInputGroup
+          type={inputTypes.TEXT}
+          name="allowedHosts"
+          helpText={translate('AllowedHostsHelpText')}
+          helpTextWarning={translate('RestartRequiredHelpTextWarning')}
+          helpLink="https://wiki.servarr.com/lidarr/settings#host"
+          onChange={onInputChange}
+          {...allowedHosts}
         />
       </FormGroup>
 
--- src/NzbDrone.Core/Configuration/ConfigFileProvider.cs
+++ src/NzbDrone.Core/Configuration/ConfigFileProvider.cs
@@ -36,6 +36,7 @@ namespace NzbDrone.Core.Configuration
         bool LaunchBrowser { get; }
         AuthenticationType AuthenticationMethod { get; }
         AuthenticationRequiredType AuthenticationRequired { get; }
+        string AllowedHosts { get; }
         bool AnalyticsEnabled { get; }
         string LogLevel { get; }
         string ConsoleLogLevel { get; }
@@ -231,6 +232,10 @@ namespace NzbDrone.Core.Configuration
             Enum.TryParse<AuthenticationRequiredType>(_authOptions.Required, out var enumValue)
                 ? enumValue
                 : GetValueEnum("AuthenticationRequired", AuthenticationRequiredType.Enabled);
+
+        public bool TrustCgnatIpAddresses => _authOptions.TrustCgnatIpAddresses ?? GetValueBoolean("TrustCgnatIpAddresses", false, persist: false);
+
+        public string AllowedHosts => _serverOptions.AllowedHosts ?? GetValue("AllowedHosts", string.Empty);
 
         public bool AnalyticsEnabled => _logOptions.AnalyticsEnabled ?? GetValueBoolean("AnalyticsEnabled", true, persist: false);
 
@@ -507,7 +512,5 @@ namespace NzbDrone.Core.Configuration
         {
             SetValue("ApiKey", GenerateApiKey());
         }
-
-        public bool TrustCgnatIpAddresses => _authOptions.TrustCgnatIpAddresses ?? GetValueBoolean("TrustCgnatIpAddresses", false, persist: false);
     }
 }
