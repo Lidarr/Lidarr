Sonarr/Sonarr

Add reason to health check

src/NzbDrone.Core/HealthCheck/Checks/IndexerSearchCheck.cs
@@ -25,21 +25,33 @@ namespace NzbDrone.Core.HealthCheck.Checks
 
             if (automaticSearchEnabled.Empty())
             {
-                return new HealthCheck(GetType(), HealthCheckResult.Warning, _localizationService.GetLocalizedString("IndexerSearchNoAutomaticHealthCheckMessage"), "#no-indexers-available-with-automatic-search-enabled-sonarr-will-not-provide-any-automatic-search-results");
+                return new HealthCheck(GetType(),
+                    HealthCheckResult.Warning,
+                    HealthCheckReason.IndexerSearchNoAutomatic,
+                    _localizationService.GetLocalizedString("IndexerSearchNoAutomaticHealthCheckMessage"),
+                    "#no-indexers-available-with-automatic-search-enabled-sonarr-will-not-provide-any-automatic-search-results");
             }
 
             var interactiveSearchEnabled = _indexerFactory.InteractiveSearchEnabled(false);
 
             if (interactiveSearchEnabled.Empty())
             {
-                return new HealthCheck(GetType(), HealthCheckResult.Warning, _localizationService.GetLocalizedString("IndexerSearchNoInteractiveHealthCheckMessage"), "#no-indexers-available-with-interactive-search-enabled");
+                return new HealthCheck(GetType(),
+                    HealthCheckResult.Warning,
+                    HealthCheckReason.IndexerSearchNoInteractive,
+                    _localizationService.GetLocalizedString("IndexerSearchNoInteractiveHealthCheckMessage"),
+                    "#no-indexers-available-with-interactive-search-enabled");
             }
 
             var active = _indexerFactory.AutomaticSearchEnabled(true);
 
             if (active.Empty())
             {
-                return new HealthCheck(GetType(), HealthCheckResult.Warning, _localizationService.GetLocalizedString("IndexerSearchNoAvailableIndexersHealthCheckMessage"), "#indexers-are-unavailable-due-to-failures");
+                return new HealthCheck(GetType(),
+                    HealthCheckResult.Warning,
+                    HealthCheckReason.IndexerSearchNoAvailableIndexers,
+                    _localizationService.GetLocalizedString("IndexerSearchNoAvailableIndexersHealthCheckMessage"),
+                    "#indexers-are-unavailable-due-to-failures");
             }
 
             return new HealthCheck(GetType());

src/NzbDrone.Core/HealthCheck/Checks/SystemTimeCheck.cs
@@ -37,7 +37,11 @@ namespace NzbDrone.Core.HealthCheck.Checks
                 if (Math.Abs(result.DateTimeUtc.Subtract(systemTime).TotalDays) >= 1)
                 {
                     _logger.Error("System time mismatch. SystemTime: {0} Expected Time: {1}. Update system time", systemTime, result.DateTimeUtc);
-                    return new HealthCheck(GetType(), HealthCheckResult.Error, _localizationService.GetLocalizedString("SystemTimeHealthCheckMessage"), "#system-time-off");
+                    return new HealthCheck(GetType(),
+                        HealthCheckResult.Error,
+                        HealthCheckReason.SystemTime,
+                        _localizationService.GetLocalizedString("SystemTimeHealthCheckMessage"),
+                        "#system-time-off");
                 }
             }
             catch (Exception e)

Lidarr/Lidarr

(cherry picked from commit beadc687ae47c6d90cba247f84595a692d79a736)

src/NzbDrone.Core/HealthCheck/Checks/IndexerSearchCheck.cs
@@ -25,21 +25,33 @@ namespace NzbDrone.Core.HealthCheck.Checks
 
             if (automaticSearchEnabled.Empty())
             {
-                return new HealthCheck(GetType(), HealthCheckResult.Warning, _localizationService.GetLocalizedString("IndexerSearchCheckNoAutomaticMessage"), "#no-indexers-available-with-automatic-search-enabled-lidarr-will-not-provide-any-automatic-search-results");
+                return new HealthCheck(GetType(),
+                    HealthCheckResult.Warning,
+                    HealthCheckReason.IndexerSearchNoAutomatic,
+                    _localizationService.GetLocalizedString("IndexerSearchCheckNoAutomaticMessage"),
+                    "#no-indexers-available-with-automatic-search-enabled-lidarr-will-not-provide-any-automatic-search-results");
             }
 
             var interactiveSearchEnabled = _indexerFactory.InteractiveSearchEnabled(false);
 
             if (interactiveSearchEnabled.Empty())
             {
-                return new HealthCheck(GetType(), HealthCheckResult.Warning, _localizationService.GetLocalizedString("IndexerSearchCheckNoInteractiveMessage"), "#no-indexers-available-with-interactive-search-enabled");
+                return new HealthCheck(GetType(),
+                    HealthCheckResult.Warning,
+                    HealthCheckReason.IndexerSearchNoInteractive,
+                    _localizationService.GetLocalizedString("IndexerSearchCheckNoInteractiveMessage"),
+                    "#no-indexers-available-with-interactive-search-enabled");
             }
 
             var active = _indexerFactory.AutomaticSearchEnabled(true);
 
             if (active.Empty())
             {
-                return new HealthCheck(GetType(), HealthCheckResult.Warning, _localizationService.GetLocalizedString("IndexerSearchCheckNoAvailableIndexersMessage"), "#indexers-are-unavailable-due-to-failures");
+                return new HealthCheck(GetType(),
+                    HealthCheckResult.Warning,
+                    HealthCheckReason.IndexerSearchNoAvailableIndexers,
+                    _localizationService.GetLocalizedString("IndexerSearchCheckNoAvailableIndexersMessage"),
+                    "#indexers-are-unavailable-due-to-failures");
             }
 
             return new HealthCheck(GetType());

src/NzbDrone.Core/HealthCheck/Checks/SystemTimeCheck.cs
@@ -37,7 +37,11 @@ namespace NzbDrone.Core.HealthCheck.Checks
                 if (Math.Abs(result.DateTimeUtc.Subtract(systemTime).TotalDays) >= 1)
                 {
                     _logger.Error("System time mismatch. SystemTime: {0} Expected Time: {1}. Update system time", systemTime, result.DateTimeUtc);
-                    return new HealthCheck(GetType(), HealthCheckResult.Error, _localizationService.GetLocalizedString("SystemTimeCheckMessage"), "#system-time-off");
+                    return new HealthCheck(GetType(),
+                        HealthCheckResult.Error,
+                        HealthCheckReason.SystemTime,
+                        _localizationService.GetLocalizedString("SystemTimeCheckMessage"),
+                        "#system-time-off");
                 }
             }
             catch (Exception e)